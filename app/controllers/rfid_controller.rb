1fc1c78ed5e3abded03181cfc2520ad21a3c13cf
Wesley Ellis <tahnok@gmail.com>
2016-03-07 20:49:05 -0500

    change recent_unset to use updated_at and touch existing rfids
    Also includes tests for rfid_controller

--- app/controllers/rfid_controller.rb
+++ app/controllers/rfid_controller.rb
@@ -1,18 +1,27 @@
 class RfidController < SessionsController
 
   def card_number
-  	rfid = Rfid.find_by card_number: params[:rfid]
+  	rfid = Rfid.find_by(card_number: params[:card_number])
 
-		if rfid && rfid.user_id
-			render json: { success: "RFID exist" }, status: :ok
+		if rfid
+      if rfid.user_id
+        render json: { success: "RFID exist" }, status: :ok
+      else
+        rfid.touch
+				render json: { error: "Temporary RFID already exists" }, status: :unprocessable_entity
+      end
 		else
-			new_rfid = Rfid.create(card_number: params[:rfid])
+			new_rfid = Rfid.create(rfid_params)
 			if new_rfid.valid?
 				render json: { new_rfid: "Temporary RFID created" }, status: :unprocessable_entity
-			else
-				render json: { error: "Temporary RFID already exists" }, status: :unprocessable_entity
 			end
 		end
+  end
+
+  private
+
+  def rfid_params
+    params.permit(:card_number)
   end
 
 end
