d820df1dd0d3b88275d15875c87e49eda2d51a8a
Erick Kelvin <erickkelvin@Ericks-MacBook-Pro.local>
2016-06-16 12:06:44 -0400

    Adding/fixing page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -69,6 +69,15 @@ module ApplicationHelper
   #     a
   #   end
   # end
+  
+  def page_title (curr_page = '')
+    base_title = "MakerRepo"
+    if curr_page.empty?
+      base_title
+    else
+      curr_page + " | " + base_title
+    end
+  end
 
 end
 
