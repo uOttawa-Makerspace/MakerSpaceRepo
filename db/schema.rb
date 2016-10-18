1339ade58f2eb98c2888ca30b4d4f63d9675ccf5
Wesley Ellis <tahnok@gmail.com>
2016-10-18 19:43:46 -0400

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151027000659) do
+ActiveRecord::Schema.define(version: 20160826210404) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.integer  "repository_id"
+    t.string   "name"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  add_index "categories", ["repository_id"], name: "index_categories_on_repository_id", using: :btree
+
+  create_table "category_options", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "certifications", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "certifications", ["user_id"], name: "index_certifications_on_user_id", using: :btree
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id"
@@ -28,6 +52,32 @@ ActiveRecord::Schema.define(version: 20151027000659) do
 
   add_index "comments", ["repository_id"], name: "index_comments_on_repository_id", using: :btree
   add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
+
+  create_table "equipment", force: :cascade do |t|
+    t.integer  "repository_id"
+    t.string   "name"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  add_index "equipment", ["repository_id"], name: "index_equipment_on_repository_id", using: :btree
+
+  create_table "equipment_options", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "lab_sessions", force: :cascade do |t|
+    t.integer  "user_id"
+    t.datetime "sign_in_time"
+    t.datetime "sign_out_time"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.string   "mac_address"
+  end
+
+  add_index "lab_sessions", ["user_id"], name: "index_lab_sessions_on_user_id", using: :btree
 
   create_table "likes", force: :cascade do |t|
     t.integer  "user_id"
@@ -53,6 +103,25 @@ ActiveRecord::Schema.define(version: 20151027000659) do
 
   add_index "photos", ["repository_id"], name: "index_photos_on_repository_id", using: :btree
 
+  create_table "pi_readers", force: :cascade do |t|
+    t.string   "pi_mac_address"
+    t.string   "pi_location"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
+
+  create_table "repo_files", force: :cascade do |t|
+    t.integer  "repository_id"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
+    t.string   "file_file_name"
+    t.string   "file_content_type"
+    t.integer  "file_file_size"
+    t.datetime "file_updated_at"
+  end
+
+  add_index "repo_files", ["repository_id"], name: "index_repo_files_on_repository_id", using: :btree
+
   create_table "repositories", force: :cascade do |t|
     t.integer  "user_id"
     t.string   "title"
@@ -77,18 +146,10 @@ ActiveRecord::Schema.define(version: 20151027000659) do
     t.string   "card_number"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "mac_address"
   end
 
   add_index "rfids", ["user_id"], name: "index_rfids_on_user_id", using: :btree
-
-  create_table "tags", force: :cascade do |t|
-    t.integer  "repository_id"
-    t.string   "name"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-  end
-
-  add_index "tags", ["repository_id"], name: "index_tags_on_repository_id", using: :btree
 
   create_table "upvotes", force: :cascade do |t|
     t.integer  "user_id"
@@ -124,14 +185,17 @@ ActiveRecord::Schema.define(version: 20151027000659) do
     t.boolean  "terms_and_conditions"
   end
 
+  add_foreign_key "categories", "repositories"
+  add_foreign_key "certifications", "users"
   add_foreign_key "comments", "repositories"
   add_foreign_key "comments", "users"
+  add_foreign_key "equipment", "repositories"
   add_foreign_key "likes", "repositories"
   add_foreign_key "likes", "users"
   add_foreign_key "photos", "repositories"
+  add_foreign_key "repo_files", "repositories"
   add_foreign_key "repositories", "users"
   add_foreign_key "rfids", "users"
-  add_foreign_key "tags", "repositories"
   add_foreign_key "upvotes", "comments"
   add_foreign_key "upvotes", "users"
 end
