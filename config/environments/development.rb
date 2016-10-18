aa13b8fc9daeeef29f3c38e86e8c9957f16dcf14
Wesley Ellis <tahnok@gmail.com>
2016-10-18 19:46:05 -0400

    still run if no AWS creds in dev

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -66,10 +66,10 @@ Rails.application.configure do
   config.paperclip_defaults = {
     storage: :s3,
     s3_credentials: {
-      bucket: ENV.fetch('S3_BUCKET_NAME'),
-      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-      s3_region: ENV.fetch('AWS_REGION')
+      bucket: ENV.fetch('S3_BUCKET_NAME', "wrong"),
+      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID', "wrong"),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY', "wrong"),
+      s3_region: ENV.fetch('AWS_REGION', "wrong")
     }
   }
 
