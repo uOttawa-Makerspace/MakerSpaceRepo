853c37edb86e5d6fc7935397957a743da0899507
Karim Chukfeh <karimchukfeh@gmail.com>
2017-05-11 14:29:55 -0400

    removed the stuff i had written in user_test.rb

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,12 +1,7 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-
-  context 'user basic info' do
-    setup do
-      @b = users(:bob)
-      should validate_presence_of(:email)
-      should validate_presence_of(:password)
-    end
-  end
+  # test "the truth" do
+  #   assert true
+  # end
 end
