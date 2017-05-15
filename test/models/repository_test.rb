2e0d6f8e19248288618f55daabafa332bef15dd9
Parastoo <psaha091@uottawa.ca>
2017-05-15 11:24:02 -0400

    Wrote tests for valid repositories

--- test/models/repository_test.rb
+++ test/models/repository_test.rb
@@ -1,7 +1,37 @@
 require 'test_helper'
 
 class RepositoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	
+	test "Presence of title" do
+		repository = repositories(:one)
+
+		repository.title = "Project 1"
+		assert repository.valid? , "Project title is required."
+
+		repository.title = nil
+		assert repository.invalid? , "Project title is required."
+	end
+
+	test "Uniqueness of repository title" do
+
+		repository = repositories(:two)
+
+		repository.title = "Repository1"
+		assert repository.invalid? , "Project title is already in use."
+
+
+		repository.title = "Repository2"
+		assert repository.valid? , "Project title is already in use."
+	end
+
+	test "Valid project title" do
+		repository = repositories(:one)
+
+		repository.title = "MakerRepo"
+		assert repository.valid?, "Invalid project title"
+
+		repository.title = "/*MakerRepo*/"
+		assert repository.invalid?, "Invalid project title"
+
+	end
 end
