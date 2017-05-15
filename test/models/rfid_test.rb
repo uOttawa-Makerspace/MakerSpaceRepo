950defad2bdfd080ec24850a5b749ba5b4e39539
Parastoo <psaha091@uottawa.ca>
2017-05-15 13:21:05 -0400

    Wrote tests for Rfid validation

--- test/models/rfid_test.rb
+++ test/models/rfid_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class RfidTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+	test "Card presence" do 
+		rfid = rfids(:assigned)
+
+		rfid.card_number = "123456"
+		assert rfid.valid? , "Card number is required."
+
+		rfid.card_number = nil
+		assert rfid.invalid? , "Card number is required."
+	end
+
+	test "Card's uniqueness" do
+
+		rfid = rfids(:assigned)
+
+		rfid.card_number = "123456789"
+		assert rfid.invalid?, "Card number is already in use."
+
+		rfid.card_number = "987654321"
+		assert rfid.valid?, "Card number is already in use."
+	end
+
 end
