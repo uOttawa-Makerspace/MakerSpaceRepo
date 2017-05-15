759e4f80a469f97934d99a096a674e32d02e2de8
Parastoo <psaha091@uottawa.ca>
2017-05-15 15:00:33 -0400

    Wrote tests for comments' validation

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,35 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	test "Presence of content" do 
+		comment = comments(:one)
+
+		comment.content = nil
+		assert comment.invalid? , "Please add content"
+
+		comment.content = "Great job!"
+		assert comment.valid? , "Please add content"
+	end
+
+	test "Presence of user ID" do 
+		comment = comments(:one)
+
+		comment.user_id = nil
+		assert comment.invalid? , "Please add user ID"
+
+		comment.user_id = 246813579
+		assert comment.valid? , "Please add user ID"
+	end
+
+	test "Presence of repository ID" do 
+		comment = comments(:one)
+
+		comment.repository_id = nil
+		assert comment.invalid? , "Please add repositoy ID"
+
+		comment.repository_id = 224466
+		assert comment.valid? , "Please add repositoy ID"
+	end
+
 end
