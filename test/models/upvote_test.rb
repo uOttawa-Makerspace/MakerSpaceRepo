fcddb9e357f0b328400428f37d6623c6ab2ae5bd
Parastoo <psaha091@uottawa.ca>
2017-05-16 11:10:44 -0400

    completed tests for upvotes

--- test/models/upvote_test.rb
+++ test/models/upvote_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class UpvoteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	test "Comment ID uniqueness" do 
+		upvote = Upvote.new(:user_id => 123123 , :comment_id => 456456)
+		assert upvote.invalid? , "You have already upvoted this comment"
+
+		upvote = Upvote.new(:user_id => 123123 , :comment_id => 654654)
+		assert upvote.valid? , "You have already upvoted this comment"
+	end
+
+	test "User ID uniqueness" do
+
+		upvote = Upvote.new(:user_id => 123123 , :comment_id => 456456)
+		assert upvote.invalid? , "You have already upvoted this comment"
+
+		upvote = Upvote.new(:user_id => 321321 , :comment_id => 456456)
+		assert upvote.valid? , "You have already upvoted this comment"
+	end
+
 end
