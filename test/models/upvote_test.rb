fd6d29c19dc1805c9605f27cca06516644b5e2f6
Parastoo <psaha091@uottawa.ca>
2017-05-17 11:08:41 -0400

    made changes to the comment_id test

--- test/models/upvote_test.rb
+++ test/models/upvote_test.rb
@@ -2,21 +2,30 @@ require 'test_helper'
 
 class UpvoteTest < ActiveSupport::TestCase
 
-	test "Comment ID uniqueness" do 
-		upvote = Upvote.new(:user_id => 123123 , :comment_id => 456456)
-		assert upvote.invalid? , "You have already upvoted this comment"
+	test "Comment ID uniqueness" do
+		user1 = users(:bob)
+		user2 = users(:mary)
 
-		upvote = Upvote.new(:user_id => 123123 , :comment_id => 654654)
-		assert upvote.valid? , "You have already upvoted this comment"
+		upvote1 = Upvote.new(:user => user1 , :comment_id => 456456)
+		upvote2 = Upvote.new(:user => user2 , :comment_id => 654654)
+
+		assert_not_equal(upvote1.comment_id , upvote2.comment_id, "Comment ID's are not unique")
+
+ 
+		upvote1 = Upvote.new(:user => user1 , :comment_id => 456456)
+		upvote2 = Upvote.new(:user => user2 , :comment_id => 456456)
+
+		assert_equal(upvote1.comment_id , upvote2.comment_id, "Comment ID's are unique")
 	end
 
-	test "User ID uniqueness" do
 
-		upvote = Upvote.new(:user_id => 123123 , :comment_id => 456456)
-		assert upvote.invalid? , "You have already upvoted this comment"
+	test "User ID presence" do
 
-		upvote = Upvote.new(:user_id => 321321 , :comment_id => 456456)
-		assert upvote.valid? , "You have already upvoted this comment"
+		upvote = Upvote.new(:user => nil , :comment_id => 654654)
+		assert upvote.invalid? , "Please provide user ID"
+
+		upvote = Upvote.new(:user => users(:bob) , :comment_id => 654654)
+		assert upvote.valid? , "Please provide user ID"
 	end
 
 end
