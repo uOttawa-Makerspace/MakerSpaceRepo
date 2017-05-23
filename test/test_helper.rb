0c4c2f5f8daccb87549e11efae8c6b96fa274943
Karim Chukfeh <karimchukfeh@gmail.com>
2017-05-23 13:26:09 -0400

    require mocha in test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,13 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
+require File.expand_path('../../app/helpers/application_helper')
 require 'rails/test_help'
+require "mocha/test_unit"
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+
 end
