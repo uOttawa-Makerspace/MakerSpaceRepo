a34fa0b3930981f2f27a2cb76f97ab5c85d82b9c
Karim Chukfeh <karimchukfeh@gmail.com>
2017-05-24 11:47:02 -0400

    these tests dont work

--- test/controllers/staff_dashboard_controller_test.rb
+++ test/controllers/staff_dashboard_controller_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class StaffDashboardControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+=begin
+   test "regular users are redirected to home" do
+     @user = users(:bob)
+     get :index
+     assert_redirected_to root_path
+   end
+
+   test "admins succeed at loading Staff Dashboard" do
+     @user = User.create(:username => "test3",
+                         :password => "Test12345",
+                         :pword => "Test12345",
+                         :email => "admin3@test.com",
+                         :role => "admin",
+                         terms_and_conditions: true)
+     get :index
+     assert_response :found
+   end
+=end
 end
