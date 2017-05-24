e99d838e42cfbcf21cc5e0eb61067d511fc42279
Karim Chukfeh <karimchukfeh@gmail.com>
2017-05-24 10:38:00 -0400

    ensure user is admin

--- app/controllers/staff_dashboard_controller.rb
+++ app/controllers/staff_dashboard_controller.rb
@@ -1,2 +1,15 @@
 class StaffDashboardController < ApplicationController
+
+  before_action :current_user
+  before_action :ensure_admin
+
+
+  private
+
+  def ensure_admin
+    unless admin?
+      redirect_to '/' and return
+    end
+  end
+
 end
