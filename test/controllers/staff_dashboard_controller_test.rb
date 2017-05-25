e413ef9443e31edc09ab1c1ea6b505875ceefd09
Karim Chukfeh <karimchukfeh@gmail.com>
2017-05-25 10:05:10 -0400

    added asserstions to tests. still not working properly

--- test/controllers/staff_dashboard_controller_test.rb
+++ test/controllers/staff_dashboard_controller_test.rb
@@ -5,21 +5,19 @@ class StaffDashboardControllerTest < ActionController::TestCase
    test "admins succeed at loading Staff Dashboard" do
      @user2 = users(:tom)
      get :index
-     #assert_response :found
-     #assert_redirected_to root_path
      p "tom is an " + @user2.role
      p request.original_url
      print "\n"
+     assert_redirected_to root_path
    end
 
    test "regular users are redirected to home" do
      @user1 = users(:bob)
      get :index
-     #assert_redirected_to root_path
      p "bob is a " + @user1.role
      p request.original_url
      print "\n"
-
+     assert_response :found
    end
 
 end
