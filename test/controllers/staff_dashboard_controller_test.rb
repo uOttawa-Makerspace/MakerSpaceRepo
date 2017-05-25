fe1e031ba4e167f11ec5536c2f24319280fea355
Karim Chukfeh <karimchukfeh@gmail.com>
2017-05-25 09:52:34 -0400

    added admin fixture and demonstration of the tests not working as intended

--- test/controllers/staff_dashboard_controller_test.rb
+++ test/controllers/staff_dashboard_controller_test.rb
@@ -1,23 +1,25 @@
 require 'test_helper'
 
 class StaffDashboardControllerTest < ActionController::TestCase
-=begin
-   test "regular users are redirected to home" do
-     @user = users(:bob)
-     get :index
-     assert_redirected_to root_path
-   end
 
    test "admins succeed at loading Staff Dashboard" do
-     @user = User.create(:username => "test3",
-                         :password => "Test12345",
-                         :pword => "Test12345",
-                         :email => "admin3@test.com",
-                         :role => "admin",
-                         terms_and_conditions: true)
+     @user2 = users(:tom)
      get :index
-     assert_response :found
+     #assert_response :found
+     #assert_redirected_to root_path
+     p "tom is an " + @user2.role
+     p request.original_url
+     print "\n"
    end
-=end
+
+   test "regular users are redirected to home" do
+     @user1 = users(:bob)
+     get :index
+     #assert_redirected_to root_path
+     p "bob is a " + @user1.role
+     p request.original_url
+     print "\n"
+
+   end
 
 end
