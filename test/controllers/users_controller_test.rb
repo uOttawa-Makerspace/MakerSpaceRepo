afc045af2fd7a027a9b5f14b381203e9beb187be
Karim Chukfeh <karimchukfeh@gmail.com>
2017-05-29 11:05:51 -0400

    added more user tests for change_password

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,8 +2,9 @@ require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
 
+  ##########
+  #create tests
   test "creating the same username or email again return unprocessable_entity" do
-    #try to create bob again, bob is a fixture
     post :create, user: {
                 username: "bob",
                 name: "Bob",
@@ -15,7 +16,6 @@ class UsersControllerTest < ActionController::TestCase
   end
 
   test "creating a user returns :found and saves user in the database" do
-    #try to create sam for the first time
     post :create, user: {
                 username: "sam",
                 name: "Sam",
@@ -26,13 +26,17 @@ class UsersControllerTest < ActionController::TestCase
     assert User.exists?(username: "sam"), "\nFailed at saving Sam"
   end
 
+  ##########
+  #new tests
   test "new returns :success if user is not signed in and redirects to home if user is" do
     get :new
     assert_response :success or
     assert_redirected_to root_path,
-                    "User is signed in and failed at redirecting to home"
+                    "User is signed in but failed at redirecting to home"
   end
 
+  ##########
+  #change_password tests
   test "users should be able sign in to change password" do
     post :create, user: {
                 username: "sam",
@@ -44,11 +48,50 @@ class UsersControllerTest < ActionController::TestCase
     assert_response :found, "\nFailed at creating Sam"
     assert User.exists?(username: "sam"), "\nFailed at saving Sam"
     @user = User.find_by(username: "sam")
+    @oldpass = @user.password
     post :change_password, username: @user.username,
       user: {old_password: "Password1", password: "Password2", password_confirmation: "Password2"}
+    @newpass = User.find_by(username: "sam").password
     assert_equal 'Password changed successfully', flash[:notice]
-
+    assert @oldpass != @newpass
   end
 
+  test "users can't change password if old password is wrong" do
+    post :create, user: {
+                username: "sam",
+                name: "Sam",
+                email: "sam@sam.sam",
+                terms_and_conditions: true,
+                password: "Password1",
+                role: "admin"}
+    assert_response :found, "\nFailed at creating Sam"
+    assert User.exists?(username: "sam"), "\nFailed at saving Sam"
+    @user = User.find_by(username: "sam")
+    @oldpass = @user.password
+    post :change_password, username: @user.username,
+      user: {old_password: "WrongOldPass1", password: "Password2", password_confirmation: "Password2"}
+    @newpass = User.find_by(username: "sam").password
+    assert_equal 'Incorrect old password.', flash.now[:alert]
+    assert @oldpass == @newpass
+  end
+
+  test "users can't change password if passwords don't match" do
+    post :create, user: {
+                username: "sam",
+                name: "Sam",
+                email: "sam@sam.sam",
+                terms_and_conditions: true,
+                password: "Password1",
+                role: "admin"}
+    assert_response :found, "\nFailed at creating Sam"
+    assert User.exists?(username: "sam"), "\nFailed at saving Sam"
+    @user = User.find_by(username: "sam")
+    @oldpass = @user.password
+    post :change_password, username: @user.username,
+      user: {old_password: "Password1", password: "Password2", password_confirmation: "WrongConfirmationPass1"}
+    @newpass = User.find_by(username: "sam").password
+    assert_equal nil, flash.now[:alert]
+    assert @oldpass == @newpass
+  end
 
 end
