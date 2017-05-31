0de2981300e87f5db60c9a27dd03f8790003866b
Karim Chukfeh <karimchukfeh@gmail.com>
2017-05-31 00:04:11 -0400

    got the the tests working

--- test/controllers/staff_dashboard_controller_test.rb
+++ test/controllers/staff_dashboard_controller_test.rb
@@ -2,22 +2,16 @@ require 'test_helper'
 
 class StaffDashboardControllerTest < ActionController::TestCase
 
-  #  test "admins succeed at loading Staff Dashboard" do
-  #    @user2 = users(:tom)
-  #    get :index
-  #    p "tom is an " + @user2.role
-  #    p request.original_url
-  #    print "\n"
-  #    assert_redirected_to root_path
-  #  end
-   #
-  #  test "regular users are redirected to home" do
-  #    @user1 = users(:bob)
-  #    get :index
-  #    p "bob is a " + @user1.role
-  #    p request.original_url
-  #    print "\n"
-  #    assert_response :found
-  #  end
+   test "admins succeed at loading Staff Dashboard" do
+     session[:user_id] = users(:adam).id
+     get :index
+     assert_response :success
+   end
+
+   test "regular users are redirected to home" do
+     session[:user_id] = users(:bob).id
+     get :index
+     assert_redirected_to root_path
+   end
 
 end
