f23e9fb56e1dd77b3a4c36452e50b8c68dea418d
Parastoo <psaha091@uottawa.ca>
2017-06-01 10:22:26 -0400

    Deleted the routes for additonal page

--- config/routes.rb
+++ config/routes.rb
@@ -106,9 +106,6 @@ Rails.application.routes.draw do
    # USER RESOURCES
   resources :users, path: '/', param: :username, except: :edit do
     get 'likes', on: :member
-    # get 'sign_up_successful', on: :member
-    # match 'additional_info', on: :member, via: [:get, :patch]
-
     patch 'change_password', on: :member
   end
 
