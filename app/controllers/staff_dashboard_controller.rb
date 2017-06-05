f954bbdbf5d86a672d0e0340b78334e8fdf2142b
Karim Chukfeh <karimchukfeh@gmail.com>
2017-06-05 09:25:34 -0400

    added bulk_add_certifications to staff_dashboard_controller

--- app/controllers/staff_dashboard_controller.rb
+++ app/controllers/staff_dashboard_controller.rb
@@ -4,6 +4,25 @@ class StaffDashboardController < ApplicationController
   def index
   end
 
+  def create_lab_session
+  end
+
+  def bulk_add_certifications
+    @staff = current_user
+    if params['bulk_cert_users'].present? && params['bulk_certifications'].present?
+      params['bulk_cert_users'].each do |user|
+        if !User.find(user).certifications.where(name: params['bulk_certifications']).present?
+          Certification.create(name: params['bulk_certifications'], user_id: user, staff_id: @staff)
+        end
+      end
+      redirect_to (:back)
+      flash[:notice] = "Certifications added succesfully!"
+    else
+      redirect_to (:back)
+      flash[:alert] = "Invalid parameters!"
+    end
+  end
+
   private
 
   def ensure_staff
