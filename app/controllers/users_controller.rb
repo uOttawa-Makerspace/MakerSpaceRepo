2acd057468e5b761d0b9504210bc5dfc4b37fe1e
Parastoo <psaha091@uottawa.ca>
2017-06-05 13:14:23 -0400

    fixed the mistake calling the mailer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < SessionsController
     respond_to do |format|
       if @new_user.save
         flash[:notice] = "Profile created successfully."
-        MsrMailer.welcome_email(@user).deliver_later
+        MsrMailer.welcome_email(@new_user).deliver_now
         session[:user_id], cookies[:user_id] = @new_user.id, @new_user.id
         format.html { redirect_to settings_profile_path(@new_user.username) }
         format.json { render json: { success: @new_user.id }, status: :ok }
