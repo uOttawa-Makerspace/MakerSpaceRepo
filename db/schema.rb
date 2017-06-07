3fe2fd4533b8693827a6577f5d1b5935a287cdbc
Karim Chukfeh <karimchukfeh@gmail.com>
2017-06-07 12:11:49 -0400

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160826210404) do
+ActiveRecord::Schema.define(version: 20170607160621) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -151,6 +151,15 @@ ActiveRecord::Schema.define(version: 20160826210404) do
 
   add_index "rfids", ["user_id"], name: "index_rfids_on_user_id", using: :btree
 
+  create_table "trainings", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "trainings", ["user_id"], name: "index_trainings_on_user_id", using: :btree
+
   create_table "upvotes", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "comment_id"
@@ -196,6 +205,7 @@ ActiveRecord::Schema.define(version: 20160826210404) do
   add_foreign_key "repo_files", "repositories"
   add_foreign_key "repositories", "users"
   add_foreign_key "rfids", "users"
+  add_foreign_key "trainings", "users"
   add_foreign_key "upvotes", "comments"
   add_foreign_key "upvotes", "users"
 end
