8b7246ff708338cc16d31eb6da82c99b5ffea7f7
Parastoo <psaha091@uottawa.ca>
2017-06-09 11:26:48 -0400

    Passed in the wanted info as an arguments to generate the csv

--- app/controllers/report_generator_controller.rb
+++ app/controllers/report_generator_controller.rb
@@ -3,9 +3,9 @@ class ReportGeneratorController < ApplicationController
   	@users = User.all
 
   	respond_to do |format|
-
+  		attributes = %w{id name username email faculty created_at}
   		format.html
-  		format.csv {send_data @users.to_csv}
+  		format.csv {send_data @users.to_csv(*attributes)}
   	end
   end
 end
