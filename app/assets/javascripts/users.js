0e21fd3ec7f711b2fe3378f2023f42509d74f06c
Parastoo <psaha091@uottawa.ca>
2017-06-14 13:19:24 -0400

    Added the toggling functions

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,6 +1,14 @@
 $(document).ready(function() {
   $("[id^=student]").hide();
-  $(".some-class").change(function() {
-    $("[id^=student]").toggle(this.value == 'Student');
+  $(".class1").change(function() {
+
+    $("[id^=student]").toggle(this.value == 'Grad');
+  });
+});
+
+$(document).ready(function() {
+  $("[id^=student]").hide();
+  $(".class2").change(function() {
+    $("[id^=student]").toggle(this.value == 'Undergrad');
   });
 });
