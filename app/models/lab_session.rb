582fe83bca5cb3d217555ab306bec1fc029d6b8f
Parastoo <psaha091@uottawa.ca>
2017-06-26 12:46:06 -0400

    Added a new method to create the csv file

--- app/models/lab_session.rb
+++ app/models/lab_session.rb
@@ -1,3 +1,27 @@
 class LabSession < ActiveRecord::Base
   belongs_to :user
+
+  def create_array                  
+    labs = LabSession.all    
+    column = []             
+    labs.each do |lab|       
+      row = []              
+      row << lab.id        
+      user = lab.user       
+      row << user.name << user.email << user.faculty     
+      column << row         
+    end                     
+    column                  
+  end
+
+  def to_csv
+  	attributes = create_array
+
+    CSV.generate do |csv|
+      attributes.each do |row|
+        csv << row
+      end
+    end
+  end                       
+
 end
