8bdc25ed940f88460d001e0b9ff8269cd3669966
Parastoo <psaha091@uottawa.ca>
2017-06-27 14:24:20 -0400

    Deleted the test

--- test/models/lab_session_test.rb
+++ test/models/lab_session_test.rb
@@ -2,15 +2,4 @@ require 'test_helper'
 
 class LabSessionTest < ActiveSupport::TestCase
 
-	# Will delete this later
-
-	# test "array" do 
-	# 	lab = LabSession.new
-	# 	array1 = lab.create_array
-	# 	array2 = [[980190962, "Bob", "bob@gmail.com", "science"],
- # 				  [298486374, "Mary", "mary@gmail.com", "engineering"],
- # 				  [113629430, "Bob", "bob@gmail.com", "science"]]		
-
-	# 	assert_equal( array2 , array1, "Not Equal")
-	# end
 end
