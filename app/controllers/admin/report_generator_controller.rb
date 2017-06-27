4d654d01f552c86f7933cd15240e36a356866d5d
Parastoo <psaha091@uottawa.ca>
2017-06-27 14:13:43 -0400

    Report2 returns the people signed in, in the past month (visits)

--- app/controllers/admin/report_generator_controller.rb
+++ app/controllers/admin/report_generator_controller.rb
@@ -16,11 +16,19 @@ class Admin::ReportGeneratorController < AdminAreaController
   end
 
   def report2
-    @lab_info = LabSession.new
+    @labs = LabSession.in_last_month
+    column = []
+    column << ["lab_id", "sign_in_time", "user_id", "name", "email", "faculty", "program"]             
+    @labs.each do |lab|       
+      row = []              
+      row << lab.id << lab.sign_in_time       
+      user = lab.user       
+      row << lab.user.id << lab.user.name << lab.user.email << user.faculty << lab.user.program    
+      column << row         
+    end                                      
     respond_to do |format|
       format.html
-      format.csv {send_data @lab_info.to_csv}
+      format.csv {send_data @labs.to_csv(column)}
     end
   end
-
 end
