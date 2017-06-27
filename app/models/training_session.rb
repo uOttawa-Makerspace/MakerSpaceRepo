feada3a6cf361c9938a79f7a4025031aa4fb724a
Karim Chukfeh <karimchukfeh@gmail.com>
2017-06-27 10:26:37 -0400

    making sure no duplicate users in training_session

--- app/models/training_session.rb
+++ app/models/training_session.rb
@@ -1,7 +1,7 @@
 class TrainingSession < ActiveRecord::Base
   belongs_to :training
   belongs_to :user
-  has_and_belongs_to_many :users
+  has_and_belongs_to_many :users, :uniq => true
   has_many :certifications
 
   validates :training, presence: { message: "A training subject is required"}
@@ -11,4 +11,5 @@ class TrainingSession < ActiveRecord::Base
   def is_staff
     errors.add(:string, "user must be staff") unless self.user.staff?
   end
+
 end
