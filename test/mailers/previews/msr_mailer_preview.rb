37fbe870439d374ea0e3ec572b8c0fb8cd84412c
Parastoo <psaha091@uottawa.ca>
2017-06-28 10:16:52 -0400

    Added previews for mailers

--- test/mailers/previews/msr_mailer_preview.rb
+++ test/mailers/previews/msr_mailer_preview.rb
@@ -1,4 +1,15 @@
 # Preview all emails at http://localhost:3000/rails/mailers/msr_mailer
 class MsrMailerPreview < ActionMailer::Preview
 
+	def welcome_email
+		MsrMailer.welcome_email(User.first)
+	end
+
+	def repository_email
+		MsrMailer.repo_report(Repository.first)
+	end
+
+	def reset_password_email
+		MsrMailer.reset_password_email(User.first.email , "Password2")
+	end
 end
