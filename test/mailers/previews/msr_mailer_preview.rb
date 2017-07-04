77dd2a9e98bb2ec4002824c8c2457718c8058691
Parastoo <psaha091@uottawa.ca>
2017-07-04 15:24:32 -0400

    Added a new preview

--- test/mailers/previews/msr_mailer_preview.rb
+++ test/mailers/previews/msr_mailer_preview.rb
@@ -12,4 +12,11 @@ class MsrMailerPreview < ActionMailer::Preview
 	def reset_password_email
 		MsrMailer.reset_password_email(User.first.email , "Password2")
 	end
+
+	def send_report_email
+		MsrMailer.send_report('makerspace@uottawa.ca', Admin::ReportGeneratorController.new.report1_generator(),
+													Admin::ReportGeneratorController.new.report2_generator(),
+													Admin::ReportGeneratorController.new.report3_generator())
+	end
+
 end
