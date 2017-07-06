8e3fac2ed424dfda08fc6e3088e08e510daeca05
Karim Chukfeh <karimchukfeh@gmail.com>
2017-07-06 12:02:49 -0400

    def repositories for CategoryOption

--- app/models/category_option.rb
+++ app/models/category_option.rb
@@ -2,4 +2,9 @@ class CategoryOption < ActiveRecord::Base
   belongs_to :admin
   validates :name, presence: true, uniqueness: true
   scope :show_options, -> { order("lower(name) ASC").all }
+
+  def repositories
+    return Repository.joins(:categories).where(categories: { category_option_id: 1 })
+  end
+
 end
