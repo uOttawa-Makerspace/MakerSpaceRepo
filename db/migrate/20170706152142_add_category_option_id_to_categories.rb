00e3d70c9ce85703c2208586b3a388158c008aca
Karim Chukfeh <karimchukfeh@gmail.com>
2017-07-10 10:11:05 -0400

    use cat.save! instead of cat.save

--- db/migrate/20170706152142_add_category_option_id_to_categories.rb
+++ db/migrate/20170706152142_add_category_option_id_to_categories.rb
@@ -2,7 +2,7 @@ class AddCategoryOptionIdToCategories < ActiveRecord::Migration
   def change
     Category.all.each do |cat|
       cat.category_option_id = CategoryOption.find_by(name: cat.name).id
-      cat.save
+      cat.save!
     end
   end
 end
