1ce366f4d34fbdd7b4fb9efcb021cdb096896e44
Parastoo <psaha091@uottawa.ca>
2017-07-12 16:11:52 -0400

    Added a new method to call report5

--- app/controllers/admin/report_generator_controller.rb
+++ app/controllers/admin/report_generator_controller.rb
@@ -29,10 +29,21 @@ class Admin::ReportGeneratorController < AdminAreaController
     end
   end
 
+# Diversity of users based on gender
   def report4
     respond_to do |format|
       format.html
       format.csv {send_data ReportGenerator.gender_frequesncy_report}
     end
   end
+
+# Unique visitors/ month
+  def report5
+    respond_to do |format|
+      format.html
+      format.csv {send_data ReportGenerator.unique_visitors_report}
+    end
+  end
+
+
 end
