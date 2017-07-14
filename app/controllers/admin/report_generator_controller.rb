52fd8fdf0d57af6d20a3f46b7b45c452ebf0f769
Parastoo <psaha091@uottawa.ca>
2017-07-14 10:38:56 -0400

    Added filename to the methods

--- app/controllers/admin/report_generator_controller.rb
+++ app/controllers/admin/report_generator_controller.rb
@@ -8,7 +8,7 @@ class Admin::ReportGeneratorController < AdminAreaController
   def new_users
   	respond_to do |format|
   		format.html
-  		format.csv {send_data ReportGenerator.new_user_report }
+  		format.csv {send_data ReportGenerator.new_user_report, filename: "new_users-#{Date.today}.csv" }
     end
   end
 
@@ -16,7 +16,7 @@ class Admin::ReportGeneratorController < AdminAreaController
   def total_visits
     respond_to do |format|
       format.html
-      format.csv {send_data ReportGenerator.lab_session_report }
+      format.csv {send_data ReportGenerator.lab_session_report, filename: "total_visits-#{Date.today}.csv" }
     end
   end
 
@@ -24,7 +24,7 @@ class Admin::ReportGeneratorController < AdminAreaController
   def unique_visits
     respond_to do |format|
       format.html
-      format.csv {send_data ReportGenerator.unique_visitors_report}
+      format.csv {send_data ReportGenerator.unique_visitors_report, filename: "unique_visits-#{Date.today}.csv"}
     end
   end
 
@@ -32,7 +32,7 @@ class Admin::ReportGeneratorController < AdminAreaController
   def faculty_frequency
     respond_to do |format|
       format.html
-      format.csv {send_data ReportGenerator.faculty_frequency_report}
+      format.csv {send_data ReportGenerator.faculty_frequency_report, filename: "faculty_frequency-#{Date.today}.csv"}
     end
   end
 
@@ -40,7 +40,7 @@ class Admin::ReportGeneratorController < AdminAreaController
   def gender_frequency
     respond_to do |format|
       format.html
-      format.csv {send_data ReportGenerator.gender_frequesncy_report}
+      format.csv {send_data ReportGenerator.gender_frequesncy_report, filename: "gender_frequency-#{Date.today}.csv"}
     end
   end
 
