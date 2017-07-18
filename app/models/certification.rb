bac252addfa5441f2895ecf4a3d87c786101052e
Karim Chukfeh <karimchukfeh@gmail.com>
2017-07-18 13:48:33 -0400

    validate through the Certification model

--- app/models/certification.rb
+++ app/models/certification.rb
@@ -1,3 +1,15 @@
 class Certification < ActiveRecord::Base
   belongs_to :user
+
+  # validate unique_cert
+  #
+  # def unique_cert
+  #   @user_certs = self.user.certifications
+  #   @user_certs.each do |cert|
+  #     if cert.training == self.training
+  #       errors.add(:string, "certification already exists")
+  #     end
+  #   end
+  # end
+
 end
