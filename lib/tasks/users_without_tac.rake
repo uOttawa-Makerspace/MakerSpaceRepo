12e3a8a731e1307976ca462f0045e3bce4d19995
Karim Chukfeh <karimchukfeh@gmail.com>
2017-07-19 12:46:30 -0400

    added test for rake task

--- lib/tasks/users_without_tac.rake
+++ lib/tasks/users_without_tac.rake
@@ -4,8 +4,10 @@ namespace :users_without_tac do
         that didn't sign the terms and conditions."
   task get_users: :environment do
     @users = User.unsigned_tac_users
+    $emails = []
     @users.each do |user|
-      puts user['username'] + ", " + user['email']
+      #puts user['username'] + ", " + user['email']
+      $emails << user['email']
     end
   end
 
--- test/lib/users_without_tac_test.rb
+++ test/lib/users_without_tac_test.rb
@@ -1,16 +1,17 @@
 # test/lib/users_without_tac.rb
 require 'test_helper'
 require 'rake'
+
 load File.join(Rails.root, 'lib', 'tasks', 'users_without_tac.rake')
 
 class UsersWithoutTacTest < ActiveSupport::TestCase
   setup do
-    MakerSpaceRepo::Application.load_tasks
-    Rake::Task['users_without_tac:get_users'].invoke
+    @tasks = MakerSpaceRepo::Application.load_tasks
+    @task = Rake::Task['users_without_tac:get_users'].invoke
   end
 
   test "Only Sara is caught" do
-      #I dont know how to test this
+      assert $emails.include? "sara@sara.com"
   end
 
 end
