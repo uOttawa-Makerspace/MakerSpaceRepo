30ada7d31d896e8516a1f07f78afe9e5e6bc424f
Karim Chukfeh <karimchukfeh@gmail.com>
2017-07-24 19:02:02 -0400

    add validation to course options

--- app/models/course_option.rb
+++ app/models/course_option.rb
@@ -1,2 +1,5 @@
 class CourseOption < ActiveRecord::Base
+  validates :title, presence: true, uniqueness: true
+  validates :code, presence: true, uniqueness: true
+  scope :show_options, -> { order("lower(code) ASC").all }
 end
