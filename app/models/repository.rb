e26be5aee5292c27159b3e370c9f4d8a302366d8
Parastoo <psaha091@uottawa.ca>
2017-07-24 15:35:02 -0400

    Added the authenticate method and conditions for checking share_type

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,4 +1,6 @@
 class Repository < ActiveRecord::Base
+  include BCrypt
+
   belongs_to :user
   has_many   :users
   has_many   :photos,   dependent: :destroy
@@ -29,6 +31,9 @@ class Repository < ActiveRecord::Base
   validates :share_type,
     presence: { message: "Is your project public or private?" }
 
+  validates :password
+    presence: { message: "Password is required for private projects" } if private?
+    
   before_save do
     self.slug = self.title.downcase.gsub(/[^0-9a-z ]/i, '').gsub(/\s+/, '-')
   end
@@ -37,6 +42,18 @@ class Repository < ActiveRecord::Base
     self.user.decrement!(:reputation, 25)
   end
 
+  def private?
+    self.share_type.eql?("private")
+  end
+
+  def self.authenticate(slug, pass)
+    @repository = Repository.find_by_slug(slug)
+    return @repository.password == pass
+  end
+
+  # def pword
+  #   @pword ||= Password.new(password)
+  # end
 
   # validates :category,
   #   inclusion: { within: category_options },
