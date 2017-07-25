7445e9d0b3f10b12492940bad5991ef667eb080e
Parastoo <psaha091@uottawa.ca>
2017-07-25 14:28:01 -0400

    Filtered the private repos so they don't show up on explore and search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,13 +4,13 @@ class SearchController < SessionsController
   require 'will_paginate/array'
 
   def explore
-    @repositories = Repository.paginate(:per_page=>12,:page=>params[:page]).order([sort_order].to_h).page params[:page]
+    @repositories = Repository.paginate(:per_page=>12,:page=>params[:page]).where(share_type: "public").order([sort_order].to_h).page params[:page]
     @photos = photo_hash
   end
 
   def search
   	sort_arr = sort_order
-  	@repositories = Repository.paginate(:per_page=>12,:page=>params[:page]).where("lower(title) LIKE ?
+  	@repositories = Repository.paginate(:per_page=>12,:page=>params[:page]).where(share_type: "public").where("lower(title) LIKE ?
                                                 OR lower(description) LIKE ?
                                                 OR lower(user_username) LIKE ?
                                                 OR lower(category) LIKE ?",
