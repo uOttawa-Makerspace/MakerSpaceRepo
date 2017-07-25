61655ecad7829ae3fc49f3f2073bb800dcf67a30
Parastoo <psaha091@uottawa.ca>
2017-07-25 09:40:04 -0400

    Resolved conflicts

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
+
 ActiveRecord::Schema.define(version: 20170714165821) do
-=======
-ActiveRecord::Schema.define(version: 20170718195705) do
->>>>>>> Schema was updated
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
