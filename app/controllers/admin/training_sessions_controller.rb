4b3f56077ccbe2b0f5cafb0a264edebab0961d6b
Karim Chukfeh <karimchukfeh@gmail.com>
2017-07-25 10:33:54 -0400

    add action to controller

--- app/controllers/admin/training_sessions_controller.rb
+++ app/controllers/admin/training_sessions_controller.rb
@@ -1,6 +1,22 @@
 class Admin::TrainingSessionsController < ApplicationController
+  before_action :target_training_session, only: [:update]
+  before_action :training_session_params, only: [:update]
+
+  layout 'admin_area'
 
   def update
+    @training_session.update(training_session_params)
   end
+
+
+  private
+
+    def target_training_session
+      @training_session = TrainingSession.find(params[:id])
+    end
+
+    def training_session_params
+      params.require(:training_session_params).permit(:training_id, :user_id, :course, :users)
+    end
 
 end
