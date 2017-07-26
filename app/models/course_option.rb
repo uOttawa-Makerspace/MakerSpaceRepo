f5bdfd44cab50f2e4c2d28e000dac9ce22b7f1e3
Karim Chukfeh <karimchukfeh@gmail.com>
2017-07-26 14:20:42 -0400

    get rid of CourseOption model

--- app/models/course_option.rb
+++
@@ -1,5 +0,0 @@
-class CourseOption < ActiveRecord::Base
-  validates :title, presence: true, uniqueness: true
-  validates :code, presence: true, uniqueness: true
-  scope :show_options, -> { order("lower(code) ASC").all }
-end
