0f996befc8985cd9fd9b311e4832c086553dc02f
Karim Chukfeh <karimchukfeh@gmail.com>
2017-07-31 10:15:10 -0400

    add admin training session test

--- test/controllers/admin/training_sessions_controller_test.rb
+++ test/controllers/admin/training_sessions_controller_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class Admin::TrainingSessionsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  setup do
+    session[:user_id] = User.find_by(username: "adam").id
+    session[:expires_at] = "Sat, 03 Jun 2025 05:01:41 UTC +00:00"
+    @user = User.find_by(username: "adam")
+    @request.env['HTTP_REFERER'] = admin_training_sessions_url
+  end
+
+  test "admin can update training session user_id (trainer)" do
+    patch :update, id: 1, training_session: {user_id: 1337}
+    assert_redirected_to :back
+    assert_equal flash[:notice], "Updated Successfuly"
+
+  end
+
 end
