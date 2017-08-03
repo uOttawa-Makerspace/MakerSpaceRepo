3ef8ca7b8167de53226cf78a4d2a2981a9fa600a
Parastoo <psaha091@uottawa.ca>
2017-08-03 16:12:20 -0400

    Testing that staff can view the private repo

--- test/controllers/repositories_controller_test.rb
+++ test/controllers/repositories_controller_test.rb
@@ -80,6 +80,14 @@ class RepositoriesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "staff have access to private repositories" do
+    session[:user_id] = User.find_by(username: "olivia").id
+    session[:expires_at] = "Sat, 03 Jun 2020 05:01:41 UTC +00:00"
+
+    get :show, user_username: "mary", slug: "repository3"
+    assert_response :success
+  end
+
   test "after entering the password, a regular user can access the private repository" do
     session[:user_id] = User.find_by(username: "adam").id
     session[:expires_at] = "Sat, 03 Jun 2020 05:01:41 UTC +00:00"
