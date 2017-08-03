514528a06f1aa33293f519746464aa0c05d19a32
Parastoo <psaha091@uottawa.ca>
2017-08-03 14:22:51 -0400

    Added to_csv method

--- app/models/certification.rb
+++ app/models/certification.rb
@@ -13,4 +13,14 @@ class Certification < ActiveRecord::Base
     return self.training_session.user.name
   end
 
+  def self.to_csv (attributes)
+    CSV.generate do |csv|
+      attributes.each do |row|
+        csv << row
+      end
+    end
+  end
+
+  scope :in_last_month, -> { where('created_at BETWEEN ? AND ? ', 1.month.ago.beginning_of_month , 1.month.ago.end_of_month) }
+
 end
