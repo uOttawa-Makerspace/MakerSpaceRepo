843d8e1d5fc6a4e29ac662b5dee321fd259f5f07
Karim Chukfeh <karimchukfeh@gmail.com>
2017-08-08 12:26:13 -0400

    fixtures and associations

--- app/models/lab_session.rb
+++ app/models/lab_session.rb
@@ -1,6 +1,7 @@
 class LabSession < ActiveRecord::Base
   belongs_to :user
-  belongs_to :space
+  belongs_to :pi_reader
+  has_one :space, through: :pi_reader
 
   scope :in_last_month, -> { where('sign_in_time BETWEEN ? AND ? ', 1.month.ago.beginning_of_month , 1.month.ago.end_of_month) }
 
--- app/models/space.rb
+++ app/models/space.rb
@@ -1,6 +1,6 @@
 class Space < ActiveRecord::Base
-  has_many :lab_sessions
   has_many :pi_readers
+  has_many :lab_sessions, through: :pi_readers
   has_many :trainings
 
   validates :name,  presence: { message: "A name is required for the space"}, uniqueness: { message: "Space already exists"}
