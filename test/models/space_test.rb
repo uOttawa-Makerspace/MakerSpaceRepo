bf9c1dea6dfc7a863c0606349d37b99b6d6140bb
Karim Chukfeh <karimchukfeh@gmail.com>
2017-08-08 13:40:44 -0400

    added space_test.rb

--- test/models/space_test.rb
+++ test/models/space_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class SpaceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "User->LabSession->PiReader->Space association" do
+    user = users(:sara)
+
+    refute spaces(:brunsfield).users.include? user
+
+    LabSession.create(user_id: user.id, pi_reader_id: 2)
+    assert spaces(:brunsfield).users.include? user
+  end
+
+  test "Sign in" do
+    mary_session = lab_sessions(:four)
+
+    assert mary_session.sign_out_time.equal? nil #nil means she's signed in
+    assert mary_session.space.signed_in_users.include? users(:mary)
+  end
+
+  test "Sign out" do
+    mary_session = lab_sessions(:two)
+
+    refute mary_session.sign_out_time.equal? nil #not nil means she's signed out
+    refute mary_session.space.signed_in_users.include? users(:mary)
+  end
+
 end
