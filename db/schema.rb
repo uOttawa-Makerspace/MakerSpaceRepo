6e6c45d883ea501b25cc3f2eb817494485974ae0
Karim Chukfeh <karimchukfeh@gmail.com>
2017-08-08 11:38:56 -0400

    dump schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170803234757) do
+ActiveRecord::Schema.define(version: 20170717220935) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -152,12 +152,6 @@ ActiveRecord::Schema.define(version: 20170803234757) do
   end
 
   add_index "rfids", ["user_id"], name: "index_rfids_on_user_id", using: :btree
-
-  create_table "spaces", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "training_sessions", force: :cascade do |t|
     t.integer  "training_id"
