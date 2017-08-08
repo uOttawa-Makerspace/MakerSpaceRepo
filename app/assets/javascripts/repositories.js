9426f6320d3ba5954d404cfd8ccb388341a7af99
Parastoo <psaha091@uottawa.ca>
2017-08-08 09:44:32 -0400

    Toggling is not working properly for changing password of private repos

--- app/assets/javascripts/repositories.js
+++ app/assets/javascripts/repositories.js
@@ -1,11 +1,22 @@
 $(document).ready(function() {
-	$("#hidden").hide();
+	$("#pass").hide();
 	$(".show_extra").change(function() {
 		if(this.value === "private") {
-			$("#hidden").show();
+			$("#pass").show();
 		}
 		else {
-			$("#hidden").hide();
+			$("#pass").hide();
+			$("#change").hide();
 		}
 	});
+});
+
+
+$(document).ready( function() {
+	$("#pass").hide();
+	// $("#repo_pass").hide();
+	$("#change").on('change', function() {
+	  $('#pass').toggle(this.checked);
+	}).change();
+
 });
