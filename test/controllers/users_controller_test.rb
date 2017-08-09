4c704d22ee72a24cbeac555546a35236021284cc
Parastoo <psaha091@uottawa.ca>
2017-08-09 12:03:51 -0400

    Added update user tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -54,9 +54,9 @@ class UsersControllerTest < ActionController::TestCase
   test "user should be able to update profile with patch" do
     session[:user_id] = User.find_by(username: "bob").id
     session[:expires_at] = "Sat, 03 Jun 2020 05:01:41 UTC +00:00"
-    patch :update, username: "bob", user: {gender: "female"} #from male
+    patch :update, username: "bob", user: {gender: "Female"} #from male
     assert_equal 'Profile updated successfully.', flash[:notice]
-    assert_equal "female", User.find_by(username: "bob").gender
+    assert_equal "Female", User.find_by(username: "bob").gender
     assert_redirected_to settings_profile_path
   end
 
@@ -97,6 +97,43 @@ class UsersControllerTest < ActionController::TestCase
     assert_nil flash.now[:alert]
     assert_equal @oldpass, @newpass
     assert_response :ok
+  end
+
+  test "old valid user should be able to update profile" do
+    session[:user_id] = User.find_by(username: "mary").id
+    session[:expires_at] = "Sat, 03 Jun 2020 05:01:41 UTC +00:00"
+
+    patch :update, username: "mary", user: {name: "Mary", gender: "Female", identity: "community_member"}
+    assert_equal 'Profile updated successfully.', flash[:notice]
+    assert_equal "Mary", User.find_by(username: "mary").name
+    assert_equal "Female", User.find_by(username: "mary").gender
+    assert_equal "community_member", User.find_by(username: "mary").identity
+    assert_redirected_to settings_profile_path
+  end
+
+  test "Profile cannot be updated if input is invalid" do
+    session[:user_id] = User.find_by(username: "john").id
+    session[:expires_at] = "Sat, 03 Jun 2020 05:01:41 UTC +00:00"
+
+    patch :update, username: "john", user: {name: "John", gender: "Male", identity: "grad"}
+    assert_equal "Could not save changes.", flash[:alert]
+    assert_redirected_to settings_profile_path
+  end
+
+  test "old valid user should be able to update profile with valid inputs" do
+    session[:user_id] = User.find_by(username: "john").id
+    session[:expires_at] = "Sat, 03 Jun 2020 05:01:41 UTC +00:00"
+
+    patch :update, username: "john", user: {name: "John", gender: "Male", identity: "grad", student_id: 9876543, faculty: "arts", program: "Honours BA in English", year_of_study: "3"}
+    assert_equal 'Profile updated successfully.', flash[:notice]
+    assert_equal "John", User.find_by(username: "john").name
+    assert_equal "Male", User.find_by(username: "john").gender
+    assert_equal "grad", User.find_by(username: "john").identity
+    assert_equal 9876543, User.find_by(username: "john").student_id
+    assert_equal "arts", User.find_by(username: "john").faculty
+    assert_equal "Honours BA in English", User.find_by(username: "john").program
+    assert_equal "3", User.find_by(username: "john").year_of_study
+    assert_redirected_to settings_profile_path
   end
 
 end
