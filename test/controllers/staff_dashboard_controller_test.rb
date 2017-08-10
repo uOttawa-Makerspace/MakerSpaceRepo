135f8e79f77a4c535ea6eaccd50fb2354915d747
Karim Chukfeh <karimchukfeh@gmail.com>
2017-08-10 10:46:24 -0400

    update staff dashboard controller test

--- test/controllers/staff_dashboard_controller_test.rb
+++ test/controllers/staff_dashboard_controller_test.rb
@@ -2,18 +2,52 @@ require 'test_helper'
 
 class StaffDashboardControllerTest < ActionController::TestCase
 
+  setup do
+    session[:user_id] = User.find_by(username: "adam").id
+    session[:expires_at] = "Sat, 03 Jun 2025 05:01:41 UTC +00:00"
+  end
+
    test "staff succeed at loading Staff Dashboard" do
-     session[:user_id] = users(:olivia).id
-     session[:expires_at] = "Sat, 03 Jun 2025 05:01:41 UTC +00:00"
      get :index
+
      assert_response :success
    end
 
    test "regular users are redirected to home" do
      session[:user_id] = users(:bob).id
      session[:expires_at] = "Sat, 03 Jun 2025 05:01:41 UTC +00:00"
+
      get :index
+
      assert_redirected_to root_path
    end
+
+  #  test "a space is chosen by default" do
+  #    get :index
+   #
+  #    assert_response :success
+  #    assert response.body.include? 'brunsfield'
+  #  end
+   #
+  #  test "a space can passed in params" do
+  #    get :index, space_name: 'makerspace'
+   #
+  #    assert_response :success
+  #    assert response.body.include? 'makerspace'
+  #    refute response.body.include? 'brunsfield'
+  #  end
+   #
+  #  test "space doesn't go back to default on its own" do
+  #    get :index, space_name: 'makerspace'
+  #    assert_response :success
+  #    assert response.body.include? 'makerspace'
+  #    refute response.body.include? 'brunsfield'
+   #
+  #    get :index
+  #    assert_response :success
+  #    assert response.body.include? 'makerspace'
+  #    refute response.body.include? 'brunsfield'
+  #  end
+
 
 end
