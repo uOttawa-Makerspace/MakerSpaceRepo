be91d9048313a858e89d51177cbc1159e14ce972
Karim Chukfeh <karimchukfeh@gmail.com>
2017-08-10 12:52:55 -0400

    add migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20170717220935) do
+ActiveRecord::Schema.define(version: 20170810164929) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -129,8 +128,8 @@ ActiveRecord::Schema.define(version: 20170717220935) do
     t.integer  "user_id"
     t.string   "title"
     t.string   "description"
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
     t.string   "category"
     t.string   "license"
     t.string   "github"
@@ -140,6 +139,7 @@ ActiveRecord::Schema.define(version: 20170717220935) do
     t.integer  "make_id"
     t.integer  "make",          default: 0
     t.string   "slug"
+    t.boolean  "pinned",        default: false
   end
 
   add_index "repositories", ["user_id"], name: "index_repositories_on_user_id", using: :btree
