8411a8a0360184d528a8e1e577407eb3301fb817
Karim Chukfeh <karimchukfeh@gmail.com>
2017-08-10 10:42:45 -0400

    add before_action to controller

--- app/controllers/staff_dashboard_controller.rb
+++ app/controllers/staff_dashboard_controller.rb
@@ -1,6 +1,21 @@
 class StaffDashboardController < StaffAreaController
+  #before_action :default_space
 
   def index
   end
+
+  # private
+  #
+  # def default_space
+  #   if @space.equal? nil
+  #     if params['space_name'].present?
+  #       @space = Space.find_by(name: params['space_name'])
+  #     else
+  #       @space = current_user.lab_sessions.last.space
+  #     end
+  #   else
+  #     @space
+  #   end
+  # end
 
 end
