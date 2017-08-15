e4a5f2c90005e50842f85dddac4b64fc43d635ad
Parastoo <psaha091@uottawa.ca>
2017-08-15 15:13:59 -0400

    repository_report + picking date range

--- app/controllers/admin/report_generator_controller.rb
+++ app/controllers/admin/report_generator_controller.rb
@@ -1,7 +1,15 @@
 class Admin::ReportGeneratorController < AdminAreaController
   layout 'admin_area'
+  require 'date'
 
   def index
+    if session[:selected_dates] == nil
+      @start_date = 1.month.ago
+      @end_date = DateTime.current
+    else
+      @start_date = DateTime.parse(selected_dates[0])
+      @end_date = DateTime.parse(selected_dates[1])
+    end
   end
 
 # New users/month
@@ -49,5 +57,37 @@ class Admin::ReportGeneratorController < AdminAreaController
       format.html
       format.csv {send_data ReportGenerator.training_report, filename: "trainings-#{Date.today}.csv"}
     end
+  end
+
+  def select_date_range
+    session[:selected_dates] = nil
+    @start_date1 = Date::civil(params[:start][:year].to_i, params[:start][:month].to_i,
+                      params[:start][:day].to_i)
+    @start_date = Time.zone.at(@start_date1.to_time).to_datetime
+
+    @end_date1 = Date::civil(params[:end][:year].to_i, params[:end][:month].to_i,
+                      params[:end][:day].to_i)
+    @end_date = Time.zone.at(@end_date1.to_time).to_datetime
+
+    selected_dates << @start_date << @end_date
+    redirect_to :back
+
+  end
+
+  # repositories based on selected dates
+  def repository
+    if session[:selected_dates] == nil
+      @start_date = 1.month.ago
+      @end_date = DateTime.current
+    else
+      @start_date = DateTime.parse(selected_dates[0])
+      @end_date = DateTime.parse(selected_dates[1])
+    end
+
+    respond_to do |format|
+      format.html
+      format.csv {send_data ReportGenerator.project_report(@start_date , @end_date) , filename: "repositories-#{Date.today}.csv"}
+    end
+    session[:selected_dates] = nil
   end
 end
