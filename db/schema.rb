d51ea385ef5f970f1f736f4ce2a831b9ba381a28
Karim Chukfeh <karimchukfeh@gmail.com>
2017-08-17 09:26:51 -0400

    fix schema after mergin

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20170717220935) do
+ActiveRecord::Schema.define(version: 20170808162101) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
