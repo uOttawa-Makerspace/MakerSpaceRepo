d5efa2fd1e4c242c99d13a629e0912e9d8b14fd4
Karim Chukfeh <karimchukfeh@gmail.com>
2017-08-17 11:33:07 -0400

    updated def signed_in_users to avoid using nil values

--- app/models/space.rb
+++ app/models/space.rb
@@ -7,7 +7,7 @@ class Space < ActiveRecord::Base
   validates :name,  presence: { message: "A name is required for the space"}, uniqueness: { message: "Space already exists"}
 
   def signed_in_users
-    return self.lab_sessions.where(sign_out_time: nil).map(&:user)
+    return self.lab_sessions.where("sign_out_time > ?", Time.now).map(&:user)
   end
 
 end
