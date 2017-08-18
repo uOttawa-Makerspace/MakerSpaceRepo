8906faaaf3a5f6d7fa7385d3a869b4c6e5e854c4
Karim Chukfeh <karimchukfeh@gmail.com>
2017-08-18 12:37:09 -0400

    add space has_many training_sessions through: :trainings

--- app/models/training_session.rb
+++ app/models/training_session.rb
@@ -1,8 +1,11 @@
 class TrainingSession < ActiveRecord::Base
+
   belongs_to :training
-  belongs_to :user
-  has_and_belongs_to_many :users, :uniq => true
+  belongs_to :user #the staff/admin
+  has_and_belongs_to_many :users, :uniq => true #the trainees
+
   has_many :certifications, dependent: :destroy
+  has_one :space, through: :training
 
   validates :training, presence: { message: "A training subject is required"}
   validates :user, presence: { message: "A trainer is required"}
