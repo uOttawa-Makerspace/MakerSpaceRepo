b3e25b9619f27bb0e09e11c4b1e8fc192bb9dc35
Karim Chukfeh <karimchukfeh@gmail.com>
2017-08-18 12:25:20 -0400

    migrationg adding space_id to trainings + model validations

--- app/models/training.rb
+++ app/models/training.rb
@@ -1,4 +1,7 @@
 class Training < ActiveRecord::Base
+  belongs_to :space
   has_many :training_session, dependent: :destroy
+
   validates :name, presence: true, uniqueness: true
+  validates :space_id, presence: true
 end
