7765cb45a7d2e1fc5edd18ca4e5301c32d0f99ea
Karim Chukfeh <karimchukfeh@gmail.com>
2017-08-17 21:04:27 -0400

    delete the Course-relate Projects category

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170810160454) do
+ActiveRecord::Schema.define(version: 20170818004747) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -128,8 +128,8 @@ ActiveRecord::Schema.define(version: 20170810160454) do
     t.integer  "user_id"
     t.string   "title"
     t.string   "description"
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
     t.string   "category"
     t.string   "license"
     t.string   "github"
@@ -139,6 +139,7 @@ ActiveRecord::Schema.define(version: 20170810160454) do
     t.integer  "make_id"
     t.integer  "make",          default: 0
     t.string   "slug"
+    t.boolean  "featured",      default: false
   end
 
   add_index "repositories", ["user_id"], name: "index_repositories_on_user_id", using: :btree
