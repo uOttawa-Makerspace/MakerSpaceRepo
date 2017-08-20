5a62454dac93c1039dd6d8872d99d338e30c333d
Parastoo <psaha091@uottawa.ca>
2017-08-19 22:56:32 -0400

    Reports can be generated based on dates selected

--- app/controllers/admin/report_generator_controller.rb
+++ app/controllers/admin/report_generator_controller.rb
@@ -12,53 +12,6 @@ class Admin::ReportGeneratorController < AdminAreaController
     end
   end
 
-# New users/month
-  def new_users
-  	respond_to do |format|
-  		format.html
-  		format.csv {send_data ReportGenerator.new_user_report, filename: "new_users-#{Date.today}.csv" }
-    end
-  end
-
-# Visitors/month
-  def total_visits
-    respond_to do |format|
-      format.html
-      format.csv {send_data ReportGenerator.lab_session_report, filename: "total_visits-#{Date.today}.csv" }
-    end
-  end
-
-# Unique visitors/ month
-  def unique_visits
-    respond_to do |format|
-      format.html
-      format.csv {send_data ReportGenerator.unique_visitors_report, filename: "unique_visits-#{Date.today}.csv"}
-    end
-  end
-
-# Diversity of users based on faculty
-  def faculty_frequency
-    respond_to do |format|
-      format.html
-      format.csv {send_data ReportGenerator.faculty_frequency_report, filename: "faculty_frequency-#{Date.today}.csv"}
-    end
-  end
-
-# Diversity of users based on gender
-  def gender_frequency
-    respond_to do |format|
-      format.html
-      format.csv {send_data ReportGenerator.gender_frequesncy_report, filename: "gender_frequency-#{Date.today}.csv"}
-    end
-  end
-
-  def training
-    respond_to do |format|
-      format.html
-      format.csv {send_data ReportGenerator.training_report, filename: "trainings-#{Date.today}.csv"}
-    end
-  end
-
   def select_date_range
     session[:selected_dates] = nil
     @start = Date::civil(params[:start][:year].to_i, params[:start][:month].to_i,
@@ -73,7 +26,102 @@ class Admin::ReportGeneratorController < AdminAreaController
     redirect_to :back
   end
 
-  # repositories based on selected dates
+# New users
+  def new_users
+    if session[:selected_dates] == nil
+      @start_date = 1.month.ago
+      @end_date = DateTime.current
+    else
+      @start_date = DateTime.parse(selected_dates[0])
+      @end_date = DateTime.parse(selected_dates[1])
+    end
+
+  	respond_to do |format|
+  		format.html
+  		format.csv {send_data ReportGenerator.new_user_report(@start_date , @end_date), filename: "new_users-#{Date.today}.csv" }
+    end
+  end
+
+# Visitors
+  def total_visits
+    if session[:selected_dates] == nil
+      @start_date = 1.month.ago
+      @end_date = DateTime.current
+    else
+      @start_date = DateTime.parse(selected_dates[0])
+      @end_date = DateTime.parse(selected_dates[1])
+    end
+
+    respond_to do |format|
+      format.html
+      format.csv {send_data ReportGenerator.lab_session_report(@start_date , @end_date), filename: "total_visits-#{Date.today}.csv" }
+    end
+  end
+
+# Unique visitor
+  def unique_visits
+    if session[:selected_dates] == nil
+      @start_date = 1.month.ago
+      @end_date = DateTime.current
+    else
+      @start_date = DateTime.parse(selected_dates[0])
+      @end_date = DateTime.parse(selected_dates[1])
+    end
+
+    respond_to do |format|
+      format.html
+      format.csv {send_data ReportGenerator.unique_visitors_report(@start_date , @end_date), filename: "unique_visits-#{Date.today}.csv"}
+    end
+  end
+
+# Diversity of users based on faculty
+  def faculty_frequency
+    if session[:selected_dates] == nil
+      @start_date = 1.month.ago
+      @end_date = DateTime.current
+    else
+      @start_date = DateTime.parse(selected_dates[0])
+      @end_date = DateTime.parse(selected_dates[1])
+    end
+
+    respond_to do |format|
+      format.html
+      format.csv {send_data ReportGenerator.faculty_frequency_report(@start_date , @end_date), filename: "faculty_frequency-#{Date.today}.csv"}
+    end
+  end
+
+# Diversity of users based on gender
+  def gender_frequency
+    if session[:selected_dates] == nil
+      @start_date = 1.month.ago
+      @end_date = DateTime.current
+    else
+      @start_date = DateTime.parse(selected_dates[0])
+      @end_date = DateTime.parse(selected_dates[1])
+    end
+
+    respond_to do |format|
+      format.html
+      format.csv {send_data ReportGenerator.gender_frequesncy_report(@start_date , @end_date), filename: "gender_frequency-#{Date.today}.csv"}
+    end
+  end
+
+  def training
+    if session[:selected_dates] == nil
+      @start_date = 1.month.ago
+      @end_date = DateTime.current
+    else
+      @start_date = DateTime.parse(selected_dates[0])
+      @end_date = DateTime.parse(selected_dates[1])
+    end
+
+    respond_to do |format|
+      format.html
+      format.csv {send_data ReportGenerator.training_report(@start_date , @end_date), filename: "trainings-#{Date.today}.csv"}
+    end
+  end
+
+  # repositories
   def repository
     if session[:selected_dates] == nil
       @start_date = 1.month.ago
@@ -87,6 +135,5 @@ class Admin::ReportGeneratorController < AdminAreaController
       format.html
       format.csv {send_data ReportGenerator.project_report(@start_date , @end_date) , filename: "repositories-#{Date.today}.csv"}
     end
-    session[:selected_dates] = nil
   end
 end
