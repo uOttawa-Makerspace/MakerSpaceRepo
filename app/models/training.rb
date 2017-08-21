c85ce16e2441d48938001432ed7fe8f7cb10336e
Karim Chukfeh <karimchukfeh@gmail.com>
2017-08-21 18:20:50 -0400

    add association

--- app/models/training.rb
+++ app/models/training.rb
@@ -1,5 +1,8 @@
 class Training < ActiveRecord::Base
   belongs_to :space
-  has_many :training_session, dependent: :destroy
+  has_many :training_session
+
   validates :name, presence: true, uniqueness: true
+  validates :space_id, presence: true
+
 end
