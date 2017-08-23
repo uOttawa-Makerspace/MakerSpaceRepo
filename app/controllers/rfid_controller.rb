2892fe657de8fc85ed14b6193a9bef1c1bf78d24
Karim Chukfeh <karimchukfeh@gmail.com>
2017-08-22 20:13:12 -0400

    update rfid controller and controller test to expect mac_address from our nfc readers

--- app/controllers/rfid_controller.rb
+++ app/controllers/rfid_controller.rb
@@ -1,12 +1,12 @@
 class RfidController < SessionsController
 
   def card_number
-    
+
     if !(PiReader.find_by(pi_mac_address: params[:mac_address]))
       new_reader = PiReader.new(pi_mac_address: params[:mac_address])
       new_reader.save
     end
-    
+
     rfid = Rfid.find_by(card_number: params[:rfid])
 
     if rfid
@@ -26,7 +26,7 @@ class RfidController < SessionsController
       end
     end
   end
-  
+
   def check_session(rfid)
     active_session = rfid.user.lab_sessions.where("sign_out_time > ?", Time.now)
     if active_session.present?
@@ -35,16 +35,17 @@ class RfidController < SessionsController
       new_location = PiReader.find_by(pi_mac_address: params[:mac_address]).try(:pi_location)
       if active_location != new_location
         new_session(rfid)
-      end   
+      end
     else
       new_session(rfid)
     end
   end
-  
+
   def new_session (rfid)
     sign_in = Time.now
     sign_out = sign_in + 3.hours
-    new_session = rfid.user.lab_sessions.new(sign_in_time: sign_in, sign_out_time: sign_out, mac_address: params[:mac_address])
+    raspi = PiReader.find_by(pi_mac_address: params[:mac_address]).try(params[:pi_location])
+    new_session = rfid.user.lab_sessions.new(sign_in_time: sign_in, sign_out_time: sign_out, mac_address: params[:mac_address], pi_reader_id: raspi.id)
     new_session.save
   end
 end
--- test/controllers/rfid_controller_test.rb
+++ test/controllers/rfid_controller_test.rb
@@ -15,7 +15,7 @@ class RfidControllerTest < ActionController::TestCase
   end
 
   test "posting new rfid returns unprocessable_entity" do
-    post :card_number, rfid: "completly new"
+    post :card_number, rfid: "completly new", mac_address: "m4k3rsp4c3-pi-1"
 
     assert_response :unprocessable_entity
   end
@@ -24,24 +24,24 @@ class RfidControllerTest < ActionController::TestCase
     rfid = rfids(:old)
 
     assert_no_difference('Rfid.count') do
-      post :card_number, rfid: rfid.card_number
+      post :card_number, rfid: rfid.card_number, mac_address: "m4k3rsp4c3-pi-1"
     end
   end
 
   test "posting existing card with no user updates updated_at" do
-    rfid = rfids(:old)
+    rfid = rfids(:no_user)
     old_timestamp = rfid.updated_at.to_i
 
-    post :card_number, rfid: rfid.card_number
+    post :card_number, rfid: rfid.card_number, mac_address: "8runsf13ld-pi-1"
 
     rfid.reload
     assert_not_equal old_timestamp, rfid.updated_at.to_i
   end
 
   test "posting existing card with no user returns unprocessable_entity" do
-    rfid = rfids(:old)
+    rfid = rfids(:no_user)
 
-    post :card_number, rfid: rfid.card_number
+    post :card_number, rfid: rfid.card_number, mac_address: "8runsf13ld-pi-1"
 
     assert_response :unprocessable_entity
   end
@@ -49,7 +49,7 @@ class RfidControllerTest < ActionController::TestCase
   test "posting existing card with user returns ok" do
     rfid = rfids(:assigned)
 
-    post :card_number, rfid: rfid.card_number
+    post :card_number, rfid: rfid.card_number, mac_address: "m4k3rsp4c3-pi-1"
 
     assert_response :ok
   end
