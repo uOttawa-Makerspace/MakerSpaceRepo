5a4d7e55ea0b5b560e32a16c8fd20c51858ee70c
Parastoo <psaha091@uottawa.ca>
2017-08-24 10:34:54 -0400

    Photo Validation only on create

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -25,7 +25,7 @@ class Repository < ActiveRecord::Base
     presence:   { message: "Project title is required."},
     uniqueness: { message: "Project title is already in use.", scope: :user_username}
 
-  # validates :photos, :length => { :minimum => 1 }
+  validates :photos, :length => { :minimum => 1 }, on: :create
 
   before_save do
     self.slug = self.title.downcase.gsub(/[^0-9a-z ]/i, '').gsub(/\s+/, '-')
