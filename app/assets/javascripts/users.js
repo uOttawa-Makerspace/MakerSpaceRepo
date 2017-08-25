1363e55e9cb3e628d8e3537087dbefe817694e22
Karim Chukfeh <karimchukfeh@gmail.com>
2017-08-25 14:55:37 -0400

    add to view

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -4,6 +4,10 @@ function showField(){
 function hideField(){
 	document.getElementById("hidden").style.display = 'none';
 }
+function showCreateButton(){
+	document.getElementById("fake-signup-button").style.display = 'none';
+	document.getElementById("signup-button").style.display = 'block';
+}
 
 $(document).ready(function() {
   $(".programs").select2({});
