281c452cee72f01b58291e4539636e450202346f
Karim Chukfeh <karimchukfeh@gmail.com>
2017-08-28 12:58:17 -0400

    adjsutments after pulling master

--- app/models/training.rb
+++ app/models/training.rb
@@ -1,9 +1,9 @@
 class Training < ActiveRecord::Base
   belongs_to :space
-  has_many :training_session, dependent: :destroy
+  has_many :training_sessions, dependent: :destroy
 
   validates :name, presence: true, uniqueness: true
   validates :space_id, presence: true
-  
+
   has_many :certifications, through: :training_sessions
 end
