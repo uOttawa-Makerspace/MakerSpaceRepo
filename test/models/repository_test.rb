cf6e56bce824ae09ef34a28a4d2655d8c2573fc8
Parastoo <psaha091@uottawa.ca>
2017-08-29 16:18:44 -0400

    Removed tests

--- test/models/repository_test.rb
+++ test/models/repository_test.rb
@@ -36,14 +36,4 @@ class RepositoryTest < ActiveSupport::TestCase
 
 	end
 
-	test "repository cannot be created without a photo" do
-
-		repo = Repository.create(:title => 'myRepo', :user_id => 1, :user_username => "bob", :id =>111 )
-		assert repo.invalid?, "invlid repo"
-
-		photo = photos(:three)
-		photo.repository_id = 111
-		repo.photos << photo
-		assert repo.valid?, "invlid repo"
-	end
 end
