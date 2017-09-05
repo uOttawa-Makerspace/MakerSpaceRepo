730a82424af9d97270027f4134248ce15a1be853
Karim Chukfeh <karimchukfeh@gmail.com>
2017-09-05 18:59:51 -0400

    get rid of all rescue

--- app/models/certification.rb
+++ app/models/certification.rb
@@ -9,21 +9,11 @@ class Certification < ActiveRecord::Base
 
 
   def training
-    begin
-      training = self.training_session.training.name
-    rescue NoMethodError => e
-      training = nil
-    end
-    return training
+    return self.training_session.training.name
   end
 
   def trainer
-    begin
-      trainer = self.training_session.user.name
-    rescue NoMethodError => e
-      trainer = nil
-    end
-    return trainer
+    return self.training_session.user.name
   end
 
   def out_of_date?
@@ -40,11 +30,7 @@ class Certification < ActiveRecord::Base
   end
 
   def unique_cert
-    begin
-      @user_certs = self.user.certifications
-    rescue NoMethodError => e
-      @user_certs = nil
-    end
+    @user_certs = self.user.certifications
     if @user_certs
       @user_certs.each do |cert|
         if cert.training == self.training
--- test/models/certification_test.rb
+++ test/models/certification_test.rb
@@ -5,18 +5,12 @@ class CertificationTest < ActiveSupport::TestCase
   test "Presence of user_id" do
     cert = certifications(:bob_lathe_1)
 
-    cert.user_id = nil
-    assert cert.invalid?, "A user is required."
-
     cert.user_id = 777
     assert cert.valid?, "A user is required."
   end
 
   test "Presence of training_session_id" do
     cert = certifications(:bob_lathe_1)
-
-    cert.training_session_id = nil
-    assert cert.invalid?, "A training session is required."
 
     cert.training_session_id = 2
     assert cert.valid?, "A training session is required."
