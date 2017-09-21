779b32877daa1b7f5d87e49629e7dfc30edb47ed
Karim Chukfeh <karimchukfeh@gmail.com>
2017-09-20 23:49:32 -0400

    change recently signed out users query

--- app/models/space.rb
+++ app/models/space.rb
@@ -14,9 +14,6 @@ class Space < ActiveRecord::Base
 
   def recently_signed_out_users
     users = self.lab_sessions.where("sign_out_time < ?", Time.zone.now).last(15).map(&:user).uniq
-    self.signed_in_users.each do |u|
-      users.delete(u)
-    end
     return users
   end
 
