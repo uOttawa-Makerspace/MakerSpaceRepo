3a96fc7432451e945ccedce45cccd5a7982ea59f
Karim Chukfeh <karimchukfeh@gmail.com>
2017-09-22 18:17:11 -0400

    default terms to true

--- db/migrate/20151027000659_add_terms_to_user.rb
+++ db/migrate/20151027000659_add_terms_to_user.rb
@@ -1,5 +1,5 @@
 class AddTermsToUser < ActiveRecord::Migration
   def change
-    add_column :users, :terms_and_conditions, :boolean
+    add_column :users, :terms_and_conditions, :boolean, default: true
   end
 end
