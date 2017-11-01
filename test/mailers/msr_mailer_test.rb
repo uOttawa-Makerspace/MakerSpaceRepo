3bb532821801aae8f59262c9e5f5c31610d3a24b
Parastoo <psaha091@uottawa.ca>
2017-10-31 20:44:08 -0400

    Added some tests

--- test/mailers/msr_mailer_test.rb
+++ test/mailers/msr_mailer_test.rb
@@ -34,26 +34,53 @@ class MsrMailerTest < ActionMailer::TestCase
 		assert email.body.to_s.include? "Password2"
 	end
 
-	test "Sending reports" do
-		email = MsrMailer.send_report('makerspace@uottawa.ca', 'hanis@uottawa.ca', ReportGenerator.new_user_report,
-						ReportGenerator.lab_session_report,
-						ReportGenerator.faculty_frequency_report, ReportGenerator.gender_frequency_report, ReportGenerator.unique_visitors_report)
 
-		assert_equal ['uottawa.makerepo@gmail.com'], email.from
-		assert_equal ['makerspace@uottawa.ca'], email.to
-		assert_equal ['hanis@uottawa.ca'], email.bcc
-		assert_equal 'Weekly Report', email.subject
-		assert_not_nil(email.attachments, "No attachments found")
-	end
+	# test "Sending reports" do
+	# 	email = MsrMailer.send_report('makerspace@uottawa.ca', 'hanis@uottawa.ca', ReportGenerator.new_user_report, ReportGenerator.lab_session_report,
+	# 					ReportGenerator.faculty_frequency_report, ReportGenerator.gender_frequency_report, ReportGenerator.unique_visitors_report)
+	#
+	# 	assert_equal ['uottawa.makerepo@gmail.com'], email.from
+	# 	assert_equal ['makerspace@uottawa.ca'], email.to
+	# 	assert_equal ['hanis@uottawa.ca'], email.bcc
+	# 	assert_equal 'Weekly Report', email.subject
+	# 	assert_not_nil(email.attachments, "No attachments found")
+	# end
 
 
 	test "Sending training reports" do
-		email = MsrMailer.send_training_report('brunsfield@uottawa.ca', 'MTC@uottawa.ca', 'makerspace@uottawa.ca', 'hanis@uottawa.ca',
-																						ReportGenerator.training_report())
+		email = MsrMailer.send_training_report('abc@gmail.com',ReportGenerator.makerspace_training_report, ReportGenerator.mtc_training_report)
 		assert_equal ['uottawa.makerepo@gmail.com'], email.from
-		assert_equal ['brunsfield@uottawa.ca'], email.to
-		assert_equal ['MTC@uottawa.ca', 'makerspace@uottawa.ca', 'hanis@uottawa.ca'], email.bcc
+		assert_equal ['abc@gmail.com'], email.to
 		assert_not_nil(email.attachments, "No attachments found")
+	end
+
+	test "Sending monthly reports" do
+		email = MsrMailer.send_monthly_report('abc@gmail.com', ReportGenerator.new_user_report(1.month.ago.beginning_of_month, 1.month.ago.end_of_month),
+		          ReportGenerator.unique_visitors_report(1.month.ago.beginning_of_month, 1.month.ago.end_of_month),
+		          ReportGenerator.lab_session_report(1.month.ago.beginning_of_month, 1.month.ago.end_of_month),
+		          ReportGenerator.faculty_frequency_report(1.month.ago.beginning_of_month, 1.month.ago.end_of_month),
+		          ReportGenerator.gender_frequency_report(1.month.ago.beginning_of_month, 1.month.ago.end_of_month),
+		          ReportGenerator.makerspace_training_report(1.month.ago.beginning_of_month, 1.month.ago.end_of_month),
+		          ReportGenerator.mtc_training_report(1.month.ago.beginning_of_month, 1.month.ago.end_of_month))
+
+		assert_equal ['uottawa.makerepo@gmail.com'], email.from
+		assert_equal ['abc@gmail.com'], email.to
+		assert_not_nil(email.attachments, "No attachments found")
+	end
+
+	test "Sending weekly reports" do
+		email = MsrMailer.send_weekly_report('abc@gmail.com', ReportGenerator.new_user_report,
+	          ReportGenerator.unique_visitors_report,
+	          ReportGenerator.lab_session_report,
+	          ReportGenerator.faculty_frequency_report,
+	          ReportGenerator.gender_frequency_report,
+	          ReportGenerator.makerspace_training_report,
+	          ReportGenerator.mtc_training_report)
+
+		assert_equal ['uottawa.makerepo@gmail.com'], email.from
+		assert_equal ['abc@gmail.com'], email.to
+		assert_not_nil(email.attachments, "No attachments found")
+
 	end
 
   test "Send waiver reminder email" do
@@ -61,7 +88,6 @@ class MsrMailerTest < ActionMailer::TestCase
 		assert_equal ['uottawa.makerepo@gmail.com'], email.from
 		assert_equal ['abc@123.com'], email.to
 		assert_equal 'Please Sign The Release Agreement!', email.subject
-
 	end
 
 	test "issue emails get sent successfully" do
@@ -72,6 +98,6 @@ class MsrMailerTest < ActionMailer::TestCase
 		assert email.body.to_s.include? "Julia"
 		assert email.body.to_s.include? "julia@gmail.com"
 		assert_equal 'Issue Report', email.subject
-
 	end
+
 end
