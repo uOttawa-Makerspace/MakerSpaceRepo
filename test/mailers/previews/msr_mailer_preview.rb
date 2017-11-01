761b6a27b7b17b00091140c4371e3683eea2d5d9
Parastoo <psaha091@uottawa.ca>
2017-10-31 23:12:33 -0400

    Added previews

--- test/mailers/previews/msr_mailer_preview.rb
+++ test/mailers/previews/msr_mailer_preview.rb
@@ -13,12 +13,30 @@ class MsrMailerPreview < ActionMailer::Preview
 		MsrMailer.reset_password_email(User.first.email , "Password2")
 	end
 
-	def send_report_email
-		MsrMailer.send_report('makerspace@uottawa.ca', ReportGenerator.new_user_report,
-													ReportGenerator.lab_session_report,
-													ReportGenerator.faculty_frequency_report)
+	def send_monthly_report
+		MsrMailer.send_monthly_report('abc@gmail.com', ReportGenerator.new_user_report(1.month.ago.beginning_of_month, 1.month.ago.end_of_month),
+							ReportGenerator.unique_visitors_report(1.month.ago.beginning_of_month, 1.month.ago.end_of_month),
+							ReportGenerator.lab_session_report(1.month.ago.beginning_of_month, 1.month.ago.end_of_month),
+							ReportGenerator.faculty_frequency_report(1.month.ago.beginning_of_month, 1.month.ago.end_of_month),
+							ReportGenerator.gender_frequency_report(1.month.ago.beginning_of_month, 1.month.ago.end_of_month),
+							ReportGenerator.makerspace_training_report(1.month.ago.beginning_of_month, 1.month.ago.end_of_month),
+							ReportGenerator.mtc_training_report(1.month.ago.beginning_of_month, 1.month.ago.end_of_month))
 	end
 
+	def send_weekly_report
+		MsrMailer.send_weekly_report('abc@gmail.com', ReportGenerator.new_user_report,
+						ReportGenerator.unique_visitors_report,
+						ReportGenerator.lab_session_report,
+						ReportGenerator.faculty_frequency_report,
+						ReportGenerator.gender_frequency_report,
+						ReportGenerator.makerspace_training_report,
+						ReportGenerator.mtc_training_report)
+	end
+
+	def send_training_report
+		MsrMailer.send_training_report('abc@gmail.com',ReportGenerator.makerspace_training_report, ReportGenerator.mtc_training_report)
+	end
+	
 	def waiver_reminder_email
 		MsrMailer.waiver_reminder_email('no_waiver_user@gmail.com')
 	end
