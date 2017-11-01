db021e749f387c1e904df1d0ad0f466ae714719e
Parastoo <psaha091@uottawa.ca>
2017-10-31 20:40:55 -0400

    set up some test mails

--- app/mailers/msr_mailer.rb
+++ app/mailers/msr_mailer.rb
@@ -17,20 +17,53 @@ class MsrMailer < ApplicationMailer
 		mail(to: email, subject: "New password for MakerRepo")
 	end
 
-	def send_report(email, email2, csv1, csv2, csv3, csv4, csv5)
-	    attachments['NewUsers.csv'] = {mime_type: 'text/csv', content: csv1}
-			attachments['Visits.csv'] = {mime_type: 'text/csv', content: csv2}
-			attachments['FacultyFrequency.csv'] = {mime_type: 'text/csv', content: csv3}
-			attachments['GenderFrequency.csv'] = {mime_type: 'text/csv', content: csv4}
-			attachments['UniqueVisitors.csv'] = {mime_type: 'text/csv', content: csv5}
+	# def send_report(email, email2, csv1, csv2, csv3, csv4, csv5)
+	#     attachments['NewUsers.csv'] = {mime_type: 'text/csv', content: csv1}
+	# 		attachments['Visits.csv'] = {mime_type: 'text/csv', content: csv2}
+	# 		attachments['FacultyFrequency.csv'] = {mime_type: 'text/csv', content: csv3}
+	# 		attachments['GenderFrequency.csv'] = {mime_type: 'text/csv', content: csv4}
+	# 		attachments['UniqueVisitors.csv'] = {mime_type: 'text/csv', content: csv5}
+	#
+	#     mail(to: email, subject: 'Weekly Report', bcc: [email2])
+	# end
 
-	    mail(to: email, subject: 'Weekly Report', bcc: [email2])
+	# def send_training_report(email1, email2, email3, email4, csv)
+	# 	attachments['MakerspaceTraining.csv'] = {mime_type: 'text/csv', content: csv}
+	#
+	# 	mail(to: email1, subject: 'Weekly Report of Trainings', bcc: [email2, email3, email4])
+	# end
+
+	def send_training_report(email1, csv1, csv2)
+		attachments['MakerspaceTraining.csv'] = {mime_type: 'text/csv', content: csv1}
+		attachments['MtcTraining.csv'] = {mime_type: 'text/csv', content: csv2}
+
+		mail(to: email1, subject: 'Training Reports')
 	end
 
-	def send_training_report(email1, email2, email3, email4, csv)
-		attachments['Trainings.csv'] = {mime_type: 'text/csv', content: csv}
 
-		mail(to: email1, subject: 'Weekly Report of Trainings', bcc: [email2, email3, email4])
+	#test
+	def send_monthly_report(email1, csv1, csv2, csv3, csv4, csv5, csv6, csv7)
+		attachments['NewUsers.csv'] = {mime_type: 'text/csv', content: csv1}
+		attachments['TotalVisits.csv'] = {mime_type: 'text/csv', content: csv2}
+		attachments['UniqueVisits.csv'] = {mime_type: 'text/csv', content: csv3}
+		attachments['FacultyFrequency.csv'] = {mime_type: 'text/csv', content: csv4}
+		attachments['GenderFrequency.csv'] = {mime_type: 'text/csv', content: csv5}
+		attachments['MakerspaceTraining'] = {mime_type: 'text/csv', content: csv6}
+		attachments['MtcTraining'] = {mime_type: 'text/csv', content: csv7}
+
+		mail(to: email1, subject: 'Monthly Reports')
+	end
+		#test
+		def send_weekly_report(email1, csv1, csv2, csv3, csv4, csv5, csv6, csv7)
+			attachments['NewUsers.csv'] = {mime_type: 'text/csv', content: csv1}
+			attachments['TotalVisits.csv'] = {mime_type: 'text/csv', content: csv2}
+			attachments['UniqueVisits.csv'] = {mime_type: 'text/csv', content: csv3}
+			attachments['FacultyFrequency.csv'] = {mime_type: 'text/csv', content: csv4}
+			attachments['GenderFrequency.csv'] = {mime_type: 'text/csv', content: csv5}
+			attachments['MakerspaceTraining'] = {mime_type: 'text/csv', content: csv6}
+			attachments['MtcTraining'] = {mime_type: 'text/csv', content: csv7}
+
+			mail(to: email1, subject: 'Weekly Reports')
 	end
 
 	def waiver_reminder_email(email)
