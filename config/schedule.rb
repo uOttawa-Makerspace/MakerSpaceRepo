f12d072f50e6a5827c8de3f06f7ff9f82650075c
Parastoo <psaha091@uottawa.ca>
2017-11-02 09:15:35 -0400

    was missing deliver_now'

--- config/schedule.rb
+++ config/schedule.rb
@@ -11,28 +11,28 @@
 # end
 
 
-every '0 9 * * *' do #At 9am of every day
+every '0 10 * * *' do #At 9am of every day
   runner "MsrMailer.send_monthly_report('parastoo.ss@gmail.com', ReportGenerator.new_user_report(1.month.ago.beginning_of_month, 1.month.ago.end_of_month),
           ReportGenerator.unique_visitors_report(1.month.ago.beginning_of_month, 1.month.ago.end_of_month),
           ReportGenerator.lab_session_report(1.month.ago.beginning_of_month, 1.month.ago.end_of_month),
           ReportGenerator.faculty_frequency_report(1.month.ago.beginning_of_month, 1.month.ago.end_of_month),
           ReportGenerator.gender_frequency_report(1.month.ago.beginning_of_month, 1.month.ago.end_of_month),
           ReportGenerator.makerspace_training_report(1.month.ago.beginning_of_month, 1.month.ago.end_of_month),
-          ReportGenerator.mtc_traininf_report(1.month.ago.beginning_of_month, 1.month.ago.end_of_month))"
+          ReportGenerator.mtc_traininf_report(1.month.ago.beginning_of_month, 1.month.ago.end_of_month)).deliver_now"
 
 end
 
-every '0 9 * * *' do #At 7am of first day of every week
+every '0 10 * * *' do #At 7am of first day of every week
   runner "MsrMailer.send_weekly_report('parastoo.ss@gmail.com', ReportGenerator.new_user_report,
           ReportGenerator.unique_visitors_report,
           ReportGenerator.lab_session_report,
           ReportGenerator.faculty_frequency_report,
           ReportGenerator.gender_frequency_report,
           ReportGenerator.makerspace_training_report,
-          ReportGenerator.mtc_training_report)"
+          ReportGenerator.mtc_training_report).deliver_now"
 end
-every '0 9 * * *' do #At 7am of first day of every week
-  runner "MsrMailer.send_training_report('parastoo.ss@gmail.com', ReportGenerator.makerspace_training_report, ReportGenerator.mtc_training_report)"
+every '0 10 * * *' do #At 7am of first day of every week
+  runner "MsrMailer.send_training_report('parastoo.ss@gmail.com', ReportGenerator.makerspace_training_report, ReportGenerator.mtc_training_report).deliver_now"
 end
 # Use this file to easily define all of your cron jobs.
 #
