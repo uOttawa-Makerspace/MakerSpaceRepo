75b936e7a211affd4cf986b29422b6a8bcb3df48
Parastoo <psaha091@uottawa.ca>
2017-11-20 11:22:14 -0500

    Revert "migration to change the category and category options"
    This reverts commit e2e55e0716aa264a39140b8992dcd70fe98ff383.
    
    ~
    ~
    ~
    ~
    ~
    ~

--- db/migrate/20171110152106_rename_gng_categories.rb
+++
@@ -1,6 +0,0 @@
-class RenameGngCategories < ActiveRecord::Migration
-  def change
-    Category.where(name: "gng1103").update_all(name: "gng1103/gng1503")
-    CategoryOption.where(name: "gng2101").update_all(name: "gng2101/gng1501")    
-  end
-end
