181186a77d6ebcaceb01960f7f13a0b713804e37
brunomrlima <bruno.mrlima@gmail.com>
2018-07-20 14:26:32 -0400

    creating colunm and option for youtube link

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171120233556) do
+ActiveRecord::Schema.define(version: 20180720181658) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -147,6 +147,7 @@ ActiveRecord::Schema.define(version: 20171120233556) do
     t.string   "share_type"
     t.string   "password"
     t.boolean  "featured",      default: false
+    t.string   "youtube_link"
   end
 
   add_index "repositories", ["user_id"], name: "index_repositories_on_user_id", using: :btree
