5603738467620c1184b415176f384e2dc3405097
brunomrlima <bruno.mrlima@gmail.com>
2018-08-03 14:02:49 -0400

    creating area option model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180803174033) do
+ActiveRecord::Schema.define(version: 20180803180131) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "area_options", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "categories", force: :cascade do |t|
     t.integer  "repository_id"
---
+++ test/models/area_option_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class AreaOptionTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
