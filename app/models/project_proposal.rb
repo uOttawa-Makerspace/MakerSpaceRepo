727b1df7b0a448ddb34236fe1ad410ec5b98104b
brunomrlima <bruno.mrlima@gmail.com>
2018-08-03 14:44:34 -0400

    allowing save project proposals and some validations

--- app/models/project_proposal.rb
+++ app/models/project_proposal.rb
@@ -2,4 +2,11 @@ class ProjectProposal < ActiveRecord::Base
   has_many   :categories,     dependent: :destroy
   has_one    :user,  -> {where(role: "regular_user")}
   has_one    :user,  -> {where(role: "admin")}
+
+  validates :title,
+            format:     { with:    /\A[-a-zA-Z\d\s]*\z/, message: "Invalid project title" },
+            presence:   { message: "Project title is required."}
+
+  validates :email,
+            presence: { message: "Your email is required." }
 end
