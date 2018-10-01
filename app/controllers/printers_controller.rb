d308f978d07c80719891fb59ff06ffa72d73824d
brunomrlima <bruno.mrlima@gmail.com>
2018-10-01 18:35:18 -0400

    link_printer_to_user needs to get updated

--- app/controllers/printers_controller.rb
+++ app/controllers/printers_controller.rb
@@ -2,15 +2,23 @@ class PrintersController < ApplicationController
   layout 'staff_area'
 
   def staff_printers
-    @users = User.all.pluck(:name, :id)
+    @user = current_user
+    @list_users = User.all.pluck(:name, :id)
     @printers = Printer.all
     @last_session_ultimaker = PrinterSession.joins(:printer).order(created_at: :desc)
                                       .where("printers.model = ?", "Ultimaker 2+").last
   end
 
   def link_printer_to_user
-
-
+    printer_id = Printer.find params[:printer][:printer_id]
+    user_id = User.find params[:printer][:user_id]
+    @printer_session = PrinterSession.create(:printer_id => printer_id, :user_id => user_id)
+    if @printer_session
+      flash[:notice] = "Project Session Created"
+    else
+      flash[:alert] = "Something went wrong"
+    end
+    redirect_to :back
   end
 
 end
