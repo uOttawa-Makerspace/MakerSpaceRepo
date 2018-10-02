9e725aa13139decb1c4dd0cf3ae8562587c6aced
brunomrlima <bruno.mrlima@gmail.com>
2018-10-02 18:47:26 -0400

    last number session

--- app/controllers/printers_controller.rb
+++ app/controllers/printers_controller.rb
@@ -5,19 +5,19 @@ class PrintersController < ApplicationController
   def staff_printers
     @printers = Printer.all
     @list_users = User.all.pluck(:name, :id)
-    @last_session_ultimaker = get_last_session("Ultimaker 2+")
-    @last_session_replicator2 = get_last_session("Replicator 2")
-    @last_session_replicator2x = get_last_session("Replicator 2x")
-    @last_session_dremel = get_last_session("Dremel")
+    @last_session_ultimaker = get_last_model_session("Ultimaker 2+")
+    @last_session_replicator2 = get_last_model_session("Replicator 2")
+    @last_session_replicator2x = get_last_model_session("Replicator 2x")
+    @last_session_dremel = get_last_model_session("Dremel")
   end
 
   def staff_printers_updates
     @list_users = User.all.pluck(:name, :id)
     @printers = Printer.all
-    @last_session_ultimaker = get_last_session("Ultimaker 2+")
-    @last_session_replicator2 = get_last_session("Replicator 2")
-    @last_session_replicator2x = get_last_session("Replicator 2x")
-    @last_session_dremel = get_last_session("Dremel")
+    @last_session_ultimaker = get_last_model_session("Ultimaker 2+")
+    @last_session_replicator2 = get_last_model_session("Replicator 2")
+    @last_session_replicator2x = get_last_model_session("Replicator 2x")
+    @last_session_dremel = get_last_model_session("Dremel")
   end
 
   def link_printer_to_user
@@ -31,9 +31,14 @@ class PrintersController < ApplicationController
     redirect_to :back
   end
 
-  def get_last_session(printer_model)
+  def get_last_model_session(printer_model)
     return PrinterSession.joins(:printer).order(created_at: :desc)
                .where("printers.model = ?", printer_model).first
+  end
+
+  def get_last_number_session(printer_model, number)
+    return PrinterSession.joins(:printer).order(created_at: :desc)
+               .where("printers.model = ? AND printers.number = ?", printer_model, number).first
   end
 
 end
