e7ac00cc46399c68b05fff0c8937a616d8631595
brunomrlima <bruno.mrlima@gmail.com>
2018-10-02 17:04:30 -0400

    link_printer_to_user

--- app/controllers/printers_controller.rb
+++ app/controllers/printers_controller.rb
@@ -6,14 +6,13 @@ class PrintersController < ApplicationController
     @list_users = User.all.pluck(:name, :id)
     @printers = Printer.all
     @last_session_ultimaker = PrinterSession.joins(:printer).order(created_at: :desc)
-                                      .where("printers.model = ?", "Ultimaker 2+").last
+                                      .where("printers.model = ?", "Ultimaker 2+").first
   end
 
   def link_printer_to_user
-    printer_id = Printer.find params[:printer][:printer_id]
-    user_id = User.find params[:printer][:user_id]
-    @printer_session = PrinterSession.create(:printer_id => printer_id, :user_id => user_id)
-    if @printer_session
+    printer_id = params[:printer][:printer_id]
+    user_id = params[:printer][:user_id]
+    if PrinterSession.create(:printer_id => printer_id, :user_id => user_id)
       flash[:notice] = "Project Session Created"
     else
       flash[:alert] = "Something went wrong"
