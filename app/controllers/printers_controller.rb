2bdf5116b81d50d715cf9d3dbbeab4d81ba20c9c
brunomrlima <bruno.mrlima@gmail.com>
2018-10-02 20:55:48 -0400

    everything working

--- app/controllers/printers_controller.rb
+++ app/controllers/printers_controller.rb
@@ -5,19 +5,17 @@ class PrintersController < ApplicationController
   def staff_printers
     @printers = Printer.all
     @list_users = User.all.pluck(:name, :id)
-    @last_session_ultimaker = get_last_model_session("Ultimaker 2+")
-    @last_session_replicator2 = get_last_model_session("Replicator 2")
-    @last_session_replicator2x = get_last_model_session("Replicator 2x")
-    @last_session_dremel = get_last_model_session("Dremel")
+    @last_session_ultimaker = Printer.get_last_model_session("Ultimaker 2+")
+    @last_session_replicator2 = Printer.get_last_model_session("Replicator 2")
+    @last_session_replicator2x = Printer.get_last_model_session("Replicator 2x")
+    @last_session_dremel = Printer.get_last_model_session("Dremel")
   end
 
   def staff_printers_updates
-    @list_users = User.all.pluck(:name, :id)
-    @printers = Printer.all
-    @last_session_ultimaker = get_last_model_session("Ultimaker 2+")
-    @last_session_replicator2 = get_last_model_session("Replicator 2")
-    @last_session_replicator2x = get_last_model_session("Replicator 2x")
-    @last_session_dremel = get_last_model_session("Dremel")
+    @ultimaker_printer_ids = Printer.get_printer_ids("Ultimaker 2+")
+    @replicator2_printer_ids = Printer.get_printer_ids("Replicator 2")
+    @replicator2x_printer_ids = Printer.get_printer_ids("Replicator 2x")
+    @dremel_printer_ids = Printer.get_printer_ids("Dremel")
   end
 
   def link_printer_to_user
@@ -29,16 +27,6 @@ class PrintersController < ApplicationController
       flash[:alert] = "Something went wrong"
     end
     redirect_to :back
-  end
-
-  def get_last_model_session(printer_model)
-    return PrinterSession.joins(:printer).order(created_at: :desc)
-               .where("printers.model = ?", printer_model).first
-  end
-
-  def get_last_number_session(printer_model, number)
-    return PrinterSession.joins(:printer).order(created_at: :desc)
-               .where("printers.model = ? AND printers.number = ?", printer_model, number).first
   end
 
 end
--- app/models/printer.rb
+++ app/models/printer.rb
@@ -1,3 +1,17 @@
 class Printer < ActiveRecord::Base
   has_many :printer_sessions,     dependent: :destroy
+
+  def self.get_printer_ids(model)
+    return Printer.where(:model => model).pluck(:id)
+  end
+
+  def self.get_last_model_session(printer_model)
+    return PrinterSession.joins(:printer).order(created_at: :desc)
+               .where("printers.model = ?", printer_model).first
+  end
+
+  def self.get_last_number_session(printer_id)
+    return PrinterSession.order(created_at: :desc).where(:printer_id => printer_id).first
+  end
+
 end
