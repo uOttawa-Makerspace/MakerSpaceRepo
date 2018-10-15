b4d87e06ec476135c5affd00a36539f429402043
brunomrlima <bruno.mrlima@gmail.com>
2018-10-15 11:50:26 -0400

    3d printing update

--- app/controllers/printers_controller.rb
+++ app/controllers/printers_controller.rb
@@ -6,15 +6,15 @@ class PrintersController < ApplicationController
     @printers = Printer.all
     @list_users = User.all.pluck(:name, :id)
     @last_session_ultimaker = Printer.get_last_model_session("Ultimaker 2+")
+    @last_session_ultimaker_3 = Printer.get_last_model_session("Ultimaker 3")
     @last_session_replicator2 = Printer.get_last_model_session("Replicator 2")
-    @last_session_replicator2x = Printer.get_last_model_session("Replicator 2x")
     @last_session_dremel = Printer.get_last_model_session("Dremel")
   end
 
   def staff_printers_updates
     @ultimaker_printer_ids = Printer.get_printer_ids("Ultimaker 2+")
+    @ultimaker3_printer_ids = Printer.get_printer_ids("Ultimaker 3")
     @replicator2_printer_ids = Printer.get_printer_ids("Replicator 2")
-    @replicator2x_printer_ids = Printer.get_printer_ids("Replicator 2x")
     @dremel_printer_ids = Printer.get_printer_ids("Dremel")
   end
 
