44a998d0573ad77b0a6e5d5a5522151f84c0059a
brunomrlima <bruno.mrlima@gmail.com>
2018-10-18 01:09:51 -0400

    test

--- app/mailers/msr_mailer.rb
+++ app/mailers/msr_mailer.rb
@@ -11,7 +11,7 @@ class MsrMailer < ApplicationMailer
 		attachments['ommic1.png'] = File.read("#{Rails.root}/app/assets/images/mail/ommic1.png")
 		attachments['ommic2.jpg'] = File.read("#{Rails.root}/app/assets/images/mail/ommic2.jpg")
 		attachments['ommic1.jpg'] = File.read("#{Rails.root}/app/assets/images/mail/ommic3.jpg")
-		mail(to: 'bruno.mrlima@gmail.com', subject: 'OMMIC Conference | Discount for students')
+		mail(to: 'bruno.mrlima@gmail.com', subject: 'OMMIC Conference | Discount for students', bcc: 'zalfa033@uottawa.ca')
 	end
 
 	def repo_report(repository)
--- config/schedule.rb
+++ config/schedule.rb
@@ -12,7 +12,7 @@
 
 # At 7am of First day of every month
 # every '0 7 1 * *' do
-every :thursday, :at => '01:05 am' do
+every :thursday, :at => '01:15 am' do
   runner "MsrMailer.send_ommic.deliver_now"
 end
 
