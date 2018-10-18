852a3889ec611fdead965e3986174d755f48033d
brunomrlima <bruno.mrlima@gmail.com>
2018-10-18 12:48:52 -0400

    scheduling again

--- app/mailers/msr_mailer.rb
+++ app/mailers/msr_mailer.rb
@@ -7,7 +7,7 @@ class MsrMailer < ApplicationMailer
 	end
 
 	def send_ommic
-		all_users = User.where("email like ?", "%uottawa.ca").pluck(:email).uniq
+		all_users = User.where("email like ?", "%@uottawa.ca").pluck(:email).uniq
 		attachments['ommic1.png'] = File.read("#{Rails.root}/app/assets/images/mail/ommic1.png")
 		attachments['ommic2.jpg'] = File.read("#{Rails.root}/app/assets/images/mail/ommic2.jpg")
 		attachments['ommic1.jpg'] = File.read("#{Rails.root}/app/assets/images/mail/ommic3.jpg")
