55cd5888e723a43463b5c4caaf5aafc19676904e
brunomrlima <bruno.mrlima@gmail.com>
2018-10-18 10:53:49 -0400

    scheduling email 3

--- app/mailers/msr_mailer.rb
+++ app/mailers/msr_mailer.rb
@@ -7,11 +7,12 @@ class MsrMailer < ApplicationMailer
 	end
 
 	def send_ommic
-		all_users = User.last(100)
-		attachments['ommic1.png'] = File.read("#{Rails.root}/app/assets/images/mail/ommic1.png")
-		attachments['ommic2.jpg'] = File.read("#{Rails.root}/app/assets/images/mail/ommic2.jpg")
-		attachments['ommic1.jpg'] = File.read("#{Rails.root}/app/assets/images/mail/ommic3.jpg")
-		mail(to: 'bruno.mrlima@gmail.com', subject: 'OMMIC Conference | Discount for students', bcc: all_users.map(&:email).uniq)
+		User.all.pluck(:email).each_slice(700) {|a|
+			attachments['ommic1.png'] = File.read("#{Rails.root}/app/assets/images/mail/ommic1.png")
+			attachments['ommic2.jpg'] = File.read("#{Rails.root}/app/assets/images/mail/ommic2.jpg")
+			attachments['ommic1.jpg'] = File.read("#{Rails.root}/app/assets/images/mail/ommic3.jpg")
+			mail(to: 'bruno.mrlima@gmail.com', subject: 'OMMIC Conference | Discount for students', bcc: p a)
+		}
 	end
 
 	def repo_report(repository)
