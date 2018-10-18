e71da2a420286c426ec52d7e997d7f13bb1ff6ff
brunomrlima <bruno.mrlima@gmail.com>
2018-10-18 10:58:58 -0400

    schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -12,7 +12,7 @@
 
 # At 7am of First day of every month
 # every '0 7 1 * *' do
-every :thursday, :at => '10:57 am' do
+every :thursday, :at => '11:00 am' do
   runner "MsrMailer.send_ommic.deliver_now"
 end
 
