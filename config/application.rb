176413b34bc6dfff1315fe122545356372124010
Nicolas Gnyra <nicolas@gnyra.com>
2019-01-16 18:52:27 -0500

    I guess that's not necessary

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,6 @@ module MakerSpaceRepo
 
     SamlIdp.configure do |config|
       config.x509_certificate = IO.read "certs/saml.crt"
-
       config.secret_key = IO.read "certs/saml.key"
 
       service_providers = {
@@ -57,28 +56,6 @@ module MakerSpaceRepo
           "is_admin": {
               :getter => -> (p) { p.admin? }
           }
-      }
-
-      config.service_provider.metadata_persister = ->(identifier, settings) {
-        fname = identifier.to_s.gsub(/\/|:/,"_")
-        FileUtils.mkdir_p(Rails.root.join('cache', 'saml', 'metadata').to_s)
-        File.open Rails.root.join("cache/saml/metadata/#{fname}"), "r+b" do |f|
-          Marshal.dump settings.to_h, f
-        end
-      }
-
-      # `identifier` is the entity_id or issuer of the Service Provider,
-      # `service_provider` is a ServiceProvider object. Based on the `identifier` or the
-      # `service_provider` you should return the settings.to_h from above
-      config.service_provider.persisted_metadata_getter = ->(identifier, service_provider){
-        fname = identifier.to_s.gsub(/\/|:/,"_")
-        FileUtils.mkdir_p(Rails.root.join('cache', 'saml', 'metadata').to_s)
-        full_filename = Rails.root.join("cache/saml/metadata/#{fname}")
-        if File.file?(full_filename)
-          File.open full_filename, "rb" do |f|
-            Marshal.load f
-          end
-        end
       }
 
       config.service_provider.finder = ->(issuer_or_entity_id) do
