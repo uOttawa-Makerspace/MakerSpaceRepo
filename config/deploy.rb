e45a09c10ff287e680cc1dbf23f1d5f47472dc68
Nicolas Gnyra <nicolas@gnyra.com>
2019-02-23 05:01:06 -0500

    Set git URL & branch to upstream repo

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 set :application, 'MakerRepo'
-set :repo_url, 'https://github.com/nicoco007/MakerSpaceRepo.git'
-set :branch, 'new-server'
+set :repo_url, 'https://github.com/uOttawa-Makerspace/MakerSpaceRepo.git'
+set :branch, 'master'
 set :rbenv_ruby, '2.3.1'
 set :deploy_to, '/var/www/makerrepo'
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
