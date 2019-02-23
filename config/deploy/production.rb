925359df8abd10e89bb17084887994439896fbf4
Nicolas Gnyra <nicolas@gnyra.com>
2019-02-23 10:33:38 -0500

    Use deploy user instead of makerrepo

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
-role :app, %w{makerrepo@138.197.167.242}
-role :web, %w{makerrepo@138.197.167.242}
-role :db,  %w{makerrepo@138.197.167.242}
+role :app, %w{deploy@138.197.167.242}
+role :web, %w{deploy@138.197.167.242}
+role :db,  %w{deploy@138.197.167.242}
 
 set :default_env, { 'PASSENGER_INSTANCE_REGISTRY_DIR' => '/var/www/makerrepo/passenger_instance_registry' }
