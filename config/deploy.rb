8a54f8931ea0332a5834b20cabfd25af4e0135f4
Nicolas Gnyra <nicolas@gnyra.com>
2019-02-24 14:51:07 -0500

    Fix ruby version in deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 set :application, 'MakerRepo'
 set :repo_url, 'https://github.com/uOttawa-Makerspace/MakerSpaceRepo.git'
 set :branch, 'master'
-set :rbenv_ruby, '2.3.1'
+set :rbenv_ruby, '2.3.4'
 set :deploy_to, '/var/www/makerrepo'
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'certs')
