3262ea86740f0f5e6a493e5a58baf55c27a5f070
Nicolas Gnyra <nicolas@gnyra.com>
2019-02-24 15:12:02 -0500

    Use server.makerepo.com instead of IP

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
-role :app, %w{deploy@138.197.167.242}
-role :web, %w{deploy@138.197.167.242}
-role :db,  %w{deploy@138.197.167.242}
+role :app, %w{deploy@server.makerepo.com}
+role :web, %w{deploy@server.makerepo.com}
+role :db,  %w{deploy@server.makerepo.com}
 
 set :default_env, { 'PASSENGER_INSTANCE_REGISTRY_DIR' => '/var/www/makerrepo/passenger_instance_registry' }
