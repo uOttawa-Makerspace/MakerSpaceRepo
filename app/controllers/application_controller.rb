0480cbbb760ba67f04066c3c9afd7d1a474d6cf0
Nicolas Gnyra <nicolas@gnyra.com>
2019-02-28 20:34:03 -0500

    Persist locale in session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,7 @@ class ApplicationController < ActionController::Base
   before_action :set_locale
 
   def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
+    I18n.locale = params[:locale] || session[:locale] || I18n.default_locale
+    session[:locale] = I18n.locale
   end
 end
