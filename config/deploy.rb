b77c73abbbcca522b13ee8f5d77116efd8b60c7b
Nicolas Gnyra <nicolas@gnyra.com>
2019-03-04 21:02:14 -0500

    Try again again again

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,4 +4,3 @@ set :rbenv_ruby, '2.3.1'
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'certs')
 set :default_env, { 'PASSENGER_INSTANCE_REGISTRY_DIR' => '/var/passenger_instance_registry' }
-set :ssh_options, keys: ["config/deploy_id_rsa"] if File.exist?("config/deploy_id_rsa")
