9331e550cb8058fd8527f1a9307d144dfaacd869
Nicolas Gnyra <nicolas@gnyra.com>
2019-03-06 20:11:20 -0500

    Rename to staging

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,5 +3,5 @@ role :web, %w{deploy@server.makerepo.com}
 role :db,  %w{deploy@server.makerepo.com}
 
 set :repo_url, 'https://github.com/uOttawa-Makerspace/MakerSpaceRepo.git'
-set :branch, 'develop'
+set :branch, 'staging'
 set :deploy_to, '/var/www/makerrepo-staging'
