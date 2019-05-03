370e9b02f330bb03668d2ab9b0c4abeef7616d1a
brunomrlima <bruno.mrlima@gmail.com>
2019-05-03 15:41:11 -0400

    volunteer hour new

--- app/controllers/volunteer_hours_controller.rb
+++ app/controllers/volunteer_hours_controller.rb
@@ -2,4 +2,12 @@ class VolunteerHoursController < VolunteersController
   def index
     @user = current_user
   end
+
+  def new
+    @new_volunteer_hour = VolunteerHour.new
+  end
+
+  def create
+
+  end
 end
