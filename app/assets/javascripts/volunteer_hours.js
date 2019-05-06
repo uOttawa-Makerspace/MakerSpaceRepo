2cdee079ba59c75125d334c7be40c99b7082544b
brunomrlima <bruno.mrlima@gmail.com>
2019-05-06 18:57:18 -0400

    javascript + html + controller for creating new tracking hour

--- app/assets/javascripts/volunteer_hours.js
+++ app/assets/javascripts/volunteer_hours.js
@@ -5,54 +5,15 @@ $(document).on('ready load', function () {
         var validation = Array.prototype.filter.call(forms, function(form) {
             form.addEventListener('submit', function(event) {
                 if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
                 } else{
-                    var name = $("input#name").val();
-                    var email = $("input#email").val();
-                    var phone = $("input#phone").val();
-                    var message = $("textarea#message").val();
-                    var firstName = name; // For Success/Failure Message
-                    // Check for white space in name for Success/Fail message
-                    if (firstName.indexOf(' ') >= 0) {
-                        firstName = name.split(' ').slice(0, -1).join(' ');
-                    }
-                    var $this = $("button#sendMessageButton");
-                    $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
-                    $.ajax({
-                        url: '/send_contact_info',
-                        type: "POST",
-                        data: {
-                            name: name,
-                            phone: phone,
-                            email: email,
-                            message: message
-                        },
-                        cache: false,
-                        success: function() {
-                            // Success message
-                            $('#success').html("<div class='alert alert-success'>");
-                            $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                                .append("</button>");
-                            $('#success > .alert-success')
-                                .append("<strong>Sua mensagem foi enviada. Entraremos em contato em breve. </strong>");
-                            $('#success > .alert-success')
-                                .append('</div>');
-                            //clear all fields
-                            // $('#contactForm').trigger("reset");
-                        },
-                        error: function() {
-                            // Fail message
-                            $('#success').html("<div class='alert alert-danger'>");
-                            $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                                .append("</button>");
-                            $('#success > .alert-danger').append($("<strong>").text("Desculpe " + firstName + ", parece que os servers não estão respondendo. Por favor tentar mais tarde."));
-                            $('#success > .alert-danger').append('</div>');
-                            //clear all fields
-                            // $('#contactForm').trigger("reset");
-                        },
-                    });
+                    var total_time_hidden = document.getElementById("total_time_hidden");
+                    var hour = Number($("input#hours").val());
+                    var minutes = Number($("input#minutes").val());
+                    var total_time = hour + minutes/60.0;
+                    total_time_hidden.value = total_time;
                 }
-                event.preventDefault();
-                event.stopPropagation();
                 form.classList.add('was-validated');
             }, false);
         });
--- app/controllers/volunteer_hours_controller.rb
+++ app/controllers/volunteer_hours_controller.rb
@@ -9,7 +9,17 @@ class VolunteerHoursController < VolunteersController
   end
 
   def create
-    puts "passing here"
+    @volunteer_hour = VolunteerHour.new(volunteer_hour_params)
+    @volunteer_hour.user_id = @user.try(:id)
+    if @volunteer_hour.save!
+      redirect_to new_volunteer_hour_path
+      flash[:notice] = "You have send your tracking hours"
+    end
+  end
 
+  private
+
+  def volunteer_hour_params
+    params.require(:volunteer_hour).permit(:volunteer_task_id, :date_of_task, :total_time)
   end
 end
