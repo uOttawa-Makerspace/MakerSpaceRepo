8f42ebcdfc46493bd45cc41f4b51621834a4febc
brunomrlima <bruno.mrlima@gmail.com>
2019-05-06 17:03:41 -0400

    volunteer task

--- app/controllers/volunteer_hours_controller.rb
+++ app/controllers/volunteer_hours_controller.rb
@@ -5,6 +5,7 @@ class VolunteerHoursController < VolunteersController
 
   def new
     @new_volunteer_hour = VolunteerHour.new
+    @volunteer_tasks = VolunteerTask.all.order(created_at: :desc).pluck(:id, :title)
   end
 
   def create
