1ea9748a0ea336f2d0a3ccbad2e4d8dbcfbe25ea
brunomrlima <bruno.mrlima@gmail.com>
2019-05-06 17:10:18 -0400

    pluck another order

--- app/controllers/volunteer_hours_controller.rb
+++ app/controllers/volunteer_hours_controller.rb
@@ -5,7 +5,7 @@ class VolunteerHoursController < VolunteersController
 
   def new
     @new_volunteer_hour = VolunteerHour.new
-    @volunteer_tasks = VolunteerTask.all.order(created_at: :desc).pluck(:id, :title)
+    @volunteer_tasks = VolunteerTask.all.order(created_at: :desc).pluck(:title, :id)
   end
 
   def create
