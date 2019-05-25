15dc2f5bb8b6d059d552d4610e4047b5dd51cee0
brunomrlima <bruno.mrlima@gmail.com>
2019-05-25 17:54:26 -0400

    adding approval to volunteer request

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190525200929) do
+ActiveRecord::Schema.define(version: 20190525215349) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -313,6 +313,7 @@ ActiveRecord::Schema.define(version: 20190525200929) do
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
     t.integer  "user_id"
+    t.boolean  "approval"
   end
 
   create_table "volunteer_tasks", force: :cascade do |t|
