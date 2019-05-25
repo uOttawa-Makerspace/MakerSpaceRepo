29b7a00491f73d185cbbab9b8e204c328c30933d
brunomrlima <bruno.mrlima@gmail.com>
2019-05-25 16:08:42 -0400

    volunteer request done

--- app/controllers/volunteer_requests_controller.rb
+++ app/controllers/volunteer_requests_controller.rb
@@ -4,10 +4,20 @@ class VolunteerRequestsController < ApplicationController
   end
 
   def create
-
+    @volunteer_request = VolunteerRequest.new(request_params)
+    if @volunteer_request.save!
+      redirect_to root_path
+      flash[:notice] = "You've successfully submitted your volunteer request."
+    end
   end
 
   def show
 
+  end
+
+  private
+
+  def request_params
+    params.require(:volunteer_request).permit(:interests)
   end
 end
