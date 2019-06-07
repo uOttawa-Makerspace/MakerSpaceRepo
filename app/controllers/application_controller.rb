afe1992c0bbf3706114b9ac0e8d903c8d25343e1
brunomrlima <bruno.mrlima@gmail.com>
2019-06-07 16:56:30 -0400

    set_last_seen_at'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,16 @@ class ApplicationController < ActionController::Base
   include ApplicationHelper
 
   before_action :set_locale
+  before_action :set_last_seen_at, if: proc { user_signed_in? && (user.last_seen_at.nil? || user.last_seen_at < 15.minutes.ago) }
+
 
   def set_locale
     I18n.locale = params[:locale] || session[:locale] || I18n.default_locale
     session[:locale] = I18n.locale
+  end
+
+  private
+  def set_last_seen_at
+    current_user.update_column(:last_seen_at, Time.now)
   end
 end
