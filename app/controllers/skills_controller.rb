43dccadaeec60a0dc32edbfcf68f0dc41d57d6ff
brunomrlima <bruno.mrlima@gmail.com>
2019-06-07 13:17:06 -0400

    user can edit their skill level

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -8,7 +8,19 @@ class SkillsController < ApplicationController
   end
 
   def update
+    skill = Skill.find(params[:id])
+    if skill.update(skill_params)
+      flash[:notice] = "Skills updated"
+    else
+      flash[:alert] = "Something went wrong"
+    end
+    redirect_to edit_skill_path
+  end
 
+  private
+
+  def skill_params
+    params.require(:skill).permit(:printing, :laser_cutting, :arduino, :virtual_reality, :embroidery)
   end
 
 end
