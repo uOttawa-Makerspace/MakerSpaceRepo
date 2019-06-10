b2f1f67b98ff505ffbe7988027a058ab640c4669
brunomrlima <bruno.mrlima@gmail.com>
2019-06-10 17:17:28 -0400

    migration done

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190610211108) do
+ActiveRecord::Schema.define(version: 20190610211615) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -234,6 +234,7 @@ ActiveRecord::Schema.define(version: 20190610211108) do
     t.datetime "created_at",                                null: false
     t.datetime "updated_at",                                null: false
     t.boolean  "active",          default: true
+    t.string   "soldering",       default: "No Experience"
   end
 
   create_table "spaces", force: :cascade do |t|
