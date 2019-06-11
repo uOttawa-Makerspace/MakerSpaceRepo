f5f6500ca18b550d7c9fedebc94ca9a06ec998a6
brunomrlima <bruno.mrlima@gmail.com>
2019-06-11 16:05:47 -0400

    VTJ almost ready

--- app/controllers/volunteer_task_joins_controller.rb
+++ app/controllers/volunteer_task_joins_controller.rb
@@ -1,12 +1,12 @@
 class VolunteerTaskJoinsController < ApplicationController
   def create
     volunteer_join = VolunteerTaskJoin.new(volunteer_task_joins_params)
-    if current.user.staff?
+    if current_user.staff?
       volunteer_join.user_id = params[:volunteer_task_join][:user_id] || current_user.id
     else
       volunteer_join.user_id = current_user.id
     end
-    volunteer_join.type = volunteer_join.user
+    volunteer_join.type = volunteer_join.user.role.capitalize
     if volunteer_join.save!
       redirect_to :back
       flash[:notice] = "You've successfully joined this volunteer task."
