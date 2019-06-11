636a0057aa19fd88ca35a59b364fec852ccfd833
brunomrlima <bruno.mrlima@gmail.com>
2019-06-11 15:56:13 -0400

    ApplicationController for now

--- app/controllers/volunteer_task_joins_controller.rb
+++ app/controllers/volunteer_task_joins_controller.rb
@@ -1,5 +1,21 @@
-class VolunteerTaskJoinsController < VolunteerTask
+class VolunteerTaskJoinsController < ApplicationController
   def create
+    volunteer_join = VolunteerTaskJoin.new(volunteer_task_joins_params)
+    if current.user.staff?
+      volunteer_join.user_id = params[:volunteer_task_join][:user_id] || current_user.id
+    else
+      volunteer_join.user_id = current_user.id
+    end
+    volunteer_join.type = volunteer_join.user
+    if volunteer_join.save!
+      redirect_to :back
+      flash[:notice] = "You've successfully joined this volunteer task."
+    end
+  end
 
+  private
+
+  def volunteer_task_joins_params
+    params.require(:volunteer_task_join).permit(:volunteer_task_id)
   end
 end
