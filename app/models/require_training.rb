f4aceefec0d050f8cc9b40a55fe419d5b72edeee
brunomrlima <bruno.mrlima@gmail.com>
2019-06-12 16:56:51 -0400

    certification needed and show html

--- app/models/require_training.rb
+++ app/models/require_training.rb
@@ -1,3 +1,4 @@
 class RequireTraining < ActiveRecord::Base
   belongs_to :training
+  belongs_to :volunteer_task
 end
--- app/models/volunteer_task.rb
+++ app/models/volunteer_task.rb
@@ -1,6 +1,7 @@
 class VolunteerTask < ActiveRecord::Base
   belongs_to :user
+  belongs_to :space
   has_many :volunteer_hours, dependent: :destroy
   has_many :volunteer_task_joins, dependent: :destroy
-  belongs_to :space
+  has_many :require_trainings
 end
