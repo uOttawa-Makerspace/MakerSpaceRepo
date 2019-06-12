341a6fe050e5e52f838f0b1507457cb507e03a48
brunomrlima <bruno.mrlima@gmail.com>
2019-06-12 12:44:16 -0400

    checking if user has skill

--- app/controllers/volunteers_controller.rb
+++ app/controllers/volunteers_controller.rb
@@ -2,6 +2,7 @@ class VolunteersController < ApplicationController
   layout 'volunteer'
   before_action :current_user
   before_action :grant_access
+  before_action :check_skills
   def index
     @user = current_user
   end
@@ -18,6 +19,12 @@ class VolunteersController < ApplicationController
     unless current_user.volunteer? || current_user.admin? || current_user.staff?
       redirect_to root_path
       flash[:alert] = "You cannot access this area."
+    end
+  end
+
+  def check_skills
+    unless current_user.skill
+      Skill.create(:user_id => current_user.id)
     end
   end
 end
