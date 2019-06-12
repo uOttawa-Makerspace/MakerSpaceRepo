dd5e0b363b500014ea299a2c53ea596253d81dbf
brunomrlima <bruno.mrlima@gmail.com>
2019-06-12 16:21:15 -0400

    if volunteer task is destroied, so volunteer hours and volunteer_task_joins

--- app/models/volunteer_task.rb
+++ app/models/volunteer_task.rb
@@ -1,6 +1,6 @@
 class VolunteerTask < ActiveRecord::Base
   belongs_to :user
-  has_many :volunteer_hours
-  has_many :volunteer_task_joins
+  has_many :volunteer_hours, dependent: :destroy
+  has_many :volunteer_task_joins, dependent: :destroy
   belongs_to :space
 end
