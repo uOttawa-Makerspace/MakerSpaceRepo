0cd1766cfb8785ea367c920bacaf39304be8352c
brunomrlima <bruno.mrlima@gmail.com>
2019-06-17 14:21:25 -0400

    TODO

--- app/controllers/volunteer_task_joins_controller.rb
+++ app/controllers/volunteer_task_joins_controller.rb
@@ -14,6 +14,7 @@ class VolunteerTaskJoinsController < ApplicationController
   end
 
   def remove
+    # TODO: Make this 'destroy' instead of 'remove'
     user_id = params[:volunteer_task_join][:user_id]
     volunteer_task_id = params[:volunteer_task_join][:volunteer_task_id]
     volunteer_join = VolunteerTaskJoin.find_by(:user_id => user_id,
