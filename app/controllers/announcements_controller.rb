707fada337acc69126db5a4bb9f2fce168c28d2e
brunomrlima <bruno.mrlima@gmail.com>
2019-06-18 17:59:48 -0400

    create for announcements

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -4,10 +4,22 @@ class AnnouncementsController < ApplicationController
   end
 
   def create
+    @announcement = Announcement.new(announcement_params)
+    @announcement.user_id = current_user.id
+    if @announcement.save!
+      redirect_to announcements_path
+      flash[:notice] = "You've successfully created an announcement for #{@announement.public.capitalize}"
+    end
 
   end
 
   def destroy
 
+  end
+
+  private
+
+  def announcement_params
+    params.require(:announcement).permit(:description, :public)
   end
 end
