c6d09ac9fc0915340dd729de7cdcf8ccd669112c
brunomrlima <bruno.mrlima@gmail.com>
2019-06-18 17:43:19 -0400

    model for announcements

---
+++ db/migrate/20190618212808_create_announcements.rb
@@ -0,0 +1,12 @@
+class CreateAnnouncements < ActiveRecord::Migration
+  def change
+    create_table :announcements do |t|
+      t.text :description
+      t.string :public
+      t.integer :user_id
+
+
+      t.timestamps null: false
+    end
+  end
+end
