8359ef11780f379ab97bcf41e962c9401f922b9d
brunomrlima <bruno.mrlima@gmail.com>
2019-06-19 12:46:31 -0400

    update done

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -5,13 +5,23 @@ class AnnouncementsController < ApplicationController
   end
 
   def create
-    @announcement = Announcement.new(announcement_params)
-    @announcement.user_id = current_user.id
-    if @announcement.save!
+    announcement = Announcement.new(announcement_params)
+    announcement.user_id = current_user.id
+    if announcement.save!
       redirect_to announcements_path
-      flash[:notice] = "You've successfully created an announcement for #{@announement.public_goal.capitalize}"
+      flash[:notice] = "You've successfully created an announcement for #{announcement.public_goal.capitalize}"
     end
 
+  end
+
+  def update
+    announcement = VolunteerTask.find(params[:id])
+    if announcement.update(announcement_params)
+      flash[:notice] = "Announcement updated"
+    else
+      flash[:alert] = "Something went wrong"
+    end
+    redirect_to announcements_path
   end
 
   def destroy
@@ -21,6 +31,6 @@ class AnnouncementsController < ApplicationController
   private
 
   def announcement_params
-    params.require(:announcement).permit(:description, :public_goal)
+    params.require(:announcement).permit(:description, :public_goal, :active)
   end
 end
