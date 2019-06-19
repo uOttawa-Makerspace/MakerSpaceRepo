2628597ee8870a324d59e6c765b246c8bc1fea06
brunomrlima <bruno.mrlima@gmail.com>
2019-06-19 15:11:13 -0400

    announcements for admins and staff

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -3,4 +3,6 @@ class Announcement < ActiveRecord::Base
   scope :active, -> {where(active: true)}
   scope :volunteers, -> {where(public_goal: "volunteer")}
   scope :all_users, -> {where(public_goal: "all")}
+  scope :admins, -> {where(public_goal: "admin")}
+  scope :staff, -> {where(public_goal: "staff")}
 end
