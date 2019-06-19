794182c4558642135be92201d703605c34e494cf
brunomrlima <bruno.mrlima@gmail.com>
2019-06-19 14:55:40 -0400

    all users announcements

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -2,4 +2,5 @@ class Announcement < ActiveRecord::Base
   belongs_to :user
   scope :active, -> {where(active: true)}
   scope :volunteers, -> {where(public_goal: "volunteer")}
+  scope :all_users, -> {where(public_goal: "all")}
 end
