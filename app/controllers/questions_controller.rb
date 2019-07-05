37c402fe6ab740e7a1868b76eff97f785e39e025
brunomrlima <bruno.mrlima@gmail.com>
2019-07-05 14:49:54 -0400

    show in controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -20,19 +20,7 @@ class QuestionsController < ApplicationController
   end
 
   def show
-    @volunteer_task = VolunteerTask.find(params[:id])
-    @new_volunteer_join = VolunteerTaskJoin.new
-    @new_required_training = RequireTraining.new
-    trainings_already_added = @volunteer_task.require_trainings.pluck(:training_id)
-    @trainings = Training.where.not(id: trainings_already_added).pluck(:name, :id)
-    @user_trainings = user_trainings
-    @volunteer_task_trainings = volunteer_task_trainings
-    if current_user.staff?
-      @volunteers = User.where(:role => "volunteer").where.not(:id => @volunteer_task.volunteer_task_joins.pluck(:user_id)).pluck(:name, :id)
-      @staff = User.where("users.role = ? OR users.role = ?", "staff", "admin").where.not(:id => @volunteer_task.volunteer_task_joins.pluck(:user_id)).pluck(:name, :id)
-      @volunteers_in_task = User.where(:role => "volunteer").where(:id => @volunteer_task.volunteer_task_joins.pluck(:user_id)).pluck(:name, :id)
-      @staff_in_task = User.where("users.role = ? OR users.role = ?", "staff", "admin").where(:id => @volunteer_task.volunteer_task_joins.pluck(:user_id)).pluck(:name, :id)
-    end
+    @question = Question.find(params[:id])
   end
 
   def edit
