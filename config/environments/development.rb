cf3b024c050ad625aaa9ceaf00657858f9c5a203
Arthur Fetiveau <art.fetiveau@gmail.com>
2019-07-08 10:31:16 +0200

    Updated the I printed it section so only staff that decided to print it and admins can click the button

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,7 +39,7 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  config.console = Pry
+  config.console = Pry  
 
   Octokit.configure do |c|
     c.client_id        = ENV['GITHUB_APP_KEY']
