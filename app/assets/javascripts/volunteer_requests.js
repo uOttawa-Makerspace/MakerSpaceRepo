e36f7f8ff12cae7acab1eaf75a834304fc6e3e0e
brunomrlima <bruno.mrlima@gmail.com>
2019-07-09 17:58:23 -0400

    this is not used anymore

--- app/assets/javascripts/volunteer_requests.js
+++ app/assets/javascripts/volunteer_requests.js
@@ -73,15 +73,3 @@ function fixStepIndicator(n) {
     //... and adds the "active" class on the current step:
     x[n].className += " active";
 }
-
-function showHideDivs(){
-    var passInfos = document.getElementsByClassName('pastInfo');
-    var nextInfos = document.getElementsByClassName('nextInfo');
-    for (var i = 0; i < passInfos.length; i++) {
-        passInfos[i].style.display = "none";
-    }
-    for (var i = 0; i < nextInfos.length; i++) {
-        nextInfos[i].style.display = "inline-block";
-        nextInfos[i].className += " passInfo";
-    }
-}
