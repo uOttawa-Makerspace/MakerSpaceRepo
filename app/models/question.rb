09a58bb24209afc8745deac56502bf3b81435da9
brunomrlima <bruno.mrlima@gmail.com>
2019-07-10 12:33:40 -0400

    nested atributes

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,7 @@
 class Question < ActiveRecord::Base
   belongs_to :user
   has_many :answers
+  accepts_nested_attributes_for :answers
 
   CATEGORIES = ["Basic Training",
                 "Mill Training",
