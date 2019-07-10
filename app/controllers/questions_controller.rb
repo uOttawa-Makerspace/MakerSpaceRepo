6591cc4c0120affc2b92815e5fe90bff3dfce942
brunomrlima <bruno.mrlima@gmail.com>
2019-07-10 13:43:51 -0400

    needs id to update properly

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -61,6 +61,6 @@ class QuestionsController < ApplicationController
   end
 
   def question_params
-    params.require(:question).permit(:description, :category, answers_attributes:[:description, :correct])
+    params.require(:question).permit(:description, :category, answers_attributes:[:id, :description, :correct])
   end
 end
