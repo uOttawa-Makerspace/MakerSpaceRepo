453307fb5cab59323bf7aeb3f4d0c1d796514ee5
brunomrlima <bruno.mrlima@gmail.com>
2019-07-10 13:48:10 -0400

    if question is deleted, answer is also deleted

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,6 @@
 class Question < ActiveRecord::Base
   belongs_to :user
-  has_many :answers
+  has_many :answers,  dependent: :destroy
   accepts_nested_attributes_for :answers
 
   CATEGORIES = ["Basic Training",
