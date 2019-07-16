54280e9d4f9c5f3f9c2bab6f8480fceb0b34223f
brunomrlima <bruno.mrlima@gmail.com>
2019-07-16 17:30:42 -0400

    migrating exam questions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190716204541) do
+ActiveRecord::Schema.define(version: 20190716212918) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -92,6 +92,13 @@ ActiveRecord::Schema.define(version: 20190716204541) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "exam_questions", force: :cascade do |t|
+    t.integer  "exam_id"
+    t.integer  "question_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "exams", force: :cascade do |t|
