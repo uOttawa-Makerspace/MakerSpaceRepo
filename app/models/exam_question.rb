ec7774862be3b77790b95770807c5e9e9e6afa5d
brunomrlima <bruno.mrlima@gmail.com>
2019-07-16 17:56:46 -0400

    using random_records

--- app/models/exam_question.rb
+++ app/models/exam_question.rb
@@ -3,7 +3,7 @@ class ExamQuestion < ActiveRecord::Base
   belongs_to :question
 
   def self.create_exam(exam_id, category, n_questions)
-    random_questions = Question.where(category: category).order('random()').limit(n_questions)
+    random_questions = Question.where(category: category).random_records(n_questions)
     random_questions.each do |rq|
       ExamQuestion.create(exam_id: exam_id, question_id: rq.id)
     end
