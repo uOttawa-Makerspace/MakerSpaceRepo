fc76e93e967bed8e3befbd810fff5bcc49bf893c
brunomrlima <bruno.mrlima@gmail.com>
2019-07-16 16:54:31 -0400

    exam squeleton

--- app/controllers/exams_controller.rb
+++ app/controllers/exams_controller.rb
@@ -1,2 +1,49 @@
 class ExamsController < ApplicationController
+  layout 'staff_area'
+  before_action :current_user
+  before_action :grant_access
+
+  def index
+    @exams = Exam.all.order(category: :desc).paginate(:page => params[:page], :per_page => 50)
+  end
+
+  def new
+    @new_exam = Exam.new
+    @categories = Question::CATEGORIES
+  end
+
+  def create
+    @new_exam = current_user.exams.new(exam_params)
+    if @new_exam.save!
+      redirect_to exams_path
+      flash[:notice] = "You've successfully created a new exam!"
+    end
+  end
+
+  def show
+    @exam = Exam.find(params[:id])
+  end
+
+  def destroy
+    exam = Exam.find(params[:id])
+    if exam.destroy
+      flash[:notice] = "Exam Deleted"
+    else
+      flash[:alert] = "Something went wrong"
+    end
+    redirect_to exams_path
+  end
+
+  private
+
+  def grant_access
+    unless current_user.staff? || current_user.admin?
+      flash[:alert] = "You cannot access this area."
+      redirect_to root_path
+    end
+  end
+
+  def exam_params
+    params.require(:exam).permit(:category)
+  end
 end
