6d6acfca7f9b35dd67194ded5bc1580e8059b4be
brunomrlima <bruno.mrlima@gmail.com>
2019-07-16 17:52:06 -0400

    create exam in exam question

--- app/models/exam_question.rb
+++ app/models/exam_question.rb
@@ -1,4 +1,11 @@
 class ExamQuestion < ActiveRecord::Base
   belongs_to :exam
   belongs_to :question
+
+  def self.create_exam(exam_id, category, n_questions)
+    random_questions = Question.where(category: category).order('random()').limit(n_questions)
+    random_questions.each do |rq|
+      ExamQuestion.create(exam_id: exam_id, question_id: rq.id)
+    end
+  end
 end
