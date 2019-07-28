8c23cf4ed9e77711ce965532dc61c9d10ae04899
brunomrlima <bruno.mrlima@gmail.com>
2019-07-28 15:48:59 -0400

    better name

--- app/controllers/exams_controller.rb
+++ app/controllers/exams_controller.rb
@@ -14,7 +14,7 @@ class ExamsController < ApplicationController
   def create
     @new_exam = current_user.exams.new(exam_params)
     @new_exam.save!
-    ExamQuestion.create_exam(@new_exam.id, @new_exam.category, 3)
+    ExamQuestion.create_exam_questions(@new_exam.id, @new_exam.category, 3)
     if @new_exam.save!
       redirect_to exams_path
       flash[:notice] = "You've successfully created a new exam!"
