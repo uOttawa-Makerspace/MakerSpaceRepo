8515bc9ccfb93ce631f9c679cf806baa75b76855
brunomrlima <bruno.mrlima@gmail.com>
2019-07-30 13:43:07 -0400

    moving js

--- app/assets/javascripts/question_responses.js
+++ app/assets/javascripts/question_responses.js
@@ -1,1 +1,20 @@
+function questionResponse(answer_id){
+    $.ajax({
+        url: "/question_responses#create",
+        type: "POST",
+        dataType: 'json',
+        data: {
+            answer_id: answer_id
+        }
+    });
+    changeAnswerClass()
+}
 
+function changeAnswerClass(){
+    var clickedAnswer = $(event.target);
+    var allAnswersSelected = clickedAnswer.parent().children("p.selected-answers");
+    allAnswersSelected.removeClass('selected-answers');
+    allAnswersSelected.addClass('answers');
+    clickedAnswer.removeClass('answers');
+    clickedAnswer.addClass('selected-answers');
+}
--- app/controllers/question_responses_controller.rb
+++ app/controllers/question_responses_controller.rb
@@ -1,5 +1,21 @@
 class QuestionResponsesController < ApplicationController
-  def create
+  before_action :current_user
 
+
+
+  def create
+    puts "answer_id:"
+    puts params[:answer_id]
+    respond_to do |format|
+      format.js { render nothing: true }
+    end
+  #   @new_exam = current_user.exams.new(exam_params)
+  #   @new_exam.save!
+  #   ExamQuestion.create_exam_questions(@new_exam.id, @new_exam.category, 3)
+  #   if @new_exam.save!
+  #     redirect_to exams_path
+  #     flash[:notice] = "You've successfully created a new exam!"
+  #   end
   end
+
 end
