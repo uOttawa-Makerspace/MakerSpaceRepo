62f5f102d23c9ba8095a35c8b3c234c807a85a0c
brunomrlima <bruno.mrlima@gmail.com>
2019-07-31 12:02:20 -0400

    statuses

--- app/models/exam.rb
+++ app/models/exam.rb
@@ -2,4 +2,10 @@ class Exam < ActiveRecord::Base
   belongs_to :user
   has_many :exam_questions,  dependent: :destroy
   has_many :question_responses
+
+  STATUS = { :not_started => "not started",
+              :started => "started",
+              :incomplete => "incomplete",
+              :passed => "passed",
+              :failed => "failed"}
 end
