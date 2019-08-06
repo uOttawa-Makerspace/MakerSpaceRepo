4498ba186e8786802618754037bc873cac0e2a69
brunomrlima <bruno.mrlima@gmail.com>
2019-08-06 17:58:47 -0400

    migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190806155211) do
+ActiveRecord::Schema.define(version: 20190806215710) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -115,6 +115,7 @@ ActiveRecord::Schema.define(version: 20190806155211) do
     t.datetime "created_at",                         null: false
     t.datetime "updated_at",                         null: false
     t.string   "status",     default: "not started"
+    t.integer  "score"
   end
 
   create_table "lab_sessions", force: :cascade do |t|
