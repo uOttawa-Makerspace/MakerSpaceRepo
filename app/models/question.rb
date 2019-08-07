13584f3f2b0337a817ac80d6b081099516fc3137
brunomrlima <bruno.mrlima@gmail.com>
2019-08-07 15:08:42 -0400

    categories ready

--- app/models/question.rb
+++ app/models/question.rb
@@ -18,6 +18,8 @@ class Question < ActiveRecord::Base
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
+  # CATEGORIES = (Training.all_training_names << "General Satefy").sort
+
   CATEGORIES = ["Basic Training",
                 "Mill Training",
                 "Lathe Training",
