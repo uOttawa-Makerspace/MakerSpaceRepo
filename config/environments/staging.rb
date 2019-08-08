7626d2556c3b55e3f163fb7ed4f8bd0500309eb0
brunomrlima <bruno.mrlima@gmail.com>
2019-08-08 15:22:50 -0400

    adding global variables to other environment

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  $n_exams_question = 20
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
