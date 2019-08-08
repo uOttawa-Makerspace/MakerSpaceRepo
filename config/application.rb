76d10f9af9ffd683bbe7e7c24450ddf9c5a5cabc
brunomrlima <bruno.mrlima@gmail.com>
2019-08-08 14:24:45 -0400

    global variables now implementd

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,6 @@ Bundler.require(*Rails.groups)
 
 module MakerSpaceRepo
   class Application < Rails::Application
-    $n_exams_question = 20
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
