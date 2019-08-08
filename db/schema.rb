d109aa5cafd09d53fc4291dec0afe93dbd42c5ca
brunomrlima <bruno.mrlima@gmail.com>
2019-08-08 13:58:37 -0400

    migration succeed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190806215710) do
+ActiveRecord::Schema.define(version: 20190808175714) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -112,10 +112,11 @@ ActiveRecord::Schema.define(version: 20190806215710) do
   create_table "exams", force: :cascade do |t|
     t.integer  "user_id"
     t.string   "category"
-    t.datetime "created_at",                         null: false
-    t.datetime "updated_at",                         null: false
-    t.string   "status",     default: "not started"
+    t.datetime "created_at",                                  null: false
+    t.datetime "updated_at",                                  null: false
+    t.string   "status",              default: "not started"
     t.integer  "score"
+    t.integer  "training_session_id"
   end
 
   create_table "lab_sessions", force: :cascade do |t|
@@ -164,6 +165,24 @@ ActiveRecord::Schema.define(version: 20190806215710) do
   end
 
   add_index "pi_readers", ["space_id"], name: "index_pi_readers_on_space_id", using: :btree
+
+  create_table "print_orders", force: :cascade do |t|
+    t.integer  "user_id"
+    t.boolean  "approved"
+    t.boolean  "printed"
+    t.text     "comments"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
+    t.string   "file_file_name"
+    t.string   "file_content_type"
+    t.integer  "file_file_size"
+    t.datetime "file_updated_at"
+    t.float    "quote"
+    t.integer  "staff_id"
+    t.boolean  "user_approval"
+    t.text     "staff_comments"
+    t.boolean  "expedited"
+  end
 
   create_table "printer_sessions", force: :cascade do |t|
     t.datetime "created_at", null: false
