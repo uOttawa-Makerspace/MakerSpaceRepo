ee64c02027b9dcd13cc73b559fc9cd0565a40d3d
brunomrlima <bruno.mrlima@gmail.com>
2019-08-09 11:38:56 -0400

    user cannot go back after finishing exam

---
+++ app/assets/javascripts/exams.js
@@ -0,0 +1,11 @@
+$(document).on('ready load', function () {
+    window.addEventListener( "pageshow", function ( event ) {
+        var historyTraversal = event.persisted ||
+            ( typeof window.performance != "undefined" &&
+                window.performance.navigation.type === 2 );
+        if ( historyTraversal ) {
+            // Handle page restore.
+            window.location.reload();
+        }
+    });
+});
