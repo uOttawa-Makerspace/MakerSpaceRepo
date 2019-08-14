9b612f2ccc61044cf7c74806fb3cbc9f99d074ab
Arthur Fetiveau <art.fetiveau@gmail.com>
2019-08-13 20:31:34 -0400

    Added timestamp on Approved by User, Changed Order to put Expedited on top and then sorted by older first, Added possibilty to upload a second file for staff to print or laser cut

---
+++ db/migrate/20190814001025_add_final_file_to_print_order.rb
@@ -0,0 +1,11 @@
+class AddFinalFileToPrintOrder < ActiveRecord::Migration
+  def self.up
+    change_table :print_orders do |t|
+      t.attachment :final_file
+    end
+  end
+
+  def self.down
+    remove_attachment :print_orders, :final_file
+  end
+end
