c926cf8fd18a69a3060fa3e82bae286171403037
brunomrlima <bruno.mrlima@gmail.com>
2019-08-28 12:20:33 -0400

    removing categories

--- app/models/question.rb
+++ app/models/question.rb
@@ -18,49 +18,4 @@ class Question < ActiveRecord::Base
   accepts_nested_attributes_for :answers
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
-
-  CATEGORIES = (Training.all_training_names << "General Satefy").sort
-
-  # TODO: Clean this when finished
-
-  # CATEGORIES = ["Basic Training",
-  #               "Mill Training",
-  #               "Lathe Training",
-  #               "MIG Training",
-  #               "TIG Training",
-  #               "General Satefy",
-  #               "Basic 3D Printing",
-  #               "Advanced 3D Printing",
-  #               "Basic Laser Cutting",
-  #               "Advanced Laser Cutting",
-  #               "Basic Arduino",
-  #               "Advanced Arduino",
-  #               "Embroidery",
-  #               "CAD modeling",
-  #               "3D Scanning",
-  #               "Virtual Reality",
-  #               "Soldering"]
-
-  # ["3D Printing Basics",
-  #  "3D scanning",
-  #  "Advanced 3D Printing",
-  #  "Arduino Basics",
-  #  "Basic Lathe Training",
-  #  "Basic Mill Training",
-  #  "Basic TIG ",
-  #  "Basic Training",
-  #  "Embroidery",
-  #  "GNG 3D Printing training",
-  #  "GNG 3D printing training",
-  #  "GNG Laser training",
-  #  "GNG laser training",
-  #  "Laser Cutting Basics",
-  #  "MCG Basic Lathe Training",
-  #  "MCG Basic Mill Training ",
-  #  "Makerspace Volunteer",
-  #  "Mig Welding Training",
-  #  "PCB Design",
-  #  "Soldeing Basics",
-  #  "Tig Welding Training",
-  #  "Welding Safety and Basic MIG"]
 end
