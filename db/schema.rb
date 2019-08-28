aa292e1d8d1dd85b909e464fd4c9fb1b0789e84e
brunomrlima <bruno.mrlima@gmail.com>
2019-08-28 12:24:18 -0400

    removing category from question

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190828153528) do
+ActiveRecord::Schema.define(version: 20190828162322) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -244,7 +244,6 @@ ActiveRecord::Schema.define(version: 20190828153528) do
   create_table "questions", force: :cascade do |t|
     t.integer  "user_id"
     t.text     "description"
-    t.string   "category"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
     t.string   "image_file_name"
