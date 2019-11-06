9bf6a48e347b8c0d6d810b46b54900e385d93bf6
Arthur Fetiveau <art.fetiveau@gmail.com>
2019-11-05 20:54:58 -0500

    Fixed bug for print order file upload and added print id on emails to makerspace@uottawa.ca

--- app/mailers/msr_mailer.rb
+++ app/mailers/msr_mailer.rb
@@ -11,12 +11,12 @@ class MsrMailer < ApplicationMailer
 		mail(to: 'bruno.mrlima@gmail.com', subject: 'Richard L\'Abbé Makerspace Survey 2019', bcc: all_users)
 	end
 
-	def send_print_user_approval_to_makerspace
-		mail(to: "makerspace@uottawa.ca", subject: 'Print Order has been accepted by user on Makerepo')
+	def send_print_user_approval_to_makerspace(id)
+		mail(to: "makerspace@uottawa.ca", subject: 'Print Order has been accepted by user on Makerepo | Print ID : '+id.to_s)
 	end
 
-	def send_print_to_makerspace
-		mail(to: "makerspace@uottawa.ca", subject: 'Print Order has been submitted on Makerepo')
+	def send_print_to_makerspace(id)
+		mail(to: "makerspace@uottawa.ca", subject: 'Print Order has been submitted on Makerepo | Print ID : '+id.to_s)
 	end
 
 	def send_print_quote(expedited_price, user, print_order, comments)
