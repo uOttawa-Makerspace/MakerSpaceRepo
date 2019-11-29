ca2982fcc5771c110a664aff7cde15e45c6dac58
brunomrlima <bruno.mrlima@gmail.com>
2019-11-29 17:07:50 -0500

    adding your request page

--- app/models/volunteer_task_request.rb
+++ app/models/volunteer_task_request.rb
@@ -1,4 +1,9 @@
 class VolunteerTaskRequest < ActiveRecord::Base
   belongs_to :volunteer_task
   belongs_to :user
+
+  scope :approved, -> {where(:approval => true)}
+  scope :rejected, -> {where(:approval => false)}
+  scope :not_processed, -> {where(:approval => nil)}
+  scope :processed, -> {where(:approval => [false, true])}
 end
