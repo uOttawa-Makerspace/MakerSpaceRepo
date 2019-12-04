50c756764b595d5cbe61e0e95ff1bcdf4f15f757
brunomrlima <bruno.mrlima@gmail.com>
2019-12-04 17:50:23 -0500

    not active and active joins and

--- app/models/volunteer_task_join.rb
+++ app/models/volunteer_task_join.rb
@@ -3,4 +3,7 @@ class VolunteerTaskJoin < ActiveRecord::Base
   belongs_to :user
 
   validates_uniqueness_of :user_id, scope: :volunteer_task_id
+
+  scope :active, -> {where(active: true)}
+  scope :not_active, -> {where(active: false)}
 end
