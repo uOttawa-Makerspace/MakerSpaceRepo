81d3db84e49d413b1124e55854e76e98ebe5324a
brunomrlima <bruno.mrlima@gmail.com>
2020-01-29 14:58:56 -0500

    volunteer and development program

--- app/models/program.rb
+++ app/models/program.rb
@@ -4,4 +4,6 @@ class Program < ActiveRecord::Base
             presence: { message: "A user is required." },
             uniqueness: { scope: :program_type, message: "An user can only join a program once" }
   validates :program_type, presence: { message: "The type of the program is required. Either 'Volunteer Program' or 'Development Program'" }
+  VOLUNTEER = "Volunteer Program"
+  DEV_PROGRAM = "Development Program"
 end
