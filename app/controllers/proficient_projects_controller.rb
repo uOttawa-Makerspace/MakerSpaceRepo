d489f806a790ce84a40a9a487b3e129fcd05a76d
brunomrlima <bruno.mrlima@gmail.com>
2020-01-30 17:10:32 -0500

    only staff can create new proficient projects

--- app/controllers/proficient_projects_controller.rb
+++ app/controllers/proficient_projects_controller.rb
@@ -1,5 +1,6 @@
 class ProficientProjectsController < DevelopmentProgramsController
   before_action :grant_access_to_project, only: [:show]
+  before_action :only_staff_access, only: [:new]
 
   def index
   end
@@ -20,6 +21,13 @@ class ProficientProjectsController < DevelopmentProgramsController
     unless current_user.dev_program? || current_user.admin? || current_user.staff?
       redirect_to root_path
       flash[:alert] = "You cannot access this area."
+    end
+  end
+
+  def only_staff_access
+    unless current_user.staff?
+      redirect_to development_programs_path
+      flash[:alert] = "Only staff members can access this area."
     end
   end
 
