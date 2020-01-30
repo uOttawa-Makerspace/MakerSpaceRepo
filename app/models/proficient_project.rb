76bfc09ca1c218a3a891a9cb4182f880da6e360e
brunomrlima <bruno.mrlima@gmail.com>
2020-01-30 17:55:51 -0500

    relation between training and proficient projects

--- app/models/proficient_project.rb
+++ app/models/proficient_project.rb
@@ -1,3 +1,4 @@
 class ProficientProject < ActiveRecord::Base
   has_and_belongs_to_many :users
+  belongs_to :training
 end
--- app/models/training.rb
+++ app/models/training.rb
@@ -4,6 +4,7 @@ class Training < ActiveRecord::Base
   has_many :certifications, through: :training_sessions
   has_many :require_trainings, dependent: :destroy
   has_many :questions, dependent: :destroy
+  has_many :proficient_projects, dependent: :destroy
 
   validates :name, presence: true, uniqueness: true
 
