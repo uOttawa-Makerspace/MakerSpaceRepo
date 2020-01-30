a2e612ad8b84eb811fb31760a3478e76b4a5a640
brunomrlima <bruno.mrlima@gmail.com>
2020-01-30 18:18:57 -0500

    adding proficient projects to file and photos

--- app/models/repo_file.rb
+++ app/models/repo_file.rb
@@ -1,6 +1,7 @@
 class RepoFile < ActiveRecord::Base
   belongs_to :repository
-  
+  belongs_to :proficient_project
+
   has_attached_file :file,
                     url: "/system/repo_files/:id/:basename.:extension"
   validates_attachment_content_type :file, :content_type => /\A*\/.*\Z/
