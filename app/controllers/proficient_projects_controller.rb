cd7d906f0a4279e5f8158778e0ff4912fe3b5b81
brunomrlima <bruno.mrlima@gmail.com>
2020-02-12 14:11:27 -0500

    deleting proficient project working now

--- app/controllers/proficient_projects_controller.rb
+++ app/controllers/proficient_projects_controller.rb
@@ -1,6 +1,7 @@
 class ProficientProjectsController < DevelopmentProgramsController
   before_action :grant_access_to_project, only: [:show]
   before_action :only_staff_access, only: [:new, :create]
+  before_action :set_proficient_project, only: [:show, :destroy]
 
   def index
     @proficient_projects = ProficientProject.all.order(created_at: :desc).paginate(:page => params[:page], :per_page => 50)
@@ -13,7 +14,6 @@ class ProficientProjectsController < DevelopmentProgramsController
   end
 
   def show
-    @proficient_project= ProficientProject.find(params[:id])
     @photos = @proficient_project.photos || []
     @files = @proficient_project.repo_files.order(created_at: :asc)
     @videos = @proficient_project.videos.order(created_at: :asc)
@@ -79,6 +79,10 @@ class ProficientProjectsController < DevelopmentProgramsController
     params['videos'].each do |f|
       Video.create(video: f, proficient_project_id: @proficient_project.id)
     end if params['videos'].present?
+  end
+
+  def set_proficient_project
+    @proficient_project= ProficientProject.find(params[:id])
   end
 
 end
