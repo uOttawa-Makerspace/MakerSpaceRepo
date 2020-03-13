e1695858b2260811664f4002a1bfa99c6ead0dc1
Nicolas Gnyra <nicolas.gnyra@gmail.com>
2020-03-13 00:14:46 -0400

    Fix log files in staging/production

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -54,8 +54,8 @@ Rails.application.configure do
   # config.log_tags = [ :subdomain, :uuid ]
 
   # Use a different logger for distributed setups.
-  # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
-  config.logger = Logger.new(STDOUT)
+  config.logger = Logger.new("#{Rails.root}/log/#{Rails.env}.log")
+
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
