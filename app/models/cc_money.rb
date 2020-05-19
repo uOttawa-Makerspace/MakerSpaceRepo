117402f448dc11e92d6925c0c2fd7fe791bea458
brunomrlima <bruno.mrlima@gmail.com>
2020-05-19 11:33:47 -0400

    adding relation cc money and proficient project

--- app/models/cc_money.rb
+++ app/models/cc_money.rb
@@ -1,6 +1,7 @@
 class CcMoney < ActiveRecord::Base
   belongs_to :user
   belongs_to :volunteer_task
+  belongs_to :proficient_project
 
   def self.create_cc_money_from_approval(volunteer_task_id, volunteer_id, cc)
     self.create(volunteer_task_id: volunteer_task_id, user_id: volunteer_id, cc: cc)
