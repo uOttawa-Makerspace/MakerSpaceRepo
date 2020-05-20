3c43563316955ef9942c96cf35381a24ceb4c7b5
Arthur Fetiveau <art.fetiveau@gmail.com>
2020-05-20 11:45:54 -0400

    Added more information in the development program

--- db/schema.rb
+++ db/schema.rb
@@ -232,6 +232,8 @@ ActiveRecord::Schema.define(version: 20200519191150) do
     t.text     "staff_comments"
     t.boolean  "expedited"
     t.integer  "order_type",              default: 0
+    t.text     "email"
+    t.text     "name"
     t.datetime "timestamp_approved"
     t.string   "final_file_file_name"
     t.string   "final_file_content_type"
@@ -264,6 +266,7 @@ ActiveRecord::Schema.define(version: 20200519191150) do
     t.string   "status",       default: "true"
     t.string   "availability", default: "true"
     t.string   "color",        default: "FF0000"
+    t.string   "rfid"
   end
 
   create_table "proficient_projects", force: :cascade do |t|
