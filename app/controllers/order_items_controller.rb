4d774250e20433e4009dd9466d6683484a77b680
Arthur Fetiveau <art.fetiveau@gmail.com>
2020-05-22 13:48:22 -0400

    Blocking user from adding more than once a pp

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -4,9 +4,7 @@ class OrderItemsController < DevelopmentProgramsController
     @order.user = current_user
     @order_item = @order.order_items.new(order_item_params)
     existing_order = @order.order_items.where(proficient_project_id: params[:order_item][:proficient_project_id])
-    if existing_order.count >= 1
-      existing_order.last.update_column(:quantity, existing_order.last.quantity + params[:order_item][:quantity].to_i)
-    else
+    unless existing_order.count >= 1
       @order.save
     end
     session[:order_id] = @order.id
