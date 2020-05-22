6f0b9b56a60f8872e5e97dea2e2433fbdffb962a
brunomrlima <bruno.mrlima@gmail.com>
2020-05-22 11:00:16 -0400

    adding delete to views

--- app/models/proficient_project.rb
+++ app/models/proficient_project.rb
@@ -4,15 +4,15 @@ class ProficientProject < ActiveRecord::Base
   before_save :capitalize_title
   has_and_belongs_to_many :users
   belongs_to :training
-  has_many :photos,       dependent: :destroy
-  has_many :repo_files,   dependent: :destroy
-  has_many :videos,       dependent: :destroy
-  has_many :project_requirements
+  has_many :photos,                     dependent: :destroy
+  has_many :repo_files,                 dependent: :destroy
+  has_many :videos,                     dependent: :destroy
+  has_many :project_requirements,       dependent: :destroy
   has_many :required_projects, through: :project_requirements
   has_many :inverse_project_requirements, class_name: "ProjectRequirement", foreign_key: "required_project_id"
   has_many :inverse_required_projects, through: :inverse_project_requirements, source: :proficient_project
-  has_many :cc_moneys
-  has_many :order_items
+  has_many :cc_moneys,                  dependent: :destroy
+  has_many :order_items,                dependent: :destroy
 
   scope :filter_by_level, -> (level) { where(level: level) }
   scope :filter_by_proficiency, -> (proficient) { where(proficient: proficient) }
