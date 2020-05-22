7ac2491170cc7214099738a7e29ed469ef2941c8
brunomrlima <bruno.mrlima@gmail.com>
2020-05-22 09:55:02 -0400

    maker choose video disapear if doesnt have PP

--- app/assets/javascripts/videos.js
+++ app/assets/javascripts/videos.js
@@ -1,8 +1,7 @@
 function setProficientProject(){
     var proficient_project_id = document.getElementById("select_proficient_id").value;
     document.getElementById("proficient_project").innerHTML=("Proficient Project id chosen: " + proficient_project_id);
-    document.getElementById("choose_video").classList.remove('d-none');
-    s3_uploader(proficient_project_id)
+    check_proficient_project(proficient_project_id)
 }
 
 function s3_uploader(proficient_project_id) {
@@ -27,3 +26,12 @@ function s3_uploader(proficient_project_id) {
         return alert(content.filename + ' failed to upload.');
     });
 };
+
+function check_proficient_project(proficient_project_id) {
+    if(proficient_project_id.length === 0){
+        document.getElementById("choose_video").classList.add('d-none');
+    }else{
+        document.getElementById("choose_video").classList.remove('d-none');
+        s3_uploader(proficient_project_id)
+    }
+}
