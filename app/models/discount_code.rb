ba7b7b2f16147b8de73301916c290f656f8c58c1
brunomrlima <bruno.mrlima@gmail.com>
2020-06-08 13:13:41 -0400

    adding validations to discount codes and price rule

--- app/models/discount_code.rb
+++ app/models/discount_code.rb
@@ -1,4 +1,6 @@
 class DiscountCode < ActiveRecord::Base
   belongs_to :price_rule
   belongs_to :user
+  validates :shopify_discount_code_id, presence: true
+  validates :code, presence: true
 end
--- app/models/price_rule.rb
+++ app/models/price_rule.rb
@@ -1,3 +1,5 @@
 class PriceRule < ActiveRecord::Base
   has_many :discount_codes, dependent: :destroy
+  validates :shopify_price_rule_id, presence: true
+  validates :title, presence: true
 end
