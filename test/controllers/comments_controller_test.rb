b7aabffe3144f51d1d45f07fc29a8332110186ba
brunomrlima <bruno.mrlima@gmail.com>
2020-06-08 09:16:09 -0400

    updating test date in comments controller

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -4,21 +4,21 @@ class CommentsControllerTest < ActionController::TestCase
 
   test "users can delete their own comments" do
     session[:user_id] = User.find_by(username: "bob").id
-    session[:expires_at] = "Sat, 03 Jun 2020 05:01:41 UTC +00:00"
+    session[:expires_at] = "Sat, 03 Jun 2030 05:01:41 UTC +00:00"
     delete :destroy,  id: 1
     assert_equal flash[:notice], "Comment deleted succesfully"
   end
 
   test "admins can delete any comment" do
     session[:user_id] = User.find_by(username: "adam").id
-    session[:expires_at] = "Sat, 03 Jun 2020 05:01:41 UTC +00:00"
+    session[:expires_at] = "Sat, 03 Jun 2030 05:01:41 UTC +00:00"
     delete :destroy, id: 1
     assert_equal flash[:notice], "Comment deleted succesfully"
   end
 
   test "users can't delete others' comments" do
     session[:user_id] = User.find_by(username: "bob").id
-    session[:expires_at] = "Sat, 03 Jun 2020 05:01:41 UTC +00:00"
+    session[:expires_at] = "Sat, 03 Jun 2030 05:01:41 UTC +00:00"
     delete :destroy, id: 2
     assert_equal flash[:alert], "Something went wrong"
   end
