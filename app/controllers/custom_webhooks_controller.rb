cba4063412153a06b9db4f46d4e886d0ffc9541d
brunomrlima <bruno.mrlima@gmail.com>
2020-06-10 13:38:10 -0400

    deleting the puts and error in discount code

--- app/controllers/custom_webhooks_controller.rb
+++ app/controllers/custom_webhooks_controller.rb
@@ -3,12 +3,10 @@ class CustomWebhooksController < ApplicationController
 
   def orders_paid
     params.permit!
-    10.times{puts "passing HERE"}
     discount_code_params = webhook_params.to_h
-    p discount_code_params['discount_codes']
     if discount_code_params['discount_codes'].present?
       shopify_discount_code_id = discount_code_params['discount_codes']['id']
-      discount_code = DiscountCode.find_by_id(shopify_discount_code_id)
+      discount_code = DiscountCode.find_by(shopify_discount_code_id: shopify_discount_code_id)
       discount_code.update_attributes(usage_count: 1) if discount_code
     end
     head :ok
