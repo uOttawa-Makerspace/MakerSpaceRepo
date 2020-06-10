cc5d26bcded190622133731479f4b3b08980533f
brunomrlima <bruno.mrlima@gmail.com>
2020-06-10 13:45:32 -0400

    give travis something

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,5 +1,5 @@
 ShopifyApp.configure do |config|
-  config.secret = Rails.application.secrets.shopify_webhook || ENV.fetch('SHOPIFY_WEBHOOK')
+  config.secret = Rails.application.secrets.shopify_webhook || ENV.fetch('SHOPIFY_WEBHOOK', 'travis')
   config.webhooks = [
       {topic: 'orders/paid', address: 'http://localhost:3000/custom_webooks/orders_paid', fields: ['discount_codes']}
   ]
