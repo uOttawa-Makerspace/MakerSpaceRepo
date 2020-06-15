e70cda478c5ed0973e3db3ed92af6a31be7cccee
Arthur Fetiveau <art.fetiveau@gmail.com>
2020-06-15 11:36:31 -0400

    Changed framework_defaults

--- config/initializers/new_framework_defaults.rb
+++ config/initializers/new_framework_defaults.rb
@@ -16,7 +16,7 @@ Rails.application.config.action_controller.forgery_protection_origin_check = tru
 
 # Make Ruby 2.4 preserve the timezone of the receiver when calling `to_time`.
 # Previous versions had false.
-ActiveSupport.to_time_preserves_timezone = false
+ActiveSupport.to_time_preserves_timezone = true
 
 # Require `belongs_to` associations by default. Previous versions had false.
 # TODO: set this to true and update all belongs_to (choose the ones that are optional and the ones that are not: belongs_to :model, optional: true)
