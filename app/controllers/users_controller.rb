0def2154d176cd0b564ade1f5944007087f60a87
Arthur Fetiveau <art.fetiveau@gmail.com>
2020-06-24 16:09:53 -0400

    Removed a bit of margin and changed per page to 18

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -69,9 +69,9 @@ class UsersController < SessionsController
     @repo_user = User.find_by username: params[:username]
     @github_username = Octokit::Client.new(access_token: @repo_user.access_token).login
     @repositories = if params[:username] == @user.username || @user.admin? || @user.staff?
-                      @repo_user.repositories.where(make_id: nil).paginate(page: params[:page], per_page: 20)
+                      @repo_user.repositories.where(make_id: nil).paginate(page: params[:page], per_page: 18)
                     else
-                      @repo_user.repositories.public_repos.where(make_id: nil).paginate(page: params[:page], per_page: 20)
+                      @repo_user.repositories.public_repos.where(make_id: nil).paginate(page: params[:page], per_page: 18)
                     end
 
     @acclaim_badge_url = if params[:username] == @user.username
