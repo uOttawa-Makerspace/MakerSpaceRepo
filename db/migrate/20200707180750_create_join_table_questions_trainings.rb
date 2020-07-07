e4a59ce7a3d35b1850153dd1b2f45724fb888509
brunomrlima <bruno.mrlima@gmail.com>
2020-07-07 15:12:16 -0400

    do not populate tables in migration

--- db/migrate/20200707180750_create_join_table_questions_trainings.rb
+++ db/migrate/20200707180750_create_join_table_questions_trainings.rb
@@ -4,14 +4,5 @@ class CreateJoinTableQuestionsTrainings < ActiveRecord::Migration[5.2]
       # t.index [:question_id, :training_id]
       # t.index [:training_id, :question_id]
     end
-    populate_tables
-  end
-
-  def populate_tables
-    Training.all.find_each do |t|
-      Question.where(training_id: t.id).find_each do |q|
-        t.questions <<  q
-      end
-    end
   end
 end
