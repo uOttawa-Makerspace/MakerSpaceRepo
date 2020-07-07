2e0b7e76b8a03b59cfe8d3b3cf479414fa0b16eb
brunomrlima <bruno.mrlima@gmail.com>
2020-07-07 16:13:21 -0400

    dependent destroy removed

--- app/models/training.rb
+++ app/models/training.rb
@@ -2,7 +2,7 @@
 
 class Training < ApplicationRecord
   has_and_belongs_to_many :spaces
-  has_and_belongs_to_many :questions, dependent: :destroy
+  has_and_belongs_to_many :questions
   has_many :training_sessions,    dependent: :destroy
   has_many :certifications,       through: :training_sessions
   has_many :require_trainings,    dependent: :destroy
