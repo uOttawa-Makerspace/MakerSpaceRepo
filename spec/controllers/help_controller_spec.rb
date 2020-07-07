68000e3e3c7b48dbc1ece0eb349c149dfb9392f1
Arthur Fetiveau <art.fetiveau@gmail.com>
2020-07-07 14:36:12 -0400

    Finished Help Tests

---
+++ spec/controllers/help_controller_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe HelpController, type: :controller do
+
+  context 'main' do
+
+    describe 'main' do
+
+      it 'should give a 200' do
+        get :main
+        expect(response).to have_http_status(:success)
+      end
+
+    end
+
+  end
+
+  context 'send_email' do
+
+    describe "send_email" do
+
+      it 'should send the email' do
+        put :send_email, params: {name: "John", email: "john@doe.com", subject: "HELP", message: "HELP !!!"}
+        expect(ActionMailer::Base.deliveries.count).to eq(1)
+        expect(ActionMailer::Base.deliveries.first.to.first).to eq("webmaster@makerepo.com")
+        expect(flash[:notice]).to eq("Email successfuly send. You will be contacted soon.")
+        expect(response).to redirect_to root_path
+      end
+
+    end
+
+  end
+
+end
+
+
---
+++ spec/models/exam_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe Exam, type: :model do
+
+
+
+end
