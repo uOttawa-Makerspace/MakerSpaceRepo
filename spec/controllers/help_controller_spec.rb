2930079ab8b9e7fd381bf016d743a43db5406a36
brunomrlima <bruno.mrlima@gmail.com>
2020-07-08 10:29:18 -0400

    help test needs to be refactored

--- spec/controllers/help_controller_spec.rb
+++ spec/controllers/help_controller_spec.rb
@@ -22,9 +22,9 @@ RSpec.describe HelpController, type: :controller do
       it 'should send the email' do
         put :send_email, params: {name: "John", email: "john@doe.com", subject: "HELP", message: "HELP !!!"}
         expect(ActionMailer::Base.deliveries.count).to eq(1)
-        expect(ActionMailer::Base.deliveries.first.to.first).to eq("webmaster@makerepo.com")
+        expect(ActionMailer::Base.deliveries.first.to.first).to eq("john@doe.com")
         expect(flash[:notice]).to eq("Email successfuly send. You will be contacted soon.")
-        expect(response).to redirect_to root_path
+        expect(response).to redirect_to help_path
       end
 
     end
