f3c4e930b1d14bdba0876c85309d4391d974b1c9
brunomrlima <bruno.mrlima@gmail.com>
2020-07-13 11:48:17 -0400

    print orders controller refactored

--- spec/controllers/print_orders_controller_spec.rb
+++ spec/controllers/print_orders_controller_spec.rb
@@ -2,73 +2,58 @@ require 'rails_helper'
 include FilesTestHelper
 
 RSpec.describe PrintOrdersController, type: :controller do
-  describe 'GET #index' do
-    before(:each) do
-      session[:expires_at] = DateTime.tomorrow.end_of_day
-    end
+  before(:all) do
+    @user = create(:user, :regular_user)
+    @admin = create(:user, :admin)
+    @student = create(:user, :student)
+    @print_order = create(:print_order)
+  end
 
-    context 'user' do
-      it 'should return a 200' do
-        user = create(:user, :regular_user)
-        create(:print_order)
-        session[:user_id] = user.id
+  before(:each) do
+    session[:expires_at] = DateTime.tomorrow.end_of_day
+    session[:user_id] = @user.id
+  end
+
+  describe 'GET /index' do
+    context 'logged as regular user' do
+      it 'should return 200' do
         get :index
         expect(response).to have_http_status(:success)
       end
     end
 
-    context 'admin' do
-      it 'should return a 200' do
-        admin = create(:user, :admin)
-        create :user, :regular_user
-        create(:print_order)
-        session[:user_id] = admin.id
+    context 'logged as admin' do
+      it 'should return 200' do
+        session[:user_id] = @admin.id
         get :index
         expect(response).to have_http_status(:success)
       end
     end
   end
 
-  describe 'GET #new' do
-
-    before(:each) do
-      session[:expires_at] = DateTime.tomorrow.end_of_day
-    end
-
-    context 'new' do
+  describe 'GET /new' do
+    context 'logged as regular user' do
       it 'should return a 200' do
-        user = create :user, :regular_user
-        session[:user_id] = user.id
         get :new
         expect(response).to have_http_status(:success)
       end
     end
 
-    context 'pricing' do
+    context 'pricing values' do
       it 'should return a return internal pricing of 0.15' do
-        user = create(:user, :student)
-        session[:user_id] = user.id
+        session[:user_id] = @student.id
         get :new
         expect(@controller.instance_variable_get(:@table)[0]).to eq(['3D Low (PLA/ABS), (per g)', 0.15, 15])
       end
 
       it 'should return a return external pricing of 0.3' do
-        user = create(:user, :regular_user)
-        session[:user_id] = user.id
         get :new
         expect(@controller.instance_variable_get(:@table)[0]).to eq(['3D Low (PLA/ABS), (per g)', 0.3, 15])
       end
     end
   end
 
-  describe ' POST #create' do
-
-    before(:each) do
-      user = create(:user, :regular_user)
-      session[:expires_at] = DateTime.tomorrow.end_of_day
-      session[:user_id] = user.id
-    end
-
+  describe 'POST /create' do
     context 'create print order' do
       it 'should create a print order with notice' do
         print_order_params = FactoryBot.attributes_for(:print_order)
@@ -92,19 +77,10 @@ RSpec.describe PrintOrdersController, type: :controller do
         expect(flash[:alert]).to eq('The upload as failed ! Make sure the file types are STL for 3D Printing or SVG and PDF for Laser Cutting !')
         expect { post :create, params: {print_order: print_order_params} }.to change(PrintOrder, :count).by(0)
       end
-
     end
   end
 
-
   describe 'PATCH #update' do
-
-    before(:each) do
-      user = create :user, :regular_user
-      session[:expires_at] = DateTime.tomorrow.end_of_day
-      session[:user_id] = user.id
-    end
-
     context 'Update print order to approved' do
       it 'should update the print order and send the quote' do
         print_order = create(:print_order, :with_file)
@@ -151,17 +127,13 @@ RSpec.describe PrintOrdersController, type: :controller do
         expect(ActionMailer::Base.deliveries.count).to eq(2)
         expect(ActionMailer::Base.deliveries.first.to.first).to eq(print_order.user.email)
         expect(ActionMailer::Base.deliveries.second.to.first).to eq("uomakerspaceprintinvoices@gmail.com")
-
       end
     end
   end
 
-  describe 'DELETE #destroy' do
+  describe 'DELETE /destroy' do
     context 'destroy print order' do
       it 'should return a 200' do
-        user = create :user, :regular_user
-        session[:expires_at] = 'Sat, 03 Jun 2030 05:01:41 UTC +00:00'
-        session[:user_id] = user.id
         print_order = create(:print_order)
         expect { delete :destroy, params: {id: print_order.id} }.to change(PrintOrder, :count).by(-1)
         expect(response).to redirect_to print_orders_path
@@ -169,20 +141,14 @@ RSpec.describe PrintOrdersController, type: :controller do
     end
   end
 
-  describe 'GET #invoice' do
+  describe 'GET /invoice' do
     context 'create an invoice for print order' do
-
       it 'should return render a pdf' do
-        user = create :user, :regular_user
-        session[:expires_at] = DateTime.tomorrow.end_of_day
-        session[:user_id] = user.id
         print_order = create(:print_order)
         print_order.printed = true
         get :invoice, params: {print_order_id: print_order.id, format: :pdf}
         expect(response).to have_http_status(:success)
       end
-
     end
   end
-
 end
