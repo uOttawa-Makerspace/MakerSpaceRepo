421549cb5cb45f7859ff62850e97ccac4c85f401
Arthur Fetiveau <art.fetiveau@gmail.com>
2020-07-13 15:13:57 -0400

    Finished controller for project proposals

--- spec/factories/project_proposals.rb
+++ spec/factories/project_proposals.rb
@@ -2,12 +2,13 @@ FactoryBot.define do
   factory :project_proposal do
 
     association :user, :regular_user
+
     description { Faker::Lorem.paragraph }
     title { Faker::Lorem.word }
     username { Faker::Name.unique.first_name }
     email { Faker::Internet.email }
     client { Faker::Name.name }
-    area{ {} }
+    area { "{}" }
     client_type { "individual" }
     client_interest { "Low" }
     client_background { "None" }
@@ -16,6 +17,32 @@ FactoryBot.define do
 
     trait 'normal' do
       youtube_link { "" }
+    end
+
+    trait 'broken' do
+      title { "A$CD!!!" }
+    end
+
+    trait 'approved' do
+      approved { 1 }
+      youtube_link { "" }
+    end
+
+    trait 'joined' do
+      approved { 1 }
+      youtube_link { "" }
+      after(:create) do |pp|
+        ProjectJoin.create(project_proposal_id: pp.id, user_id: User.last.id)
+      end
+    end
+
+    trait 'completed' do
+      approved { 1 }
+      youtube_link { "" }
+      after(:create) do |pp|
+        ProjectJoin.create(project_proposal_id: pp.id, user_id: User.last.id)
+        create(:repository, project_proposal_id: pp.id)
+      end
     end
 
     trait 'bad_link' do
