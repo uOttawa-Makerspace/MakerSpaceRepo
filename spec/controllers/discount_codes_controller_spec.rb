07d4e9d5e2fbfc1761857b1b990a1a12ac498607
brunomrlima <bruno.mrlima@gmail.com>
2020-07-13 09:50:19 -0400

    refactored some of discount codes controller

--- spec/controllers/discount_codes_controller_spec.rb
+++ spec/controllers/discount_codes_controller_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe DiscountCodesController, type: :controller do
-  describe "GET #index" do
-    before :context do
+  describe "GET /index" do
+    before(:context) do
       @user = create(:user, :regular_user)
       @admin = create(:user, :admin)
       Program.create(user_id: @user.id, program_type: Program::DEV_PROGRAM)
@@ -10,15 +10,17 @@ RSpec.describe DiscountCodesController, type: :controller do
       create(:discount_code, :unused, user: @admin)
     end
 
-    context 'index' do
-      it 'should return 200 with regular user' do
+    context 'logged as regular user' do
+      it 'should return 200' do
         session[:user_id] = @user.id
         get :index
         expect(response).to have_http_status(:success)
         expect(@controller.instance_variable_get(:@discount_codes).count).to eq(1)
       end
+    end
 
-      it 'should return 200 with admin' do
+    context 'logged as admin' do
+      it 'should return 200' do
         session[:user_id] = @admin.id
         get :index
         expect(response).to have_http_status(:success)
@@ -32,36 +34,36 @@ RSpec.describe DiscountCodesController, type: :controller do
   end
 
   describe 'Actions' do
-    before :context do
+    before(:context) do
       @shopify_price_rule_id = PriceRule.create_price_rule("10$ Coupon", 10)
       @price_rule = create(:price_rule, shopify_price_rule_id: @shopify_price_rule_id, value: 10)
+      @user = create(:user, :regular_user)
+      Program.create(user_id: @user.id, program_type: Program::DEV_PROGRAM)
+      CcMoney.create(user_id: @user.id, cc: 1000)
     end
-    context 'GET #new' do
-      it 'should be giving a 200' do
-        user = create(:user, :regular_user)
-        session[:user_id] = user.id
-        Program.create(user_id: user.id, program_type: Program::DEV_PROGRAM)
+
+    context 'GET /new' do
+      it 'should return 200' do
+        session[:user_id] = @user.id
         get :new
         expect(response).to have_http_status(:success)
         expect(@controller.instance_variable_get(:@price_rules).count).to eq(3)
       end
     end
 
-    context 'POST #create' do
+    context 'POST /create' do
       it 'should be redirecting to discount code path and creating a discount code' do
-        user = create(:user, :regular_user)
-        session[:user_id] = user.id
-        Program.create(user_id: user.id, program_type: Program::DEV_PROGRAM)
-        CcMoney.create(user_id: user.id, cc: 1000)
+        session[:user_id] = @user.id
         expect { post :create, params: {price_rule_id: @price_rule.id} }.to change(DiscountCode, :count).by(1)
         expect(response).to redirect_to discount_codes_path
         expect(flash[:notice]).to eq('Discount Code created')
       end
+    end
 
-      it 'should be redirecting to root code path and failing to create the discount code' do
-        user = create(:user, :regular_user)
-        session[:user_id] = user.id
-        Program.create(user_id: user.id, program_type: Program::DEV_PROGRAM)
+    context 'POST /create with no CC points' do
+      it 'should fail to create the discount code and redirect to root' do
+        session[:user_id] = @user.id
+        CcMoney.create(user_id: @user.id, cc: -1000)
         post :create, params: {price_rule_id: @price_rule.id}
         expect(response).to redirect_to root_path
         expect(flash[:alert]).to eq('Not enough CC points')
