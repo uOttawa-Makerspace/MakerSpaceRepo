17bdb45c96aee55303840bfa2355545fb78a5b06
brunomrlima <bruno.mrlima@gmail.com>
2020-07-13 10:11:26 -0400

    help controller refactored

--- spec/controllers/help_controller_spec.rb
+++ spec/controllers/help_controller_spec.rb
@@ -1,36 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe HelpController, type: :controller do
-
-  context 'main' do
-
-    describe 'main' do
-
+  context 'GET /main' do
+    describe 'without being logged in' do
       it 'should give a 200' do
         get :main
         expect(response).to have_http_status(:success)
       end
-
     end
-
   end
 
-  context 'send_email' do
-
-    describe "send_email" do
-
-      it 'should send the email' do
+  context 'PUT /send_email' do
+    describe "submit help" do
+      it 'should send an email' do
         put :send_email, params: {name: "John", email: "john@doe.com", subject: "HELP", message: "HELP !!!"}
         expect(ActionMailer::Base.deliveries.count).to eq(1)
         expect(ActionMailer::Base.deliveries.first.to.first).to eq("john@doe.com")
         expect(flash[:notice]).to eq("Email successfuly send. You will be contacted soon.")
         expect(response).to redirect_to help_path
       end
-
     end
-
   end
-
 end
 
 
