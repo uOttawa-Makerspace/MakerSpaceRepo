05e45b0eb0efe61507ae0d2e2db66b82c262b4cb
brunomrlima <bruno.mrlima@gmail.com>
2020-07-14 15:27:46 -0400

    questions controller finished

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -34,7 +34,6 @@ RSpec.describe QuestionsController, type: :controller do
   describe 'GET /new' do
     context 'logged as admin' do
       it 'should return a 200' do
-        session[:user_id] = @admin.id
         get :new
         expect(response).to have_http_status(:success)
       end
@@ -44,7 +43,6 @@ RSpec.describe QuestionsController, type: :controller do
   describe "GET /show" do
     context 'logged as admin' do
       it 'should return 200 response' do
-        session[:user_id] = @admin.id
         get :show, params: {id: @question.id}
         expect(response).to have_http_status(:success)
       end
@@ -54,7 +52,6 @@ RSpec.describe QuestionsController, type: :controller do
   describe "GET /edit" do
     context 'logged as admin' do
       it 'should return 200 response' do
-        session[:user_id] = @admin.id
         get :edit, params: {id: @question.id}
         expect(response).to have_http_status(:success)
       end
@@ -64,7 +61,6 @@ RSpec.describe QuestionsController, type: :controller do
   describe 'POST /create' do
     context 'logged as admin' do
       it 'should create a question with answers and redirect' do
-        session[:user_id] = @admin.id
         answer_params = (0..5).inject([]) { |arr, l| arr << FactoryBot.attributes_for(:answer) }
         question_params = FactoryBot.attributes_for(:question, :answers_attributes => answer_params)
         expect { post :create, params: {question: question_params} }.to change(Question, :count).by(1)
@@ -73,29 +69,27 @@ RSpec.describe QuestionsController, type: :controller do
       end
     end
   end
-  #
-  # describe 'PATCH /update' do
-  #   context 'logged as admin' do
-  #     it 'should update the announcement' do
-  #       session[:user_id] = @admin.id
-  #       patch :update, params: {id: @announcement_all.id, announcement: {public_goal: "volunteer"} }
-  #       expect(response).to redirect_to announcements_path
-  #       expect(Announcement.find(@announcement_all.id).public_goal).to eq("volunteer")
-  #       expect(flash[:notice]).to eq("Announcement updated")
-  #     end
-  #   end
-  # end
-  #
-  # describe "DELETE /destroy" do
-  #   context 'logged as admin' do
-  #     it 'should destroy the announcement' do
-  #       session[:user_id] = @admin.id
-  #       expect {  delete :destroy, params: {id: @announcement_all.id} }.to change(Announcement, :count).by(-1)
-  #       expect(response).to redirect_to announcements_path
-  #       expect(flash[:notice]).to eq("Announcement Deleted")
-  #     end
-  #   end
-  # end
+
+  describe 'PATCH /update' do
+    context 'logged as admin' do
+      it 'should update the announcement' do
+        patch :update, params: {id: @question.id, question: {description: "updated"} }
+        expect(response).to redirect_to questions_path
+        expect(Question.find(@question.id).description).to eq("updated")
+        expect(flash[:notice]).to eq("Question updated")
+      end
+    end
+  end
+
+  describe "DELETE /destroy" do
+    context 'logged as admin' do
+      it 'should destroy the announcement' do
+        expect {  delete :destroy, params: {id: @question.id} }.to change(Question, :count).by(-1)
+        expect(response).to redirect_to questions_path
+        expect(flash[:notice]).to eq("Question Deleted")
+      end
+    end
+  end
 
   after(:all) do
     Question.destroy_all
