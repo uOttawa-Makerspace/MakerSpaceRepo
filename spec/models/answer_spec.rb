ca8fe3dd20172a9b45f47c2514e750cc7247c64e
brunomrlima <bruno.mrlima@gmail.com>
2020-07-15 09:42:08 -0400

    adding two more tests to answer

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,6 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
+  describe 'Default value' do
+    context 'correct field' do
+      it 'should be false by default' do
+        answer = create(:answer)
+        expect(answer.correct).to eq(false)
+      end
+
+      it 'should be true' do
+        answer = create(:answer, :correct)
+        expect(answer.correct).to eq(true)
+      end
+    end
+
+    context 'has_many' do
+      it { should have_many(:exam_responses) }
+    end
+  end
+
   describe 'Association' do
     context 'belongs_to' do
       it { should belong_to(:question) }
