cf5e708e9f0759c062985042a1eaf0d33ba894d5
Arthur Fetiveau <art.fetiveau@gmail.com>
2020-08-04 12:18:21 -0400

    Fixed deprication warnings

--- db/schema.rb
+++ db/schema.rb
@@ -299,6 +299,8 @@ ActiveRecord::Schema.define(version: 2020_08_04_153312) do
     t.text "staff_comments"
     t.boolean "expedited"
     t.integer "order_type", default: 0
+    t.text "email"
+    t.text "name"
     t.datetime "timestamp_approved"
     t.string "final_file_file_name"
     t.string "final_file_content_type"
@@ -334,6 +336,7 @@ ActiveRecord::Schema.define(version: 2020_08_04_153312) do
     t.string "status", default: "true"
     t.string "availability", default: "true"
     t.string "color", default: "FF0000"
+    t.string "rfid"
   end
 
   create_table "proficient_projects", id: :serial, force: :cascade do |t|
