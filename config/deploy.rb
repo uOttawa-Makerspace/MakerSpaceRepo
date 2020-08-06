d5c80d5a4037be19b0fc1aa7caf7318dd14c9fc7
Arthur Fetiveau <art.fetiveau@gmail.com>
2020-08-06 13:46:27 -0400

    Added things to linked_dirs

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.5.3'
 set :linked_files, %w{config/master.key}
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
-set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'certs')
+set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'certs', 'node_modules', 'public/packs')
 set :default_env, { 'PASSENGER_INSTANCE_REGISTRY_DIR' => '/var/passenger_instance_registry' }
 
 before "deploy:assets:precompile", "deploy:yarn_install"
