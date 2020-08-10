05808e0fa20c4043bc841366d0c9329f82e3c442
brunomrlima <bruno.mrlima@gmail.com>
2020-08-10 14:51:56 -0400

    spec done

--- app/controllers/custom_webhooks_controller.rb
+++ app/controllers/custom_webhooks_controller.rb
@@ -6,28 +6,31 @@ class CustomWebhooksController < ApplicationController
   def orders_paid
     params.permit!
     discount_code_params = webhook_params.to_h
-    puts(discount_code_params['line_items'])
     if discount_code_params['discount_codes'].present?
       shopify_discount_code = discount_code_params['discount_codes'][0]['code']
       discount_code = DiscountCode.find_by(code: shopify_discount_code)
       discount_code&.update(usage_count: 1)
     end
 
-    if discount_code_params['line_items'].present?
-      discount_code_params['line_items'].each do |item|
-        if item['product_id'] == 4359597129784
-          cc = 10*item['quantity']
-          CcMoney.create(user_id: User.find_by_email(discount_code_params['customer']['email']).id, cc: cc)
-        end
-      end
-    end
+    byebug
+
+    discount_code_params['line_items'].each do |item|
+      increment_cc_money(item, discount_code_params['customer']['email'])
+    end if discount_code_params['line_items'].present?
 
     head :ok
   end
 
   private
 
-  def webhook_params
-    params.except(:controller, :action, :type)
-  end
+    def webhook_params
+      params.except(:controller, :action, :type)
+    end
+
+    def increment_cc_money(product_params, email)
+      if product_params['product_id'].to_i == 4359597129784
+        cc = 10*product_params['quantity'].to_i
+        CcMoney.create(user: User.find_by(email: email), cc: cc)
+      end
+    end
 end
