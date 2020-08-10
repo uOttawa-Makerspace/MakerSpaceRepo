3c24b4c6a8921ac6b0ba3bc4d3e29cbb3c1d7452
Arthur Fetiveau <art.fetiveau@gmail.com>
2020-08-10 12:34:20 -0400

    Updates on webhook and button

--- app/controllers/custom_webhooks_controller.rb
+++ app/controllers/custom_webhooks_controller.rb
@@ -6,11 +6,22 @@ class CustomWebhooksController < ApplicationController
   def orders_paid
     params.permit!
     discount_code_params = webhook_params.to_h
+    puts(discount_code_params['line_items'])
     if discount_code_params['discount_codes'].present?
       shopify_discount_code = discount_code_params['discount_codes'][0]['code']
       discount_code = DiscountCode.find_by(code: shopify_discount_code)
       discount_code&.update(usage_count: 1)
     end
+
+    if discount_code_params['line_items'].present?
+      discount_code_params['line_items'].each do |item|
+        if item['product_id'] == 4359597129784
+          cc = 10*item['quantity']
+          CcMoney.create(user_id: User.find_by_email(discount_code_params['customer']['email']).id, cc: cc)
+        end
+      end
+    end
+
     head :ok
   end
 
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -84,9 +84,10 @@ $(document).on('turbolinks:load', function () {
     });
 });
 
-$(document).ready(function(){
+$(document).on('turbolinks:load', function () {
 
     var clipboard = new Clipboard('.clipboard-btn');
     console.log(clipboard);
 
 });
+
