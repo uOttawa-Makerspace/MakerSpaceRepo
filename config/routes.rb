a0f45913b49f8a3c3cb43d4939c44ca59e7a935d
brunomrlima <bruno.mrlima@gmail.com>
2020-08-12 11:25:45 -0400

    dev program info

--- config/routes.rb
+++ config/routes.rb
@@ -66,6 +66,7 @@ Rails.application.routes.draw do
     get 'contact'
     get 'calendar'
     get "report_repository/:repository_id", :as => "report_repository", :action => "report_repository"
+    get 'development_program_info'
   end
 
   # RFID
