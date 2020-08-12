4ce2ff8702dd6f04a02d4dcfc825c7b5b7fa1ea1
Arthur Fetiveau <art.fetiveau@gmail.com>
2020-08-11 20:51:18 -0400

    Trying to fix migration issue

--- db/migrate/20200811180813_create_project_kits.rb
+++ db/migrate/20200811180813_create_project_kits.rb
@@ -1,8 +1,8 @@
 class CreateProjectKits < ActiveRecord::Migration[6.0]
   def change
     create_table :project_kits do |t|
-      t.references :user, index: true, foreign_key: true
-      t.references :proficient_project, index: true, foreign_key: true
+      t.references :user, index: true, foreign_key: :user_id
+      t.references :proficient_project, index: true, foreign_key: :proficient_project_id
       t.string :name
       t.boolean :delivered, default: false
 
