bf48d8453adf05c4eeda8ee545e19841dc956954
Arthur Fetiveau <art.fetiveau@gmail.com>
2020-08-19 10:37:30 -0400

    Finished course manager

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Course, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Association' do
+    context 'has_many' do
+      it { should have_many(:training_sessions) }
+    end
+  end
 end
