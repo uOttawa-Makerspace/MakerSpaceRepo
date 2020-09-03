a53c0563cd9ae6f5ff55718f616ca92c009b0bec
brunomrlima <bruno.mrlima@gmail.com>
2020-09-03 16:56:38 -0400

    adding spec

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -2,6 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Course, type: :model do
 
+  describe 'Association' do
+    context 'has_many' do
+      it { should have_many(:training_sessions) }
+    end
+  end
+
   describe 'Validations' do
     context 'presence and uniqueness' do
       it { should validate_presence_of(:name) }
