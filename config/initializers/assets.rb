b7d3fd55fba5782d01754cfc238571d6f93e08f2
Arthur Fetiveau <art.fetiveau@gmail.com>
2020-09-10 15:39:22 -0400

    Added public to assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,6 +10,7 @@ Rails.application.config.assets.precompile += %w[vendor.css vendor.js]
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
+Rails.application.config.assets.paths << Rails.root.join('public')
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
