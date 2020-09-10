57638a97471ebcf975288ba0bf64386825e0bae2
brunomrlima <bruno.mrlima@gmail.com>
2020-09-10 12:20:22 -0400

    some refactor and adding more tests

--- spec/controllers/announcements_controller_spec.rb
+++ spec/controllers/announcements_controller_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe AnnouncementsController, type: :controller do
   before(:all) do
     @admin = create(:user, :admin)
+    @staff = create(:user, :staff)
+    @regular_user = create(:user, :regular_user)
     @announcement_admin = create(:announcement, :admin)
     @announcement_all = create(:announcement, :all)
     @announcement_volunteer = create(:announcement, :volunteer)
@@ -18,10 +20,18 @@ RSpec.describe AnnouncementsController, type: :controller do
       end
     end
 
+    context 'logged as staff' do
+      it 'should return 200 response' do
+        session[:user_id] = @staff.id
+        get :index
+        expect(response).to have_http_status(:success)
+        expect(@controller.instance_variable_get(:@announcements).count).to eq(1)
+      end
+    end
+
     context 'logged as regular user' do
       it 'should redirect user to root' do
-        user = create(:user, :regular_user)
-        session[:user_id] = user.id
+        session[:user_id] = @regular_user.id
         get :index
         expect(response).to redirect_to root_path
         expect(flash[:alert]).to eq('You cannot access this area.')
@@ -37,14 +47,48 @@ RSpec.describe AnnouncementsController, type: :controller do
         expect(response).to have_http_status(:success)
       end
     end
+
+    context 'logged as staff' do
+      it 'should return 200 response' do
+        session[:user_id] = @staff.id
+        get :new
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context 'logged as regular user' do
+      it 'should redirect user to root' do
+        session[:user_id] = @regular_user.id
+        get :new
+        expect(response).to redirect_to root_path
+        expect(flash[:alert]).to eq('You cannot access this area.')
+      end
+    end
   end
 
   describe "GET /show" do
     context 'logged as admin' do
       it 'should return 200 response' do
         session[:user_id] = @admin.id
-        get :show, params: {id: @announcement_admin.id}
+        get :show, params: {id: @announcement_volunteer.id}
         expect(response).to have_http_status(:success)
+      end
+    end
+
+    context 'logged as staff' do
+      it 'should return 200 response' do
+        session[:user_id] = @staff.id
+        get :show, params: {id: @announcement_volunteer.id}
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context 'logged as regular user' do
+      it 'should redirect user to root' do
+        session[:user_id] = @regular_user.id
+        get :show, params: {id: @announcement_volunteer.id}
+        expect(response).to redirect_to root_path
+        expect(flash[:alert]).to eq('You cannot access this area.')
       end
     end
   end
@@ -53,8 +97,25 @@ RSpec.describe AnnouncementsController, type: :controller do
     context 'logged as admin' do
       it 'should return 200 response' do
         session[:user_id] = @admin.id
-        get :edit, params: {id: @announcement_admin.id}
+        get :edit, params: {id: @announcement_volunteer.id}
         expect(response).to have_http_status(:success)
+      end
+    end
+
+    context 'logged as staff' do
+      it 'should return 200 response' do
+        session[:user_id] = @staff.id
+        get :edit, params: {id: @announcement_volunteer.id}
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context 'logged as regular user' do
+      it 'should redirect user to root' do
+        session[:user_id] = @regular_user.id
+        get :edit, params: {id: @announcement_volunteer.id}
+        expect(response).to redirect_to root_path
+        expect(flash[:alert]).to eq('You cannot access this area.')
       end
     end
   end
@@ -63,10 +124,38 @@ RSpec.describe AnnouncementsController, type: :controller do
     context 'logged as admin' do
       it 'should create an announcement and redirect' do
         session[:user_id] = @admin.id
-        announcement_params =  FactoryBot.attributes_for(:announcement, :all)
+        announcement_params =  FactoryBot.attributes_for(:announcement, :volunteer)
         expect { post :create, params: {announcement: announcement_params} }.to change(Announcement, :count).by(1)
-        expect(flash[:notice]).to eq("You've successfully created an announcement for All")
+        expect(flash[:notice]).to eq("You've successfully created an announcement for Volunteer")
         expect(response).to redirect_to announcements_path
+      end
+    end
+
+    context 'logged as staff' do
+      it 'should return 200 response' do
+        session[:user_id] = @staff.id
+        announcement_params =  FactoryBot.attributes_for(:announcement, :volunteer)
+        expect { post :create, params: {announcement: announcement_params} }.to change(Announcement, :count).by(1)
+        expect(flash[:notice]).to eq("You've successfully created an announcement for Volunteer")
+        expect(response).to redirect_to announcements_path
+      end
+
+      it 'should redirect and not create' do
+        session[:user_id] = @staff.id
+        announcement_params =  FactoryBot.attributes_for(:announcement, :all)
+        expect { post :create, params: {announcement: announcement_params} }.to change(Announcement, :count).by(0)
+        expect(flash[:alert]).to eq("Please select another public goal")
+        expect(response).to redirect_to new_announcement_path
+      end
+    end
+
+    context 'logged as regular user' do
+      it 'should redirect user to root' do
+        session[:user_id] = @regular_user.id
+        announcement_params =  FactoryBot.attributes_for(:announcement, :volunteer)
+        expect { post :create, params: {announcement: announcement_params} }.to change(Announcement, :count).by(0)
+        expect(response).to redirect_to root_path
+        expect(flash[:alert]).to eq('You cannot access this area.')
       end
     end
   end
@@ -75,10 +164,38 @@ RSpec.describe AnnouncementsController, type: :controller do
     context 'logged as admin' do
       it 'should update the announcement' do
         session[:user_id] = @admin.id
-        patch :update, params: {id: @announcement_all.id, announcement: {public_goal: "volunteer"} }
+        patch :update, params: {id: @announcement_volunteer.id, announcement: {public_goal: "staff"} }
         expect(response).to redirect_to announcements_path
-        expect(Announcement.find(@announcement_all.id).public_goal).to eq("volunteer")
+        expect(Announcement.find(@announcement_volunteer.id).public_goal).to eq("staff")
         expect(flash[:notice]).to eq("Announcement updated")
+      end
+    end
+
+    context 'logged as staff' do
+      it 'should return 200 response' do
+        session[:user_id] = @staff.id
+        patch :update, params: {id: @announcement_volunteer.id, announcement: {public_goal: "staff"} }
+        expect(response).to redirect_to announcements_path
+        expect(Announcement.find(@announcement_volunteer.id).public_goal).to eq("staff")
+        expect(flash[:notice]).to eq("Announcement updated")
+      end
+
+      it 'should redirect and not update' do
+        session[:user_id] = @staff.id
+        patch :update, params: {id: @announcement_admin.id, announcement: {public_goal: "staff"} }
+        expect(flash[:alert]).to eq("Please select another public goal")
+        expect(response).to redirect_to new_announcement_path
+        expect(Announcement.find(@announcement_admin.id).public_goal).to eq("admin")
+      end
+    end
+
+    context 'logged as regular user' do
+      it 'should redirect user to root' do
+        session[:user_id] = @regular_user.id
+        patch :update, params: {id: @announcement_volunteer.id, announcement: {public_goal: "staff"} }
+        expect(Announcement.find(@announcement_volunteer.id).public_goal).to eq("volunteer")
+        expect(response).to redirect_to root_path
+        expect(flash[:alert]).to eq('You cannot access this area.')
       end
     end
   end
@@ -87,9 +204,34 @@ RSpec.describe AnnouncementsController, type: :controller do
     context 'logged as admin' do
       it 'should destroy the announcement' do
         session[:user_id] = @admin.id
-        expect {  delete :destroy, params: {id: @announcement_all.id} }.to change(Announcement, :count).by(-1)
+        expect { delete :destroy, params: {id: @announcement_volunteer.id} }.to change(Announcement, :count).by(-1)
         expect(response).to redirect_to announcements_path
         expect(flash[:notice]).to eq("Announcement Deleted")
+      end
+    end
+
+    context 'logged as staff' do
+      it 'should destroy the announcement' do
+        session[:user_id] = @staff.id
+        expect { delete :destroy, params: {id: @announcement_volunteer.id} }.to change(Announcement, :count).by(-1)
+        expect(response).to redirect_to announcements_path
+        expect(flash[:notice]).to eq("Announcement Deleted")
+      end
+
+      it 'should redirect and not delete' do
+        session[:user_id] = @staff.id
+        expect { delete :destroy, params: {id: @announcement_all.id} }.to change(Announcement, :count).by(0)
+        expect(response).to redirect_to new_announcement_path
+        expect(flash[:alert]).to eq("Please select another public goal")
+      end
+    end
+
+    context 'logged as regular user' do
+      it 'should redirect user to root' do
+        session[:user_id] = @regular_user.id
+        expect { delete :destroy, params: {id: @announcement_volunteer.id} }.to change(Announcement, :count).by(0)
+        expect(response).to redirect_to root_path
+        expect(flash[:alert]).to eq('You cannot access this area.')
       end
     end
   end
