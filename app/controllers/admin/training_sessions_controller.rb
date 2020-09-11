caa350fb72c66349857b7c661b30dfdd2922befa
Arthur Fetiveau <art.fetiveau@gmail.com>
2020-09-10 20:09:28 -0400

    Made a new sort system

--- app/controllers/admin/training_sessions_controller.rb
+++ app/controllers/admin/training_sessions_controller.rb
@@ -5,7 +5,28 @@ class Admin::TrainingSessionsController < AdminAreaController
   before_action :training_session_params, only: [:update]
   before_action :training_session, only: %i[update destroy]
 
-  def index; end
+  def index
+
+    # Preventing SQL Injection
+    sort_array = ["created_at", "trainings.name", "users.name", "course", "count(training_sessions.id)", "count(users)"]
+
+    if params[:sort_name].present? && params[:sort_name].to_i.between?(1, 6)
+      if params[:sort_last].present? && params[:sort_last] == params[:sort_name]
+        @sessions = TrainingSession.joins(:training, :user, "left join certifications on certifications.training_session_id = training_sessions.id").group('training_sessions.id', 'trainings.name', 'users.name').order("#{sort_array[params[:sort_name].to_i - 1]} ASC").paginate(:page => params[:page], :per_page => 20)
+      else
+        @sessions = TrainingSession.joins(:training, :user, "left join certifications on certifications.training_session_id = training_sessions.id").group('training_sessions.id', 'trainings.name', 'users.name').order("#{sort_array[params[:sort_name].to_i  - 1]} DESC").paginate(:page => params[:page], :per_page => 20)
+        @last = params[:sort_name]
+      end
+    else
+      @sessions = TrainingSession.all.paginate(:page => params[:page], :per_page => 20)
+    end
+
+    respond_to do |format|
+      format.js
+      format.html
+    end
+
+  end
 
   def update
     # TODO: check where this is being used?
