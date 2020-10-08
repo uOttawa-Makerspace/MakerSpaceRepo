7a9f9b321644a2de7bc80b69992de603df4ccccc
brunomrlima <bruno.mrlima@gmail.com>
2020-10-08 11:19:08 -0400

    refactoring learning module track

--- app/controllers/learning_module_track_controller.rb
+++ app/controllers/learning_module_track_controller.rb
@@ -7,26 +7,25 @@ class LearningModuleTrackController < DevelopmentProgramsController
   end
 
   def start
-    if params[:learning_module_id].present?
-      LearningModuleTrack.create(user_id: current_user.id, learning_module_id: params[:learning_module_id])
+    learning_module = LearningModule.find_by(id: params[:learning_module_id])
+    learning_module_track = learning_module.learning_module_tracks.new
+    learning_module_track.user = current_user
+    if learning_module_track.save
       flash[:notice] = 'You have successfully started the learning module !'
     else
       flash[:alert] = 'An error has occurred while starting the learning module, please try again or contact uottawa.makerepo@uottawa.ca for support.'
     end
-    redirect_to learning_module_track_index_path
+    redirect_to learning_area_path(learning_module.id)
   end
 
   def completed
-    if params[:learning_module_id].present?
-      if LearningModuleTrack.where(user_id: current_user.id, learning_module_id: params[:learning_module_id]).first.update(status: 'Completed')
-        flash[:notice] = 'You have successfully completed the learning module !'
-      else
-        flash[:alert] = 'An error has occurred while completing the learning module, please try again or contact uottawa.makerepo@uottawa.ca for support.'
-      end
+    learning_module = LearningModule.find_by(id: params[:learning_module_id])
+    if learning_module.learning_module_tracks.where(user: current_user).first.update(status: 'Completed')
+      flash[:notice] = 'You have successfully completed the learning module !'
     else
       flash[:alert] = 'An error has occurred while completing the learning module, please try again or contact uottawa.makerepo@uottawa.ca for support.'
     end
-    redirect_to learning_module_track_index_path
+    redirect_to learning_area_path(learning_module.id)
   end
 
 end
