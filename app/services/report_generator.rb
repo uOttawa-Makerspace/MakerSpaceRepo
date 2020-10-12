1974c0569c001c62d41ba4a5f89b4a162d051575
Arthur Fetiveau <art.fetiveau@gmail.com>
2020-10-12 14:04:12 -0400

    Addedd colors to trainings

--- app/services/report_generator.rb
+++ app/services/report_generator.rb
@@ -162,6 +162,23 @@ class ReportGenerator
       table_header(sheet, ['Training', 'Level', 'Course', 'Instructor', 'Date', 'Facility', 'Attendee Count'])
 
       trainings[:training_sessions].each do |row|
+
+        training = Training.find(row[:training_id])
+        color = if training.skills_id.present?
+                  if Skill.find(training.skills_id).name == "Machine Shop Training"
+                    {:bg_color => "ed7d31"}
+                  elsif Skill.find(training.skills_id).name == "Technology Trainings"
+                    {:bg_color => "70ad47"}
+                  elsif Skill.find(training.skills_id).name == "CEED Trainings"
+                    {:bg_color => "ffc000"}
+                  else
+                    {}
+                  end
+                else
+                  {}
+                end
+        style = sheet.styles.add_style(color)
+
         sheet.add_row [
             row[:training_name],
             row[:training_level],
@@ -170,7 +187,7 @@ class ReportGenerator
             row[:date].localtime.strftime('%Y-%m-%d %H:%M'),
             row[:facility],
             row[:attendee_count]
-        ]
+        ], :style => [style]
       end
     end
 
@@ -738,6 +755,7 @@ class ReportGenerator
 
     ActiveRecord::Base.connection.exec_query(query).each do |row|
       result[:training_sessions] << {
+          training_id: row['training_id'],
           training_name: row['training_name'],
           training_level: row['training_level'],
           course_name: row['course_name'],
