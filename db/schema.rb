b51a281f23dc0f8d604a02fbe23e056005d41b98
brunomrlima <bruno.mrlima@gmail.com>
2020-10-14 13:20:26 -0400

    adding list of skills to badge template

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_09_172654) do
+ActiveRecord::Schema.define(version: 2020_10_14_171425) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -85,6 +85,7 @@ ActiveRecord::Schema.define(version: 2020_10_09_172654) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image_url"
+    t.string "list_of_skills"
   end
 
   create_table "badges", id: :serial, force: :cascade do |t|
