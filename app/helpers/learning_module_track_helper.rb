37ff91bc4700df4cca2ea9044078afabbed12134
brunomrlima <bruno.mrlima@gmail.com>
2020-10-19 14:50:32 -0400

    refactoring button class to be more visible in the learning area

--- app/helpers/learning_module_track_helper.rb
+++ app/helpers/learning_module_track_helper.rb
@@ -1,9 +1,9 @@
 module LearningModuleTrackHelper
   def return_button_class(status)
     case status
-    when 'Not Started' then "btn-outline-dark"
-    when 'In progress' then "btn-outline-warning"
-    when 'Completed' then "btn-outline-success"
+    when 'Not Started' then "btn-dark"
+    when 'In progress' then "btn-warning"
+    when 'Completed' then "btn-success"
     end
   end
 end
