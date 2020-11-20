6fa138668371263765111b97f2074b1623dad236
brunomrlima <bruno.mrlima@gmail.com>
2020-11-20 10:29:37 -0500

    allowing more docs to repo file

--- app/models/repo_file.rb
+++ app/models/repo_file.rb
@@ -7,7 +7,7 @@ class RepoFile < ApplicationRecord
 
   has_one_attached :file
 
-  ALLOWED_CONTENT_TYPES = %w[application/pdf image/svg+xml text/html .doc .docx].freeze
+  ALLOWED_CONTENT_TYPES = %w[application/pdf image/svg+xml text/html .doc .docx model/stl application/vnd.ms-pki.stl application/octet-stream text/plain model/x.stl-binary text/x.gcode].freeze
 
   validates :file, file_content_type: {
       allow: [ALLOWED_CONTENT_TYPES],
