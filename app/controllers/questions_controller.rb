c00d9dfc53acf449f1dc4883b6eef146eaa452ff
brunomrlima <bruno.mrlima@gmail.com>
2020-12-04 15:30:15 -0500

    not deleting pics after update

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,7 +3,7 @@
 class QuestionsController < StaffAreaController
   layout 'staff_area'
   before_action :set_question, only: %i[show edit update destroy]
-  before_action :delete_existing_images, only: :update
+  # before_action :delete_existing_images, only: :update
 
   def index
     @questions = Question.all.order(created_at: :desc).paginate(page: params[:page], per_page: 50)
@@ -12,7 +12,7 @@ class QuestionsController < StaffAreaController
   def new
     @new_question = Question.new(params[:new_question])
     @categories = Training.all.order(:name)
-    (params[:n_answers].present? and params[:n_answers].to_i > 1) ? n = params[:n_answers].to_i : (n = 1 and params[:n_answers] = 1)
+    (params[:n_answers].present? and params[:n_answers].to_i > 1) ? n = params[:n_answers].to_i : (n = 4 and params[:n_answers] = 4)
     n.times { @new_question.answers.new }
   end
 
@@ -59,7 +59,7 @@ class QuestionsController < StaffAreaController
       @question = Question.find(params[:id])
     end
 
-    def delete_existing_images
-      @question.images.purge if @question.images.attached?
-    end
+    # def delete_existing_images
+    #   @question.images.purge if @question.images.attached?
+    # end
 end
