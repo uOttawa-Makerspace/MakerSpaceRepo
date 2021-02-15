099a49546ccb8877ab92189a10b056f7d184b103
Arthur Fetiveau <art.fetiveau@gmail.com>
2021-02-15 16:01:15 -0500

    Removed auto-login when changing spaces and added advanced search/sign in button

--- app/javascript/packs/accordion-load.js
+++ app/javascript/packs/accordion-load.js
@@ -1,29 +1,29 @@
-$(document).on('turbolinks:load', function () {
-
-    let storage = localStorage.getItem(location.pathname);
-    if ((storage != undefined) && (storage != "")) {
-        storage = storage.split(",");
-        storage.forEach(function (item) {
-            document.getElementById(item).classList.toggle("show");
-        });
-    }
-
-    $(".collapse").on('shown.bs.collapse', function(e){
-        storeid(location.pathname);
-    });
-
-    $(".collapse").on('hidden.bs.collapse', function(e){
-        storeid(location.pathname);
-    });
-
-});
-
-window.storeid = function(page) {
-    let className = document.getElementsByClassName('show');
-    let classnameCount = className.length;
-    let IdStore = new Array();
-    for(let i = 0; i < classnameCount; i++){
-        IdStore.push(className[i].id);
-    }
-    localStorage.setItem(page, IdStore.toString(), { expires: 1 });
-};
+// $(document).on('turbolinks:load', function () {
+//
+//     let storage = localStorage.getItem(location.pathname);
+//     if ((storage != undefined) && (storage != "")) {
+//         storage = storage.split(",");
+//         storage.forEach(function (item) {
+//             document.getElementById(item).classList.toggle("show");
+//         });
+//     }
+//
+//     $(".collapse").on('shown.bs.collapse', function(e){
+//         storeid(location.pathname);
+//     });
+//
+//     $(".collapse").on('hidden.bs.collapse', function(e){
+//         storeid(location.pathname);
+//     });
+//
+// });
+//
+// window.storeid = function(page) {
+//     let className = document.getElementsByClassName('show');
+//     let classnameCount = className.length;
+//     let IdStore = new Array();
+//     for(let i = 0; i < classnameCount; i++){
+//         IdStore.push(className[i].id);
+//     }
+//     localStorage.setItem(page, IdStore.toString(), { expires: 1 });
+// };
