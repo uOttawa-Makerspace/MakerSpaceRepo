4ec17cda93e6a4e02208826ebaafdf0b03710888
Arthur Fetiveau <art.fetiveau@gmail.com>
2021-09-14 10:46:59 -0400

    Splitting the 2 instances of MakerRepo

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -1,5 +1,10 @@
 Datadog.configure do |c|
-  c.env = 'production'
-  c.service = 'MakerRepo'
+  if Rails.env == "production"
+    c.env = 'production'
+    c.service = 'MakerRepo'
+  elsif Rails.env == "staging"
+    c.env = 'staging'
+    c.service = 'MakerRepo Staging'
+  end
   c.sampling.default_rate = 1.0
 end
