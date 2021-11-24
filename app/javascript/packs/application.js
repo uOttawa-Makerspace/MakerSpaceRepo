b8d41ef49c064da7e1ca5b021b4a3bbac78d8655
Arthur Fetiveau <art.fetiveau@gmail.com>
2021-11-24 10:15:09 -0500

    Fix for training sessions search in admin

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -106,3 +106,18 @@ window.setSpace = function(){
         }
     })
 }
+
+window.debounce = function(func, wait, immediate) {
+    let timeout;
+    return function() {
+        let context = this, args = arguments;
+        let later = function() {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        let callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+};
--- app/javascript/packs/badges.js
+++ app/javascript/packs/badges.js
@@ -80,18 +80,3 @@ $(document).on("turbolinks:load", function() {
         minimumInputLength: 3,
     });
 });
-
-window.debounce = function(func, wait, immediate) {
-    var timeout;
-    return function() {
-        var context = this, args = arguments;
-        var later = function() {
-            timeout = null;
-            if (!immediate) func.apply(context, args);
-        };
-        var callNow = immediate && !timeout;
-        clearTimeout(timeout);
-        timeout = setTimeout(later, wait);
-        if (callNow) func.apply(context, args);
-    };
-};
