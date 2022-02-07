ba83e4708a4ffd1b83d5627741f6f7fef5246038
Arthur Fetiveau <art.fetiveau@gmail.com>
2022-02-06 19:14:25 -0500

    Fixed stripe webhook

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -7,7 +7,7 @@ StripeEvent.configure do |events|
     obj = Stripe::Payout.retrieve(
       id: event.data.object.id,
       expand: ['destination']
-    ).data.object
+    )
     date = Time.at(obj.arrival_date).strftime("%Y-%m-%d")
 
     MsrMailer.send_email_for_stripe_transfer(obj.id, date, obj.amount, obj.destination.bank_name, obj.destination.routing_number).deliver_now
