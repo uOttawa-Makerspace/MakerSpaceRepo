a373409a01665e8aa846efd84b8a7e4425d1ce4a
Arthur Fetiveau <art.fetiveau@gmail.com>
2022-03-13 15:32:09 -0400

    Modified file and location

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -8,3 +8,4 @@ set :branch, 'master'
 set :deploy_to, '/var/www/makerrepo'
 set :keep_releases, 3
 set :rbenv_ruby, '2.7.2'
+set :maintenance_template_path, File.expand_path("../../maintenance/maintenance.html.erb", __FILE__)
