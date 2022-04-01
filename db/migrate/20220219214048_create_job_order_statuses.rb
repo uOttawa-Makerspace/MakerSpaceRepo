63bca9dd4c0cb9e16b9234fa57c72fcd2d20b100
Arthur Fetiveau <art.fetiveau@gmail.com>
2022-03-31 21:14:10 -0400

    Trying something else

--- db/migrate/20220219214048_create_job_order_statuses.rb
+++ db/migrate/20220219214048_create_job_order_statuses.rb
@@ -2,7 +2,7 @@ class CreateJobOrderStatuses < ActiveRecord::Migration[6.1]
   def change
     create_table :job_order_statuses do |t|
       t.references :job_order, index: true, foreign_key: true
-      t.references :job_status, index: false, foreign_key: true
+      t.references :job_status, index: true, foreign_key: true
       t.timestamps
     end
   end
