5ff676b78df6751c1bcc94f04bee4f83ec8ffdd0
Arthur Fetiveau <art.fetiveau@gmail.com>
2022-05-27 17:28:24 -0400

    Fixed migration change

--- db/migrate/20220527200209_convert_job_service_text_field_into_enum.rb
+++ db/migrate/20220527200209_convert_job_service_text_field_into_enum.rb
@@ -1,6 +1,12 @@
 class ConvertJobServiceTextFieldIntoEnum < ActiveRecord::Migration[6.1]
-  def change
+  def up
     change_column :job_service_groups, :text_field, :boolean, default: nil
     change_column :job_service_groups, :text_field, :integer,default: 0, using: 'case when text_field then 1::integer else 0::integer end'
+    JobServiceGroup.where(name: %w[PLA ABS]).update(text_field: :option)
+  end
+
+  def down
+    change_column :job_service_groups, :text_field, :integer, default: nil
+    change_column :job_service_groups, :text_field, :boolean,default: 0, using: 'case when text_field = 1 then true else false end'
   end
 end
