a787860df127e30419fbd76a831ef43544000db4
Arthur Fetiveau <art.fetiveau@gmail.com>
2022-06-17 15:13:48 -0400

    Feat: Add Recaptcha to help page (#1222)

--- app/controllers/help_controller.rb
+++ app/controllers/help_controller.rb
@@ -7,21 +7,34 @@ class HelpController < SessionsController
   def main; end
 
   def send_email
-    @name = params[:name]
-    @email = params[:email]
-    @subject = params[:subject]
-    @comments = params[:comments]
-    @app_version = params[:app_version]
-    MsrMailer.issue_email(@name, @email, @subject, @comments, @app_version).deliver_now
-    respond_to do |format|
-      format.html {
-        redirect_to help_path, notice: 'Email successfully sent. You will be contacted soon.'
-      }
-      format.json {
-        render json: {
-          status: 'success'
+    if verify_recaptcha || params[:app_version].present?
+      @name = params[:name]
+      @email = params[:email]
+      @subject = params[:subject]
+      @comments = params[:comments]
+      @app_version = params[:app_version]
+      MsrMailer.issue_email(@name, @email, @subject, @comments, @app_version).deliver_now
+      respond_to do |format|
+        format.html {
+          redirect_to help_path, notice: 'Email successfully sent. You will be contacted soon.'
         }
-      }
+        format.json {
+          render json: {
+            status: 'success'
+          }
+        }
+      end
+    else
+      respond_to do |format|
+        format.html {
+          redirect_to help_path, alert: 'Please make sure you fill out the captcha correctly.'
+        }
+        format.json {
+          render json: {
+            status: 'error'
+          }
+        }
+      end
     end
   end
 end
