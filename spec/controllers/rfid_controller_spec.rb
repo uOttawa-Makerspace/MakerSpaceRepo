8c1f5376a97fb2fb4ee3736f489bce3ddc1e216f
NomadNaomie <nao@naomie.digital>
2022-06-30 14:42:31 -0400

    added unit testing for multiple RFID readers

--- spec/controllers/rfid_controller_spec.rb
+++ spec/controllers/rfid_controller_spec.rb
@@ -39,6 +39,54 @@ RSpec.describe RfidController, type: :controller do
         expected = { error: 'Temporary RFID already exists' }
         expect(actual).to eq(expected)
       end
+
+      it 'should not create rfid and should sign in RFID and sign out' do
+        pi_reader = create(:pi_reader)
+        attributes = create(:rfid, mac_address: pi_reader.pi_mac_address)
+        rfid_params = { rfid: attributes.card_number, mac_address: attributes.mac_address }
+        expect { post :card_number, params: rfid_params, :format => :json }.to change(Rfid, :count).by(0)
+        actual = JSON.parse(response.body, symbolize_names: true)
+        expected = { success: 'RFID sign in' }
+        expect(actual).to eq(expected)
+        rfid_params = { rfid: attributes.card_number, mac_address: attributes.mac_address }
+        expect { post :card_number, params: rfid_params, :format => :json }.to change(Rfid, :count).by(0)
+        actual = JSON.parse(response.body, symbolize_names: true)
+        expected = { success: 'RFID sign out' }
+        expect(actual).to eq(expected)
+      end
+
+      it 'should not create rfid and should sign in RFID at one pi and sign out at other pi' do
+        sign_in_pi = create(:pi_reader)
+        sign_out_pi = create(:pi_reader, space_id: sign_in_pi.space_id)
+        attributes = create(:rfid, mac_address: sign_in_pi.pi_mac_address)
+        sign_in_rfid_params = { rfid: attributes.card_number, mac_address: sign_in_pi.pi_mac_address }
+        expect { post :card_number, params: sign_in_rfid_params, :format => :json }.to change(Rfid, :count).by(0)
+        actual = JSON.parse(response.body, symbolize_names: true)
+        sign_out_rfid_params = { rfid: attributes.card_number, mac_address: sign_out_pi.pi_mac_address }
+        expected = { success: 'RFID sign in' }
+        expect(actual).to eq(expected)
+        expect { post :card_number, params: sign_out_rfid_params, :format => :json }.to change(Rfid, :count).by(0)
+        actual = JSON.parse(response.body, symbolize_names: true)
+        expected = { success: 'RFID sign out' }
+        expect(actual).to eq(expected)
+      end
+
+      # #Temporary RFID Multi-box
+      # it 'should create an rfid and should sign in rfid ' do
+      #   sign_in_pi = create(:pi_reader)
+      #   sign_out_pi = create(:pi_reader, space_id: sign_in_pi.space_id)
+      #   attributes = FactoryBot.attributes_for(:rfid)
+      #   sign_in_rfid_params = { rfid: attributes[:card_number], mac_address: sign_in_pi.pi_mac_address }
+      #   expect { post :card_number, params: sign_in_rfid_params, :format => :json }.to change(Rfid, :count).by(1)
+      #   actual = JSON.parse(response.body, symbolize_names: true)
+      #   expected = { new_rfid: 'Temporary RFID created' }
+      #   expect(actual).to eq(expected)
+      #   sign_out_rfid_params = { rfid: attributes[:card_number], mac_address: sign_out_pi.pi_mac_address }
+      #   expect { post :card_number, params: sign_out_rfid_params, :format => :json }.to change(Rfid, :count).by(0)
+      #   actual = JSON.parse(response.body, symbolize_names: true)
+      #   expected = { error: 'Temporary RFID already exists' }
+      #   expect(actual).to eq(expected)
+      # end
     end
   end
 end
