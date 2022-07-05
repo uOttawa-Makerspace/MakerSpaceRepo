24d841ead86e09a832a24ed6f09b74ea69e9fae6
NomadNaomie <nao@naomie.digital>
2022-07-04 21:48:00 -0400

    more small header fixes

--- app/javascript/packs/header.js
+++ app/javascript/packs/header.js
@@ -1,30 +1,28 @@
 /**
  * 
  * This JS deals with the navbar
- * The navbar is only in "dark mode" when its on the home page and top of the page and above the media query width to be uncollapsable.
+ * The navbar is only in "dark mode" when its on the home page and top of the page and above the media query width to be uncollapsable and the navbar is collapsed
  * 
  * These conditions are checked by:
  *  - Checking the navbar for the static pages AND home class. (nav.classList.contains('static_pages') && nav.classList.contains('home'))
  *  - Checking the scrollY of the window, we have a 10px deadzone. (window.scrollY > 10)
  *  - Checking the media query width. As of writing its 1200px (window.matchMedia('(max-width: 1200px)').matches) and (window.matchMedia('(min-width: 1200px)').matches)
- * 
+ *  - Checking the height of the collapsable navbar (navbarSupportedContent.clientHeight)
  * The transition function has 2 parameters whether we're setting it to dark mode and whether to animate the transition or not. 
  */ 
 
 
  document.addEventListener('turbolinks:load', function() {
     
-    var nav = document.getElementsByClassName('navbar')[0];
-    var navToggler = document.getElementsByClassName('navbar-toggler')[0];
+    var nav = document.getElementById("header-navbar");
+    var navToggler = document.getElementById("navbar-toggle-button");
     var navbarSupportedContent = document.getElementById('navbarSupportedContent');
     var chevron = document.getElementsByClassName("down-indicator")[0];
     var cc_image_white = document.getElementById("myCcWhite");
     var cc_image_black = document.getElementById("myCcBlack");
     function doTransition(dark, animate) {
-        if (typeof animate === 'undefined' || animate === true) {
-            nav.classList.add('transition');
-        }
-
+        if (!(nav.classList.contains('static_pages') && nav.classList.contains('home'))){return;}
+        if (typeof animate === 'undefined' || animate === true) {nav.classList.add('transition');}
         if (dark) {
             nav.classList.add("bg-dark-gradient")
             nav.classList.add('navbar-dark');
@@ -58,7 +56,7 @@
 
     // When we scroll past the 10px deadzone, we want to change the navbar to dark mode
     window.addEventListener('scroll', () => {
-        doTransition((nav.classList.contains('static_pages') && nav.classList.contains('home')) && window.scrollY <= 10 && ((window.matchMedia('(max-width: 1200px)').matches && navbarSupportedContent.clientHeight == 0) || window.matchMedia('(min-width: 1200px)').matches), true);
+        doTransition(window.scrollY <= 10 && ((window.matchMedia('(max-width: 1200px)').matches && navbarSupportedContent.clientHeight == 0) || window.matchMedia('(min-width: 1200px)').matches), true);
         if (window.scrollY <= 10 && chevron) {
             chevron.style.opacity = 1;
         }
@@ -83,10 +81,8 @@
         window.scrollY < 10 ? doTransition(true,false) : doTransition(false, false);
     });
     if (!nav.classList.contains('static_pages') && !nav.classList.contains('home')){
-        console.log("navbarSupportedContent.clientHeight > 0");
         doTransition(false, false);
-    }else if(window.matchMedia('(max-width: 1200px)').matches && navbarSupportedContent.clientHeight > 0){
-        console.log("navbarSupportedContent.clientHeight > 0");
+    }else if(navbarSupportedContent.clientHeight > 0){
         doTransition(false, false);
     }else{
         doTransition(true, false);
