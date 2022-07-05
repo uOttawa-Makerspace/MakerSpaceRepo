c7c6c527aa9f0b485d9a30ebbeb62be36d146607
NomadNaomie <nao@naomie.digital>
2022-07-04 21:32:23 -0400

    backporting jQuery less header code

--- app/javascript/packs/header.js
+++ app/javascript/packs/header.js
@@ -1,28 +1,46 @@
-$(document).on('turbolinks:load', function () {
-    var nav = $('nav.navbar');
-    var chevron = $('.down-indicator');
-    var collapse = nav.find('.navbar-collapse');
-    var background = $('<div class="background">');
+/**
+ * 
+ * This JS deals with the navbar
+ * The navbar is only in "dark mode" when its on the home page and top of the page and above the media query width to be uncollapsable and the navbar is collapsed
+ * 
+ * These conditions are checked by:
+ *  - Checking the navbar for the static pages AND home class. (nav.classList.contains('static_pages') && nav.classList.contains('home'))
+ *  - Checking the scrollY of the window, we have a 10px deadzone. (window.scrollY > 10)
+ *  - Checking the media query width. As of writing its 1200px (window.matchMedia('(max-width: 1200px)').matches) and (window.matchMedia('(min-width: 1200px)').matches)
+ *  - Checking the height of the collapsable navbar (navbarSupportedContent.clientHeight)
+ * The transition function has 2 parameters whether we're setting it to dark mode and whether to animate the transition or not. 
+ */ 
+
+
+ document.addEventListener('turbolinks:load', function() {
+    
+    var nav = document.getElementsByClassName('navbar')[0];
+    var navToggler = document.getElementsByClassName('navbar-toggler')[0];
+    var navbarSupportedContent = document.getElementById('navbarSupportedContent');
+    var chevron = document.getElementsByClassName("down-indicator")[0];
     var cc_image_white = document.getElementById("myCcWhite");
     var cc_image_black = document.getElementById("myCcBlack");
-
     function doTransition(dark, animate) {
         if (typeof animate === 'undefined' || animate === true) {
-            nav.addClass('transition');
+            nav.classList.add('transition');
         }
 
         if (dark) {
-            nav.removeClass('navbar-light');
-            background.removeClass('bg-light');
-            nav.addClass('navbar-dark');
+            nav.classList.add("bg-dark-gradient")
+            nav.classList.add('navbar-dark');
+            
+            nav.classList.remove('navbar-light');
+            nav.classList.remove("bg-light")
             if (cc_image_white && cc_image_black){
                 cc_image_white.style.display = 'inline';
                 cc_image_black.style.display = 'none';
             }
         } else {
-            nav.removeClass('navbar-dark');
-            nav.addClass('navbar-light');
-            background.addClass('bg-light');
+            nav.classList.add("bg-light")
+            nav.classList.add('navbar-light');
+
+            nav.classList.remove('navbar-dark');
+            nav.classList.remove("bg-dark-gradient")
             if (cc_image_white && cc_image_black){
                 cc_image_white.style.display = 'none';
                 cc_image_black.style.display = 'inline';
@@ -30,31 +48,45 @@ $(document).on('turbolinks:load', function () {
         }
     }
 
-    if (!nav.hasClass('static_pages home')) {
-        return;
-    }
 
-    nav.removeClass('bg-light');
-    nav.addClass('bg-dark-gradient');
-    nav.append(background);
+    //Not sure if this is necessary anymore, was legacy code when I refactored. 
+    nav.addEventListener('transitionend', () => {nav.classList.remove('transition');});
+    nav.addEventListener('webkitTransitionEnd', () => {nav.classList.remove('transition');});
+    nav.addEventListener('oTransitionEnd', () => {nav.classList.remove('transition');});
+    nav.addEventListener('otransitionend', () => {nav.classList.remove('transition');});
+    nav.addEventListener('MSTransitionEnd', () => {nav.classList.remove('transition');});
 
-    nav.on('transitionend webkitTransitionEnd oTransitionEnd otransitionend MSTransitionEnd', function () {
-        nav.removeClass('transition');
-    });
+    // When we scroll past the 10px deadzone, we want to change the navbar to dark mode
+    window.addEventListener('scroll', () => {
+        doTransition((nav.classList.contains('static_pages') && nav.classList.contains('home')) && window.scrollY <= 10 && ((window.matchMedia('(max-width: 1200px)').matches && navbarSupportedContent.clientHeight == 0) || window.matchMedia('(min-width: 1200px)').matches), true);
+        if (window.scrollY <= 10 && chevron) {
+            chevron.style.opacity = 1;
+        }
+        else if (chevron) {
+            chevron.style.opacity = 0;
+        }
+    }); 
 
-    collapse.on('show.html.erb.bs.collapse hide.bs.collapse', function () {
-        doTransition(nav.hasClass('static_pages home') && $(window).scrollTop() <= 10 && collapse.hasClass('show.html.erb'));
-    });
-
-    $(window).on('scroll', function () {
-        doTransition(nav.hasClass('static_pages home') && $(window).scrollTop() <= 10 && !collapse.hasClass('show.html.erb'));
-
-        if ($(window).scrollTop() <= 10) {
-            chevron.css('opacity', 1);
-        } else {
-            chevron.css('opacity', 0);
+    navToggler.addEventListener('click', () => {
+        if (navbarSupportedContent.clientHeight == 0) {
+            doTransition(false, false);
+        }else{
+            window.scrollY < 10 ? doTransition(true,false) : doTransition(false, false);
         }
     });
 
-    doTransition(true, false);
+    window.addEventListener('resize', () => {
+        if (window.matchMedia('(max-width: 1200px)').matches && navbarSupportedContent.clientHeight > 0) {
+            doTransition(false, false);
+            return;
+        }
+        window.scrollY < 10 ? doTransition(true,false) : doTransition(false, false);
+    });
+    if (!nav.classList.contains('static_pages') && !nav.classList.contains('home')){
+        doTransition(false, false);
+    }else if(navbarSupportedContent.clientHeight > 0){
+        doTransition(false, false);
+    }else{
+        doTransition(true, false);
+    }
 });
