56c7ade7cf6eeaf9c9a2bea8c658314bdc33463a
Arthur Fetiveau <art.fetiveau@gmail.com>
2022-07-22 14:41:30 -0400

    Fix: Broken Accordion Algorithm

--- app/javascript/packs/accordion-load.js
+++ app/javascript/packs/accordion-load.js
@@ -2,33 +2,22 @@
  * This class stores information about accordion states in local storage and sets states on page load. 
  */
 
-document.addEventListener('turbolinks:load', function() {
+document.addEventListener('turbolinks:load', () => {
     let storage = localStorage.getItem(location.pathname);
-    if ((storage != undefined) && (storage != "")) {
+    if (storage) {
         storage = storage.split(",");
         storage.forEach(function (item) {
             let accordion_load_item = document.getElementById(item);
             if (accordion_load_item){accordion_load_item.classList.toggle("show");}
         });
     }
-    let collapseElements = [...document.getElementsByClassName('collapse')];
-    collapseElements.forEach(function (collapseElement) {
-        collapseElement.addEventListener('show.bs.collapse', function () {
-            storeid(location.pathname);
+    const collapseElements = [...document.getElementsByClassName('collapse')];
+    collapseElements.forEach((collapseElement) => {
+        collapseElement.addEventListener('show.bs.collapse', (event) => {
+            localStorage.setItem(location.pathname, localStorage.getItem(location.pathname) ? [...localStorage.getItem(location.pathname).split(","), event.target.id].toString() : [event.target.id]);
         });
-        collapseElement.addEventListener('hide.bs.collapse', function () {
-            storeid(location.pathname);
+        collapseElement.addEventListener('hide.bs.collapse', (event) => {
+            localStorage.setItem(location.pathname, localStorage.getItem(location.pathname) ? localStorage.getItem(location.pathname).split(",").filter(el => el !== event.target.id).toString() : []);
         });
     });
 });
-
-window.storeid = function(page) {
-    let shownItems = document.getElementsByClassName('show');
-    let IdStore = new Array();
-    for(let i = 0; i < shownItems.length; i++){
-        if (shownItems[i].id != ""){
-            IdStore.push(shownItems[i].id);
-        }
-    }
-    localStorage.setItem(page, IdStore.toString(), { expires: 1 });
-};
