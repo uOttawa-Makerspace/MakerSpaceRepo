951cb6ce538d9512ce1a09018e837ba2968d4880
Arthur Fetiveau <art.fetiveau@gmail.com>
2022-07-26 14:13:02 -0400

    Fixed it so it would work with new URLs

--- app/javascript/packs/accordion-load.js
+++ app/javascript/packs/accordion-load.js
@@ -4,13 +4,22 @@
 
 document.addEventListener('turbolinks:load', () => {
     let storageString = localStorage.getItem(location.pathname);
+    let storage;
     if (storageString) {
-        try{storage = JSON.parse(storageString);}catch(e){storage = {};}
+        try {
+            storage = JSON.parse(storageString);
+        } catch (e) {
+            storage = {};
+        }
         Object.keys(storage).forEach(function (item) {
             if (storage[item] === true) {
-                if (document.getElementById(item)){document.getElementById(item).classList.add("show");}
-            }else if (storage[item] === false){
-                if (document.getElementById(item)){document.getElementById(item).classList.remove("show");}
+                if (document.getElementById(item)) {
+                    document.getElementById(item).classList.add("show");
+                }
+            } else if (storage[item] === false) {
+                if (document.getElementById(item)) {
+                    document.getElementById(item).classList.remove("show");
+                }
             }
         });
     }
@@ -18,20 +27,33 @@ document.addEventListener('turbolinks:load', () => {
     collapseElements.forEach((collapseElement) => {
         collapseElement.addEventListener('show.bs.collapse', (event) => {
             let storageString = localStorage.getItem(location.pathname);
+            let storage;
             if (storageString) {
-                try{storage = JSON.parse(storageString);}catch{storage = {};}
-                storage[event.target.id] = true;
-                localStorage.setItem(location.pathname, JSON.stringify(storage));
+                try {
+                    storage = JSON.parse(storageString);
+                } catch {
+                    storage = {};
+                }
+            } else {
+                storage = {}
             }
+            storage[event.target.id] = true;
+            localStorage.setItem(location.pathname, JSON.stringify(storage));
         });
         collapseElement.addEventListener('hide.bs.collapse', (event) => {
             let storageString = localStorage.getItem(location.pathname);
+            let storage;
             if (storageString) {
-                try{storage = JSON.parse(storageString);}catch{storage = {};}
-                storage[event.target.id] = false;
-                localStorage.setItem(location.pathname, JSON.stringify(storage));
+                try {
+                    storage = JSON.parse(storageString);
+                } catch {
+                    storage = {};
+                }
+            } else {
+                storage = {}
             }
-
+            storage[event.target.id] = false;
+            localStorage.setItem(location.pathname, JSON.stringify(storage));
         });
     });
 });
