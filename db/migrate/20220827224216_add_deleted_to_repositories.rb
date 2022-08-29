d048736613c1ba67b9a58751738be83f89e118c6
NomadNaomie <nao@naomie.digital>
2022-08-29 18:52:00 -0400

    unscoping migrations

--- db/migrate/20220827224216_add_deleted_to_repositories.rb
+++ db/migrate/20220827224216_add_deleted_to_repositories.rb
@@ -1,7 +1,7 @@
 class AddDeletedToRepositories < ActiveRecord::Migration[6.1]
   def self.up
     add_column :repositories, :deleted, :boolean
-    Repository.update_all(deleted: false)
+    Repository.unscoped.update_all(deleted: false)
   end
   def self.down
     remove_column :repositories, :deleted
