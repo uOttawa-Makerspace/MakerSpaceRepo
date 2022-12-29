7d40ed238a38a640bf9dbdb6b34b1d6ae3fe8cab
NomadNaomie <nao@naomie.digital>
2022-12-29 19:12:09 +0000

    Removing paperclip dependent migrations

--- db/migrate/20150522041757_add_attachment_avatar_to_users.rb
+++ db/migrate/20150522041757_add_attachment_avatar_to_users.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 class AddAttachmentAvatarToUsers < ActiveRecord::Migration[5.0]
-  def self.up
-    change_table :users do |t|
-      t.attachment :avatar
-    end
-  end
+  # def self.up
+  #   change_table :users do |t|
+  #     # t.attachment :avatar
+  #   end
+  # end
 
-  def self.down
-    remove_attachment :users, :avatar
-  end
+  # def self.down
+  #   remove_attachment :users, :avatar
+  # end
 end
--- db/migrate/20190624193501_add_paperclip_to_print_order.rb
+++ db/migrate/20190624193501_add_paperclip_to_print_order.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 class AddPaperclipToPrintOrder < ActiveRecord::Migration[5.0]
-  def up
-    add_attachment :print_orders, :file
-  end
+  # def up
+  #   add_attachment :print_orders, :file
+  # end
 
-  def down
-    remove_attachment :print_orders, :file
-  end
+  # def down
+  #   remove_attachment :print_orders, :file
+  # end
 end
