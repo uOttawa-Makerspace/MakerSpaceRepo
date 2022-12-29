bbbadc7dc8ab3179b7db2881f40a5500a9267b0a
NomadNaomie <nao@naomie.digital>
2022-12-29 19:38:24 +0000

    Fixing video file migration

--- db/migrate/20201221175812_remove_video_file_name_from_videos.rb
+++ db/migrate/20201221175812_remove_video_file_name_from_videos.rb
@@ -1,9 +1,9 @@
 class RemoveVideoFileNameFromVideos < ActiveRecord::Migration[6.0]
-  # def change
-  #   remove_column :videos, :direct_upload_url, :string
-  #   remove_column :videos, :video_file_name, :string
-  #   remove_column :videos, :video_file_size, :integer
-  #   remove_column :videos, :video_content_type, :string
-  #   remove_column :videos, :video_updated_at, :datetime
-  # end
+  def change
+    remove_column :videos, :direct_upload_url, :string
+    #   remove_column :videos, :video_file_name, :string
+    #   remove_column :videos, :video_file_size, :integer
+    #   remove_column :videos, :video_content_type, :string
+    #   remove_column :videos, :video_updated_at, :datetime
+  end
 end
