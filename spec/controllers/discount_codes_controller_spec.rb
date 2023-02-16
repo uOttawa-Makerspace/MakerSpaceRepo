03ce45c10148f9f0b18bef72030459b2ba31ae8c
NomadNaomie <nao@naomie.digital>
2023-02-15 22:13:55 -0500

    Replacing Shopify CC Redemption with a temporary GoDaddy version

--- spec/controllers/discount_codes_controller_spec.rb
+++ spec/controllers/discount_codes_controller_spec.rb
@@ -6,8 +6,10 @@ RSpec.describe DiscountCodesController, type: :controller do
       @user = create(:user, :regular_user)
       @admin = create(:user, :admin)
       Program.create(user_id: @user.id, program_type: Program::DEV_PROGRAM)
-      create(:discount_code, :unused, user: @user)
-      create(:discount_code, :unused, user: @admin)
+      # create(:discount_code, :unused, user: @user)
+      # create(:discount_code, :unused, user: @admin)
+      create(:coupon_code, user: @user)
+      create(:coupon_code, :claimed)
     end
 
     context "logged as regular user" do
@@ -15,8 +17,11 @@ RSpec.describe DiscountCodesController, type: :controller do
         session[:user_id] = @user.id
         get :index
         expect(response).to have_http_status(:success)
+        # expect(@controller.instance_variable_get(:@discount_codes).count).to eq(
+        #   1
+        # )
         expect(@controller.instance_variable_get(:@discount_codes).count).to eq(
-          1
+          0
         )
       end
     end
@@ -39,13 +44,16 @@ RSpec.describe DiscountCodesController, type: :controller do
 
   describe "Actions" do
     before(:context) do
-      @shopify_price_rule_id = PriceRule.create_price_rule("10$ Coupon", 10)
-      @price_rule =
-        create(
-          :price_rule,
-          shopify_price_rule_id: @shopify_price_rule_id,
-          value: 10
-        )
+      # @shopify_price_rule_id = PriceRule.create_price_rule("10$ Coupon", 10)
+      # @price_rule =
+      #   create(
+      #     :price_rule,
+      #     shopify_price_rule_id: @shopify_price_rule_id,
+      #     value: 10
+      #   )
+      @price_rule = create(:coupon_code)
+      @price_rule_2 = create(:coupon_code)
+      @price_rule_3 = create(:coupon_code)
       @user = create(:user, :regular_user)
       Program.create(user_id: @user.id, program_type: Program::DEV_PROGRAM)
       CcMoney.create(user_id: @user.id, cc: 1000)
@@ -57,7 +65,7 @@ RSpec.describe DiscountCodesController, type: :controller do
       it "should return 200" do
         get :new
         expect(response).to have_http_status(:success)
-        expect(@controller.instance_variable_get(:@price_rules).count).to eq(3)
+        # expect(@controller.instance_variable_get(:@price_rules).count).to eq(3)
       end
     end
 
@@ -65,7 +73,8 @@ RSpec.describe DiscountCodesController, type: :controller do
       it "should be redirecting to discount code path and creating a discount code" do
         expect {
           post :create, params: { price_rule_id: @price_rule.id }
-        }.to change(DiscountCode, :count).by(1)
+          # }.to change(DiscountCode, :count).by(1)
+        }.to change(CouponCode.claimed, :count).by(1)
         expect(response).to redirect_to discount_codes_path
         expect(flash[:notice]).to eq("Discount Code created")
       end
@@ -78,7 +87,7 @@ RSpec.describe DiscountCodesController, type: :controller do
           post :create, params: { price_rule_id: price_rule.id }
         }.to change(DiscountCode, :count).by(0)
         expect(response).to redirect_to new_discount_code_path
-        expect(flash[:alert]).to eq("This coupon is expired")
+        expect(flash[:alert]).to eq("This coupon is already claimed")
       end
     end
 
@@ -92,7 +101,7 @@ RSpec.describe DiscountCodesController, type: :controller do
     end
 
     after :context do
-      PriceRule.delete_price_rule_from_shopify(@shopify_price_rule_id)
+      # PriceRule.delete_price_rule_from_shopify(@shopify_price_rule_id)
     end
   end
 end
