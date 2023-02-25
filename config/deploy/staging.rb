c06dad617f72f837fc9113b09497efea62f1be53
nomadnaomie <nao@naomie.digital>
2023-02-25 16:25:31 -0500

    Changing over ruby versions

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -7,6 +7,6 @@ role :db, %w[deploy@staging-server.makerepo.com]
 set :branch, "staging"
 set :deploy_to, "/var/www/makerrepo-staging"
 set :keep_releases, 3
-set :rbenv_ruby, "2.7.3"
+set :rbenv_ruby, "3.1.2"
 set :maintenance_template_path,
     File.expand_path("../../maintenance/maintenance.html.erb", __FILE__)
