ed263468282212af3d3d7a48fd83068f415c7203
Charley Liu <charleyliu716@gmail.com>
2023-05-11 13:37:49 -0400

    Modified photo gallery javascript file to work with PhotoSwipe v4.1.3

--- app/javascript/entrypoints/photo_gallery.js
+++ app/javascript/entrypoints/photo_gallery.js
@@ -1,45 +1,54 @@
-// var photoArray = [];
-//
-// document.addEventListener("DOMContentLoaded", function () {
-//   photoArray = [];
-//   let gallery = photoSwipe();
-//   let photoSlide = document.getElementById("photo-slide");
-//   if (photoSlide) {
-//     photoSlide.childNodes.forEach(function (node) {
-//       let img = node;
-//       if (img.tagName === "IMG") {
-//         photoArray.push({
-//           src: img.getAttribute("src"),
-//           w: img.getAttribute("data-width"),
-//           h: img.getAttribute("data-height"),
-//         });
-//         img.addEventListener("click", function () {
-//           gallery.init();
-//         });
-//       }
-//     });
-//   }
-//   let showPhoto = document.getElementById("show-photo");
-//   if (showPhoto) {
-//     showPhoto.addEventListener("click", function () {
-//       gallery.init();
-//     });
-//   }
-// });
-//
-// function photoSwipe() {
-//   var pswpElement = document.querySelectorAll(".pswp")[0];
-//
-//   var items = photoArray;
-//   var options = {
-//     index: 0,
-//   };
-//
-//   var gallery = new PhotoSwipe(
-//     pswpElement,
-//     PhotoSwipeUI_Default,
-//     items,
-//     options
-//   );
-//   return gallery;
-// }
+import PhotoSwipe from "photoswipe";
+import PhotoSwipeUI_Default from "photoswipe/dist/photoswipe-ui-default";
+
+document.addEventListener("DOMContentLoaded", function () {
+  var images = [];
+
+  var parseImages = function (el) {
+    var childNodes = el.children;
+    var items = [];
+
+    Array.from(childNodes).forEach(function (node, index) {
+      if (node.tagName === "IMG") {
+        let item = {
+          src: node.src,
+          w: node.getAttribute("data-width"),
+          h: node.getAttribute("data-height"),
+        };
+        items.push(item);
+
+        node.addEventListener("click", () => openPhotoSwipe(index));
+      }
+    });
+
+    images = items;
+
+    var mainImageDiv = document.getElementById("show-photo");
+    if (
+      mainImageDiv &&
+      mainImageDiv.children[0] &&
+      mainImageDiv.children[0].tagName === "IMG"
+    ) {
+      mainImageDiv.children[0].addEventListener("click", () =>
+        openPhotoSwipe(0)
+      );
+    }
+  };
+
+  var openPhotoSwipe = function (startIndex) {
+    var options = {
+      index: startIndex,
+    };
+
+    var gallery = new PhotoSwipe(
+      pswpElement,
+      PhotoSwipeUI_Default,
+      images,
+      options
+    );
+    gallery.init();
+  };
+
+  parseImages(document.getElementById("photo-slide"));
+  var pswpElement = document.querySelectorAll(".pswp")[0];
+});
