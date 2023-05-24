a8f810f3715b03407885a43f682501f95a2581eb
Charley Liu <charleyliu716@gmail.com>
2023-05-24 15:06:48 -0400

    Reverted flash messages back to previous message

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -254,7 +254,7 @@ class RepositoriesController < SessionsController
     owner = User.find_by(id: owner_id)
 
     if repository.users.include? owner
-      flash[:alert] = "#{owner.username} is already in your repository"
+      flash[:alert] = "This user is already in your repository"
       redirect_to repository_path(repository.user_username, repository.slug)
       return
     end
@@ -262,7 +262,7 @@ class RepositoriesController < SessionsController
     repository.users << owner if !owner.nil?
 
     if repository.save
-      flash[:notice] = "#{owner.username} was added to your repository"
+      flash[:notice] = "This owner was added to your repository"
     else
       flash[:alert] = "Something went wrong."
     end
@@ -281,7 +281,7 @@ class RepositoriesController < SessionsController
     elsif owner.id == repository.user_id
       flash[:alert] = "You cannot remove the original owner of this repository."
     elsif repository.users.delete(owner)
-      flash[:notice] = "#{owner.username} was removed from your repository"
+      flash[:notice] = "This owner was removed from your repository"
     else
       flash[:alert] = "Something went wrong."
     end
