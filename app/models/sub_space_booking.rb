a99c2bcde33afe87430da7ea21de400b5f1b842c
Arthur Fetiveau <art.fetiveau@gmail.com>
2023-06-11 17:59:47 +0200

    Feat: Color Legend in Calendar

--- app/models/sub_space_booking.rb
+++ app/models/sub_space_booking.rb
@@ -9,4 +9,54 @@ class SubSpaceBooking < ApplicationRecord
   validates :description, presence: true
   validates :start_time, presence: true
   validates :end_time, presence: true
+
+  COLOR_LEGEND = [
+    {
+      id: "your_pending_bookings",
+      name: "Your Pending Bookings",
+      color: "#B5A500"
+    },
+    {
+      id: "your_confirmed_bookings",
+      name: "Your Confirmed Bookings",
+      color: "#49794b"
+    },
+    {
+      id: "other_pending_bookings",
+      name: "Other Pending Bookings",
+      color: "#FFA500"
+    },
+    {
+      id: "other_confirmed_bookings",
+      name: "Other Confirmed Bookings",
+      color: "#497979"
+    },
+    { id: "blocking_booking", name: "Blocking Bookings", color: "#ABABABFF" }
+  ]
+
+  def self.color_legend
+    COLOR_LEGEND
+  end
+
+  def color(current_user_id)
+    status =
+      if blocking
+        "blocking_booking"
+      elsif SubSpaceBookingStatus.find(
+            sub_space_booking_status_id
+          ).booking_status_id == BookingStatus::APPROVED.id
+        if user_id == current_user_id
+          "your_confirmed_bookings"
+        else
+          "other_confirmed_bookings"
+        end
+      else
+        if user_id == current_user_id
+          "your_pending_bookings"
+        else
+          "other_pending_bookings"
+        end
+      end
+    COLOR_LEGEND.find { |c| c[:id] == status }[:color]
+  end
 end
