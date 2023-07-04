fa591c8b5e47ed1094730ee94c62b9cdbe1b6e77
Arthur Fetiveau <art.fetiveau@gmail.com>
2023-07-04 18:18:07 +0200

    Fix: CORS Filter

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -4,7 +4,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
             "uottawa-makerspace.github.io/makerepo-react-app",
             "uottawa-makerspace.github.io",
             "app.makerepo.com",
-            /^(.*\.|)makerepo-app.pages.dev$/
+            %r{https://.*\.makerepo-app.pages.dev}
 
     resource "*",
              headers: :any,
