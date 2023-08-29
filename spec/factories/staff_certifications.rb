c47bbed581e29a7d234a4424dc7521b61b9442f8
Charley Liu <charleyliu716@gmail.com>
2023-08-29 17:16:17 -0400

    Added staff certification controller spec

--- spec/factories/staff_certifications.rb
+++ spec/factories/staff_certifications.rb
@@ -1,16 +1,4 @@
 FactoryBot.define do
   factory :staff_certification do
-    trait :certifications_attached do
-      after(:build) do |sc|
-        (1..StaffCertification::TOTAL_NUMBER_OF_FILES).each do |i|
-          pdf_file =
-            fixture_file_upload(
-              Rails.root.join("spec/support/assets", "RepoFile1.pdf"),
-              "application/pdf"
-            )
-          sc.send("pdf_file_#{i}").attach(pdf_file)
-        end
-      end
-    end
   end
 end
