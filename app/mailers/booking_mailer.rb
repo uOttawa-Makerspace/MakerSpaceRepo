5d4cef220d38ea5ecbdd736e41d20a482b798e74
Charley Liu <charleyliu716@gmail.com>
2023-10-23 00:45:32 -0400

    fixed booking emails with automatic approvals

--- app/mailers/booking_mailer.rb
+++ app/mailers/booking_mailer.rb
@@ -17,6 +17,26 @@ class BookingMailer < ApplicationMailer
     mail(to: @email, subject: "Booking needs approval")
   end
 
+  def send_booking_automatically_approved(booking_id)
+    @email =
+      ContactInfo.find_by(
+        space_id: SubSpaceBooking.where(id: booking_id).first.sub_space.space.id
+      ).email
+    @booking = SubSpaceBooking.find(booking_id)
+    @sub_space = SubSpace.find(@booking.sub_space_id)
+
+    return if @email.blank?
+    @message =
+      "A booking has been made and automatically approved in " +
+        @sub_space.name + " by " + @booking.name + " for " +
+        @booking.description + " on " +
+        @booking.start_time.to_formatted_s(:long) + " to " +
+        @booking.end_time.to_formatted_s(:long) +
+        ". If you want to disable automatic booking for this subspace, go to the admin space page."
+
+    mail(to: @email, subject: "Booking automatically approved.")
+  end
+
   def send_booking_approved(booking_id)
     @booking = SubSpaceBooking.find(booking_id)
     @sub_space = SubSpace.find(@booking.sub_space_id)
