2f691ab65601c867dfe771f36d8349fa995fe9d7
Charley Liu <charleyliu716@gmail.com>
2024-02-26 01:01:33 -0500

    try fixing specs

--- spec/factories/printer_types.rb
+++ spec/factories/printer_types.rb
@@ -1,19 +1,19 @@
 FactoryBot.define do
   factory :printer_type do
     trait :UM2P do
-      name { "Ultimaker 2+" }
+      name { "Ultimaker 2+ Test" }
       short_form { "UM2P" }
     end
     trait :UM3 do
-      name { "Ultimaker 3" }
+      name { "Ultimaker 3 Test" }
       short_form { "UM3" }
     end
     trait :RPL2 do
-      name { "Replicator 2" }
+      name { "Replicator 2 Test" }
       short_form { "RPL2" }
     end
     trait :Dremel do
-      name { "Dremel" }
+      name { "Dremel Test" }
       short_form { "" }
     end
   end
