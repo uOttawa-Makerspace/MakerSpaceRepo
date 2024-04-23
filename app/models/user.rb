69bf13a665a75abbb50d0c5b20b66f7e971cd266
Solin <solinmrf@gmail.com>
2024-04-23 19:57:15 -0400

    fixed statuses to match test spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -337,20 +337,20 @@ class User < ApplicationRecord
       # case when the user is part of the development program only
     elsif !volunteer_program? && dev_program? && !teams_program?
       status = 3
-      # case when the user is part of the teams program only
-    elsif !volunteer_program? && !dev_program? && teams_program?
-      status = 4
       # case when the user is part of both the volunteer and development programs
     elsif volunteer_program? && dev_program? && !teams_program?
-      status = 5
+      status = 4
       # case when the user is part of both the volunteer and teams programs
     elsif volunteer_program? && !dev_program? && teams_program?
-      status = 6
+      status = 5
       # case when the user is part of both the development and teams programs
     elsif !volunteer_program? && dev_program? && teams_program?
-      status = 7
+      status = 6
       # case when the user is part of all three programs
     elsif volunteer_program? && dev_program? && teams_program?
+      status = 7
+      # case when the user is part of the teams program only
+    elsif !volunteer_program? && !dev_program? && teams_program?
       status = 8
     end
     status
