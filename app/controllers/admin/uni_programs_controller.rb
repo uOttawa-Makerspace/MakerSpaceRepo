3484c34bae542fde653ec681a33eb127d1fc1883
PencilAmazing <16854231+PencilAmazing@users.noreply.github.com>
2024-06-23 19:44:44 +0300

    Touch up program upload, error reporting

--- app/controllers/admin/uni_programs_controller.rb
+++ app/controllers/admin/uni_programs_controller.rb
@@ -15,37 +15,49 @@ class Admin::UniProgramsController < AdminAreaController
 
   # Takes in a csv file, parses and verifies all
   def import_programs
+    allow_new_programs = params[:allow_new_programs]
     file = params[:file]
     file_ext = File.extname(file.original_filename)
-    raise "Unknown file type: #{file_ext}" unless file_ext == ".csv"
+    raise "Unknown file extension: #{file_ext}" unless file_ext == ".csv"
 
-    test_data = CSV.new(file.to_io)
+    test_data = CSV.read(file.to_io, headers: true)
 
     # test header
     expected_header = %w[program level faculty department]
-    unless test_data.shift.to_a == expected_header
-      raise "Wrong header, expecting #{expected_header}"
+    unless test_data.headers == expected_header
+      raise "Wrong header, expecting #{expected_header} but found #{test_data.headers}"
     end
 
     # test each row for existence, trim and watch out for whitespace
-    test_data.each do |row|
-      expected_header
-        .each
-        .with_index(1) do |k, lineno|
-          if row[k].nil? || row[k] == ""
+    # lineno skip headers
+    current_faculties = ProgramList.faculty_list
+    test_data
+      .filter
+      .with_index(2) do |row, lineno|
+        expected_header.each do |k|
+          if row[k].blank?
             raise "Error line number #{lineno}, column #{k} is empty"
           end
 
           row[k] = row[k].squish # remove possible BOM issues
         end
-    end
 
-    flash[:notice] = "Upload complete"
+        # if the university adds a new faculty, this will break,
+        # mostly designed to catch any user errors
+        unless allow_new_programs || current_faculties.include?(row["faculty"])
+          raise "Unknown faculty #{row["faculty"]} at line number #{lineno}"
+        end
+      end
+
     # Save to assets
-    newfile.rewind
     File.open(Rails.root.join("lib/assets/programs.csv"), "w") do |newfile|
-      newfile.write(file.read)
+      newfile.write(test_data.to_csv) # write out as string
     end
+  rescue StandardError => e # Grab any error, display to user
+    flash[:alert] = "Programs upload error: #{e}"
+  else
+    flash[:notice] = "Upload complete, #{test_data.count} programs found"
+  ensure
     redirect_to admin_uni_programs_index_path
   end
 end
