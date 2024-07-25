52c7e3e5fecaa4c8bfcc9ffad4ddade4ee3d0f1d
PencilAmazing <16854231+PencilAmazing@users.noreply.github.com>
2024-07-26 02:33:20 +0300

    Admin can hide printer types from view
    Adds a new column to all printer types, disables printers from being
    added to staff dashboard

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_06_23_173424) do
+ActiveRecord::Schema[7.0].define(version: 2024_07_25_202442) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "unaccent"
@@ -729,6 +729,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_06_23_173424) do
     t.string "short_form", default: ""
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "available", default: true
   end
 
   create_table "printers", id: :serial, force: :cascade do |t|
