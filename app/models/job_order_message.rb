11dd327e1104b4b7efbd1158cb60e3ca75acc988
PencilAmazing <16854231+PencilAmazing@users.noreply.github.com>
2024-07-26 13:35:09 +0300

    Fix job order message, add print failed message
    Works with same table as job order message, and uses path helper to
    properly determine the form method

--- app/models/job_order_message.rb
+++ app/models/job_order_message.rb
@@ -2,6 +2,15 @@ class JobOrderMessage < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :message, presence: true
 
+  def self.print_failed
+    # this won't be created first time
+    # fails if we don't supply a default message
+    create_with(message: "Print failed").find_or_create_by!(
+      name: "print_failed"
+    )
+  end
+
+  # Generate formatted message for job id
   def retrieve_message(job_id)
     print_id = "[PRINT_ID]"
     quote_balance = "[QUOTED_BALANCE]"
@@ -20,6 +29,19 @@ class JobOrderMessage < ApplicationRecord
       new_message
     else
       message
+    end
+  end
+
+  # Generated formatted message for printer
+  def format_print_failed(printer_id, user_id, notes)
+    printer = Printer.find_by_id(printer_id)
+    print_owner = User.find_by_id(user_id)
+    if printer && print_owner
+      message
+        .gsub("[PRINTER_NUMBER]", printer.number)
+        .gsub("[PRINT_OWNER_NAME]", print_owner.name)
+        .gsub("[PRINT_OWNER_USERNAME]", print_owner.username)
+        .gsub("[STAFF_NOTES]", notes)
     end
   end
 end
