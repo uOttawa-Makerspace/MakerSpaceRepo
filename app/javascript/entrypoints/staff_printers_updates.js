7625a6106de7f6a0c2a4e9a413bb19e752646a74
PencilAmazing <16854231+PencilAmazing@users.noreply.github.com>
2024-07-28 00:37:34 +0300

    Email status and modal from updates now functional
    Sending an email shows a flash with result, the modal in the printer
    updates window actually works now, and the staff notes label is properly attached

---
+++ app/javascript/entrypoints/staff_printers_updates.js
@@ -0,0 +1,15 @@
+window.populateMessageModal = function (element) {
+  //console.log(element.dataset.username + ' / ' + element.dataset.printerNumber)
+  let user_select = document.getElementById("user_dashboard_select").tomselect;
+  user_select.clearOptions(); // Clear prev options
+  user_select.addOption({
+    id: element.dataset.username,
+    name: element.dataset.username,
+  }); // Add our username manually
+  user_select.setValue(element.dataset.username); // Set that username
+  document.getElementById("printerNumberSelect").value =
+    element.dataset.printerNumber; // printers are already loaded - this isn't a tomselect i think
+};
+document.querySelectorAll(".print-failed-button").forEach((e) => {
+  e.addEventListener("click", (event) => populateMessageModal(event.target));
+});
