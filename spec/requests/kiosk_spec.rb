bd027757c0d0d09a5cff5225e480c10fb85e4f84
PencilAmazing <16854231+PencilAmazing@users.noreply.github.com>
2024-07-31 02:15:15 +0300

    Implement kiosk tests

--- spec/requests/kiosk_spec.rb
+++ spec/requests/kiosk_spec.rb
@@ -1,24 +1,5 @@
 require "rails_helper"
 
 RSpec.describe "Kiosks", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/kiosk/index"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /show" do
-    it "returns http success" do
-      get "/kiosk/show"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /edit" do
-    it "returns http success" do
-      get "/kiosk/edit"
-      expect(response).to have_http_status(:success)
-    end
-  end
+  # covered by controller tests
 end
