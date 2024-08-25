f23d2f1173ab31f02d8f12ade1a22685173ce0f2
PencilAmazing <16854231+PencilAmazing@users.noreply.github.com>
2024-08-25 13:28:03 +0300

    Locale specific notes

--- app/models/opening_hour.rb
+++ app/models/opening_hour.rb
@@ -3,8 +3,26 @@ class OpeningHour < ApplicationRecord
 
   def formatted(field)
     tag_map = I18n.t("hours").transform_keys { |k| "[#{k.upcase}]" }
+    # copy field
     formatted = self[field]
+    # transform simple tags
     tag_map.each { |tag, value| formatted.gsub!(tag, value) }
-    formatted
+    # transform locale specific tags [EN=text text] or [FR=text text]
+    # This uses a regex, searched for a pattern replaces with
+    # a captured group note text text
+    # This is rather convoluted, but I'd admit is somewhat easier
+    # than making a whole new table for this
+    # Get all locales
+    I18n.available_locales.each do |locale|
+      # interpolate locale regex
+      # /\[EN=(.*?)\]/
+      # /\[FR=(.*?)\]/
+      locale_tag = /\[#{locale.to_s.upcase}=(.*?)\]/
+      # Replace with first capture group \1
+      # or empty string if not current locale
+      formatted.gsub!(locale_tag, I18n.locale == locale ? '\1' : "")
+    end
+
+    return formatted
   end
 end
