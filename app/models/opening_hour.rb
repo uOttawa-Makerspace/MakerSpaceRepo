aaab93b014fe069c3e18a0cb67d807883f1adf51
PencilAmazing <16854231+PencilAmazing@users.noreply.github.com>
2024-08-25 11:17:47 +0300

    Modernize and simplify hours tile

--- app/models/opening_hour.rb
+++ app/models/opening_hour.rb
@@ -1,3 +1,10 @@
 class OpeningHour < ApplicationRecord
   belongs_to :contact_info, optional: true
+
+  def formatted(field)
+    tag_map = I18n.t("hours").transform_keys { |k| "[#{k.upcase}]" }
+    formatted = self[field]
+    tag_map.each { |tag, value| formatted.gsub!(tag, value) }
+    formatted
+  end
 end
