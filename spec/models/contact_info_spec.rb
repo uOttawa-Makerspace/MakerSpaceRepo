6ee3818729941cdae303ced6b46a174bd2a1e3fe
PencilAmazing <16854231+PencilAmazing@users.noreply.github.com>
2024-09-26 14:34:46 -0400

    Fix opening hours tests

--- spec/models/contact_info_spec.rb
+++ spec/models/contact_info_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe ContactInfo, type: :model do
   describe "Association" do
     context "has_one" do
-      it { should have_one(:opening_hour) }
+      it { should have_many(:opening_hours) }
     end
   end
 end
