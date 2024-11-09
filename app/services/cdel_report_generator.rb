926b03ac49525e7c6b24d5cee08ec2e4b04551f4
PencilAmazing <16854231+PencilAmazing@users.noreply.github.com>
2024-11-09 14:45:50 -0500

    New user reports, centralize filenames
    New user reports are just a dump of users and their created_at column
    
    Filenames are now returned by the report generator itself, to ensure
    consistency with mailer and admin report page.

--- app/services/cdel_report_generator.rb
+++ app/services/cdel_report_generator.rb
@@ -1,16 +1,41 @@
 # frozen_string_literal: true
 
 # SQL dumps into csv for CDEL
+# The goal here is to provide what CDEL doesn't have
+# CEED space visitors, lab and equipment usages, sign up times and rates
+# They know student demographics and faculties, and have much more accurate data
+# than we do. We don't bother reporting gender, faculty, because they can get that
+# via the student email/student number
+#
+# This class returns the data as a CSV, and the filename
+# because this is also being called by a periodic mailer
+# ensures consistency across the manual admin view generator and the MsrMailer
+#
+# NOTE: If selecting a column make sure you specify table name to avoid duplicating rows
 class CdelReportGenerator
+  # Used by the mailer
+  def self.generate_all_reports(start_date, end_date)
+    [
+      self.generate_visitors_report(start_date, end_date),
+      self.generate_certifications_report(start_date, end_date),
+      self.generate_new_users_report(start_date, end_date)
+    ]
+  end
+
   def self.generate_visitors_report(start_date, end_date)
-    headers = user_headers + %i[spaces.name sign_in_time sign_out_time]
+    headers =
+      user_headers +
+        %i[spaces.name lab_sessions.sign_in_time lab_sessions.sign_out_time]
     query =
       LabSession
         .where(created_at: start_date..end_date)
         .joins(:user, :space)
         .order(:created_at)
         .pluck(*headers)
-    make_into_csv headers, query
+    {
+      data: make_into_csv(headers, query),
+      filename: "CEED_visitors_dump-#{start_date}-#{end_date}.csv"
+    }
   end
 
   def self.generate_certifications_report(start_date, end_date)
@@ -30,7 +55,34 @@ class CdelReportGenerator
         .joins(:user, :training_session, { training: :skill }, :space)
         .order(:created_at)
         .pluck(*headers)
-    make_into_csv headers, query
+    {
+      data: make_into_csv(headers, query),
+      filename: "CEED_certifications_dump-#{start_date}-#{end_date}.csv"
+    }
+  end
+
+  def self.generate_new_users_report(start_date, end_date)
+    headers =
+      user_headers +
+        %i[
+          users.username
+          users.created_at
+          users.last_signed_in_time
+          users.last_seen_at
+          users.use
+          users.how_heard_about_us
+          users.confirmed
+        ]
+    query =
+      User
+        .where(created_at: start_date..end_date)
+        .order(:created_at)
+        .pluck(*headers)
+    {
+      data: make_into_csv(headers, query),
+      filename:
+        "CEED_new_users_dump-#{start_date.to_date}-#{end_date.to_date}.csv"
+    }
   end
 
   private
