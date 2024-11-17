b84a17a08eeb1a46e26b55d43ca6f4e989c8a26d
PencilAmazing <16854231+PencilAmazing@users.noreply.github.com>
2024-11-17 13:55:55 -0500

    Preprocess uploaded images, basic CRUD
    Assumes imagemagick is installed on the server. I know it is required by
    active storage itself, but I don't think the deployment script installs
    it. Has to be manual on new server/dev setup

--- app/controllers/staff/makerstore_links_controller.rb
+++ app/controllers/staff/makerstore_links_controller.rb
@@ -1,4 +1,55 @@
 class Staff::MakerstoreLinksController < StaffAreaController
   def index
+    @makerstore_links = MakerstoreLink.all
+  end
+
+  def show
+    @makerstore_link = MakerstoreLink.find(params[:id])
+  end
+
+  def new
+    @makerstore_link = MakerstoreLink.new
+  end
+
+  def create
+    @makerstore_link = MakerstoreLink.new(makerstore_link_params)
+    # file is uploaded to temporary storage
+    temp_filepath = makerstore_link_params[:image].tempfile.path
+    # compress the temporary file before upload
+    resized =
+      ImageProcessing::MiniMagick.source(temp_filepath).resize_to_fit!(220, 250)
+    # Reattach new temp file
+    @makerstore_link.image.attach(
+      io: resized,
+      filename: makerstore_link_params[:image].original_filename
+    )
+    if @makerstore_link.save
+      flash[:notice] = "Link created"
+      redirect_to staff_makerstore_links_path
+    else
+      flash[:alert] = "Link not created: " +
+        @makerstore_link.errors.full_messages.join(", ")
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def destroy
+    @makerstore_link = MakerstoreLink.find(params[:id])
+    if @makerstore_link.destroy
+      flash[:notice] = "Link destroyed"
+    else
+      flash[:alert] = "Error destroying link: " +
+        @makerstore_link.errors.full_messages.join(", ")
+    end
+    redirect_to staff_makerstore_links_path, status: :see_other
+  end
+
+  private
+
+  def makerstore_link_params
+    params.require(:makerstore_link).permit(:title, :url, :image, :order)
   end
 end
--- app/models/makerstore_link.rb
+++ app/models/makerstore_link.rb
@@ -1,4 +1,4 @@
 class MakerstoreLink < ApplicationRecord
-  validates :title, :url, :image_url, presence: true
+  validates :title, :url, :image, presence: true
   has_one_attached :image
 end
