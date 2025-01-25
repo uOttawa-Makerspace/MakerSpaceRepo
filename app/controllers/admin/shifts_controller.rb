58a713389edc409e25e7735cfe3a60a3bd02196e
PencilAmazing <16854231+PencilAmazing@users.noreply.github.com>
2025-01-25 18:53:17 -0500

    Eliminate one more N+1 query

--- app/controllers/admin/shifts_controller.rb
+++ app/controllers/admin/shifts_controller.rb
@@ -282,6 +282,7 @@ class Admin::ShiftsController < AdminAreaController
     @space_id = params[:space_id] if params[:space_id].present?
     StaffAvailability
       .includes(:user, :exceptions)
+      .includes(user: :staff_spaces)
       .where(
         user_id: StaffSpace.where(space_id: @space_id).pluck(:user_id),
         time_period: @time_period
