4a7be534e7e6f6733ab17d4f0c18ec429e01e304
PencilAmazing <16854231+PencilAmazing@users.noreply.github.com>
2025-02-08 14:54:55 -0500

    Upgrade deploy ruby version

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,6 +7,6 @@ role :db, %w[deploy@prod-server.makerepo.com]
 set :branch, "master"
 set :deploy_to, "/var/www/makerrepo"
 set :keep_releases, 3
-set :rbenv_ruby, "3.1.2"
+set :rbenv_ruby, "3.4.1"
 set :maintenance_template_path,
     File.expand_path("../../maintenance/maintenance.html.erb", __FILE__)
