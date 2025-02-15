4a865520831c74a843960e468d7184577d591ef1
PencilAmazing <16854231+PencilAmazing@users.noreply.github.com>
2025-02-15 16:10:04 -0500

    Fix JS errors on printer page

--- app/javascript/entrypoints/printers.js
+++ app/javascript/entrypoints/printers.js
@@ -1,11 +1,16 @@
 import TomSelect from "tom-select";
-new TomSelect("#remove_printer", {});
 
-document.getElementById("model_id").addEventListener("change", (event) => {
-  document.getElementById("newPrinterCode").innerText = event.target.options[
-    event.target.selectedIndex
-  ].text
-    .match(/\((.*)\)/)
-    .pop();
-});
-document.getElementById("model_id").dispatchEvent(new Event("change"));
+if (document.querySelector("#remove_printer")) {
+  new TomSelect("#remove_printer", {});
+}
+
+if (document.getElementById("model_id")) {
+  document.getElementById("model_id").addEventListener("change", (event) => {
+    document.getElementById("newPrinterCode").innerText = event.target.options[
+      event.target.selectedIndex
+    ].text
+      .match(/\((.*)\)/)
+      .pop();
+  });
+  document.getElementById("model_id").dispatchEvent(new Event("change"));
+}
