28127bf04f7485be26c099b013a9ba94903c97c0
PencilAmazing <16854231+PencilAmazing@users.noreply.github.com>
2025-05-23 09:35:50 -0400

    Use a variant on the home page

--- config/application.rb
+++ config/application.rb
@@ -36,6 +36,8 @@ module MakerSpaceRepo
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = "Eastern Time (US & Canada)"
 
+    config.active_storage.variant_processor = :mini_magick
+
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
