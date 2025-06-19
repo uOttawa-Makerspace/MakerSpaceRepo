a3ef620e4d88dbc1e58341c62f0c649578251ccd
PencilAmazing <16854231+PencilAmazing@users.noreply.github.com>
2025-06-19 10:38:27 -0400

    Rework migration a bit, make it cleaner

--- db/migrate/20250613150014_rework_development_program.rb
+++ db/migrate/20250613150014_rework_development_program.rb
@@ -1,72 +1,68 @@
 class ReworkDevelopmentProgram < ActiveRecord::Migration[7.2]
   def change
-    rename_column :badge_templates, :badge_name, :name
-    add_column :badge_templates, :name_fr, :string
-    rename_column :badge_templates, :name, :name_en
-    add_column :trainings, :list_of_skills, :string
-    add_column :trainings, :name_fr, :string
-
-    BadgeTemplate.all.each do |t|
-      Training.find_by(id: t.training_id).update(description: t.badge_description) unless t.training_id.nil?
+    change_table :badge_templates do |t|
+      # Split name into separate languages
+      t.rename :badge_name, :name_en
+      t.string :name_fr
     end
 
-    BadgeTemplate.all.each do |t|
-      Training.find_by(id: t.training_id).update(list_of_skills: t.list_of_skills) unless t.training_id.nil?
-    end
-
-    BadgeTemplate.all.each do |t|
-      Training.find_by(id: t.training_id).update(name_fr: t.name_fr) unless t.training_id.nil?
-    end
-
-    rename_column :trainings, :name, :name_en
-
-    add_column :trainings, :has_badge, :boolean, default: true
-
-    drop_table :badges
-
-    change_table :proficient_projects do |p|
-      p.remove :badge_template_id
-    end
-
-    drop_table :badge_requirements
-
-    drop_table :badge_templates
-
-    add_column :trainings, :description_fr, :string
     change_table :trainings do |t|
+      t.string :list_of_skills
+      t.rename :name, :name_en
+      t.string :name_fr
+      t.boolean :has_badge, default: true
+
       t.rename :description, :description_en
-    end
+      t.string :description_fr
 
-    add_column :trainings, :list_of_skills_fr, :string
-    change_table :trainings do |t|
       t.rename :list_of_skills, :list_of_skills_en
+      t.string :list_of_skills_fr
     end
-
-    Training.all.each do |t|
-      t.update(description_fr: t.description_en.split('||').last)
-      t.update(description_en: t.description_en.split('||').first)
-    end 
-
-    Training.all.each do |t|
-      t.update(name_fr: t.name_fr.split('-').last) unless t.name_fr.nil?
-    end 
 
     create_table :training_requirements do |t|
-      t.timestamps null: false
       t.references :training, index: true, foreign_key: true
       t.references :proficient_project, index: true, foreign_key: true
+      t.timestamps
     end
 
     add_column :certifications, :level, :string
-    Certification.all.each do |c|
-      c.update(level: c.training_session.level) unless c.training_session.level.nil?
-    end 
+
+    change_table :proficient_projects do |t|
+      t.remove :badge_template_id
+    end
 
     create_table :proficient_project_sessions do |t|
-      t.timestamps null: false
       t.references :certification, index: true, foreign_key: true
       t.references :proficient_project, index: true, foreign_key: true
       t.string :level
+      t.timestamps
+    end
+
+    drop_table :badges
+    drop_table :badge_requirements
+    drop_table :badge_templates
+
+    Certification.all.find_each do |c|
+      unless c.training_session.level.nil?
+        c.update(level: c.training_session.level)
+      end
+    end
+
+    Training.all.find_each do |t|
+      t.update(
+        description_en: t.description_en.split("||").first,
+        description_fr: t.description_en.split("||").last
+      )
+      t.update(name_fr: t.name_fr.split("-").last) unless t.name_fr.nil?
+    end
+
+    BadgeTemplate.all.find_each do |t|
+      next if t.training_id.nil?
+      Training.find_by(id: t.training_id).update(
+        name_fr: t.name_fr,
+        description: t.badge_description,
+        list_of_skills: t.list_of_skills
+      )
     end
   end
 end
