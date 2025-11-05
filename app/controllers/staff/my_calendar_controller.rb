c1e6ad9f5a0b9d8d502dbc4b3a4dcb4043aa8b26
Max T <maxsontully@gmail.com>
2025-11-05 10:38:35 -0500

    hot fix skipping events in past resulting in null values

--- app/controllers/staff/my_calendar_controller.rb
+++ app/controllers/staff/my_calendar_controller.rb
@@ -13,9 +13,6 @@ class Staff::MyCalendarController < StaffAreaController
     return render json: { error: "Space ID is required" }, status: :bad_request if params[:id].blank?
 
     events = Event.where(space_id: params[:id], draft: false).map do |event|        
-      next if event.recurrence_rule.blank? && event.end_time < (Time.now.utc - 2.months)
-      # Skip events that are far in the past
-
         title = if event.title == event.event_type.capitalize && !event.event_assignments.empty?
           "#{'✎ ' if event.draft}#{event.event_type.capitalize} for #{event.event_assignments.map do |ea|
 ea.user.name end.join(", ")}"
