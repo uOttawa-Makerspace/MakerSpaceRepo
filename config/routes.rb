b3dfd60a30c14bf1536b22137f809843a9fd3907
Surya Vasudev <suryavasudev05@gmail.com>
2025-11-10 10:45:07 -0500

    added extra space

--- config/routes.rb
+++ config/routes.rb
@@ -438,7 +438,7 @@ Rails.application.routes.draw do
       collection do
         get 'unavailabilities_json/:id', to: 'calendar#unavailabilities_json', as: :unavailabilities_json
         get 'imported_calendars_json/:id', to: 'calendar#imported_calendars_json', as: :imported_calendars_json
-        get 'ics_to_json', to: 'calendar#ics_to_json', as: :ics_to_json
+        get 'ics_to_json', to: 'calendar#ics_to_json', as: :ics_to_json 
         post :update_color
       end
     end
