1c765e662ba0a4a2d132a4a30023513c00dfe9a9
Surya Vasudev <suryavasudev05@gmail.com>
2025-11-10 10:26:40 -0500

    Wrong revert mb

--- config/routes.rb
+++ config/routes.rb
@@ -438,8 +438,7 @@ Rails.application.routes.draw do
       collection do
         get 'unavailabilities_json/:id', to: 'calendar#unavailabilities_json', as: :unavailabilities_json
         get 'imported_calendars_json/:id', to: 'calendar#imported_calendars_json', as: :imported_calendars_json
-        get 'ics_to_json', to: 'calendar#ics_to_json', as: :ics_to_json 
-        get 'shifts_json', to: 'calendar#shifts_json', as: :shifts_json
+        get 'ics_to_json', to: 'calendar#ics_to_json', as: :ics_to_json
         post :update_color
       end
     end
