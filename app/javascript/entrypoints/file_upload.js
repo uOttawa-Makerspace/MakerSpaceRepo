1db7b30b2043875166f6197a905a6d66bc5df964
PencilAmazing <16854231+PencilAmazing@users.noreply.github.com>
2025-12-07 15:13:29 -0500

    Remove button styling

--- app/javascript/entrypoints/file_upload.js
+++ app/javascript/entrypoints/file_upload.js
@@ -5,8 +5,16 @@ function appendFileToPreview(file, previewContainer, fieldPrefix) {
   preview.classList.add("file-upload-item-preview");
 
   const previewImage = document.createElement("img");
+  // If image errors out, this isn't something we can preview.
+  previewImage.addEventListener("error", (evt) => {
+    evt.currentTarget.removeAttribute("src");
+  });
   previewImage.src = URL.createObjectURL(file);
   preview.appendChild(previewImage);
+
+  const previewFilename = document.createElement("span");
+  previewFilename.innerText = file.name;
+  preview.appendChild(previewFilename);
 
   const previewInput = document.createElement("input");
   previewInput.type = "file";
@@ -14,18 +22,21 @@ function appendFileToPreview(file, previewContainer, fieldPrefix) {
   const dataTransfer = new DataTransfer();
   dataTransfer.items.add(file);
   previewInput.files = dataTransfer.files;
+  // Rails requires arrays to be assigned an index for nested attributes to
+  // work with pre-existing files. Unix timestamp are okay.
   previewInput.name = `${fieldPrefix}[${Date.now()}][image]`;
   preview.append(previewInput);
 
+  // For non-persisted files this is a button
   const previewDelete = document.createElement("button");
-  previewDelete.addEventListener("click", (evt) => {
-    preview.remove();
-    evt.preventDefault;
-  });
-  previewDelete.role = "button"; // Prevent form submit
-  previewDelete.classList.add("btn");
-  previewDelete.classList.add("btn-danger");
   previewDelete.innerHTML = "<i class='fa fa-remove'></i> Delete";
+  previewDelete.classList.add("file-upload-item-delete");
+  previewDelete.role = "button"; // Prevent form submit
+  previewDelete.addEventListener("click", (evt) => {
+    // Remove parent preview container
+    preview.remove();
+    evt.preventDefault();
+  });
   preview.append(previewDelete);
 
   previewContainer.appendChild(preview);
@@ -52,6 +63,25 @@ function createFileInput(target) {
   target.dataset.fileUploadPrefix = target.name;
   // Clear name, we don't want to submit this input anymore
   target.name = null;
+
+  // For pre-existing preview boxes attach a delete handler to the checkboxes
+  const preview = document.querySelector(
+    target.dataset.fileUploadPreviewSelector,
+  );
+  preview
+    .querySelectorAll("[data-file-upload-preview] .file-upload-item-preview")
+    .forEach((previewBox) => {
+      previewBox
+        .querySelectorAll("[data-file-upload-item-delete]")
+        .forEach((check) => {
+          check.addEventListener("click", (evt) => {
+            if (evt.currentTarget.checked) {
+              // We have to submit the _destroy flag to server, hide preview instead
+              previewBox.hidden = true;
+            }
+          });
+        });
+    });
 }
 
 document.addEventListener("turbo:load", function () {
