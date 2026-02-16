6f492e2b316a4522217148ba787d1ab42748688c
PencilAmazing <16854231+PencilAmazing@users.noreply.github.com>
2026-02-16 15:28:00 -0500

    Change locker size in-place

--- app/javascript/entrypoints/lockers.js
+++ app/javascript/entrypoints/lockers.js
@@ -39,6 +39,64 @@ document.addEventListener("turbo:load", function () {
 });
 
 document.addEventListener("turbo:load", function () {
+  // Get all select dropdowns
+  let lockerSizeSelects = document.querySelectorAll("[data-locker-size-for]");
+
+  lockerSizeSelects.forEach((select) => {
+    select.addEventListener("change", function () {
+      // On select dropdown change, disable the dropdown while we send the
+      // request. re-enable after we're done.
+      this.tomselect.disable();
+
+      // Find parent cell
+      let cell = this.closest("td");
+
+      // Show spinner
+      let spinner = cell.querySelector(".spinner-border");
+      spinner.hidden = false;
+
+      let checkmark = cell.querySelector("i");
+
+      let lockerId = this.dataset.lockerSizeFor;
+      // console.log(`Changing ${lockerId} to ${this.value}`);
+
+      const formData = new FormData();
+      formData.append(this.name, this.value);
+
+      fetch(`/lockers/${lockerId}`, {
+        method: "PATCH",
+        headers: {
+          "X-CSRF-Token": document.querySelector('[name="csrf-token"]').content,
+        },
+        body: formData,
+      })
+        .catch((error) => {
+          console.error(error);
+        })
+        .finally(() => {
+          // Hide spinner
+          spinner.hidden = true;
+          this.tomselect.enable();
+          // Show checkmark
+          checkmark.hidden = false;
+          // Hide after 300ms
+          setTimeout(() => {
+            checkmark.hidden = true;
+          }, 400);
+        });
+    });
+  });
+
+  // Toggle to prevent accidental changes
+  let lockerEditToggle = document.querySelector("#lockerEditToggle");
+  lockerEditToggle.addEventListener("change", function () {
+    lockerSizeSelects.forEach((el) => {
+      this.checked ? el.tomselect.enable() : el.tomselect.disable();
+    });
+  });
+});
+
+document.addEventListener("turbo:load", function () {
   // Setup table selection
   let locker_inventory_table = document.querySelector(
     "#locker_inventory_table",
