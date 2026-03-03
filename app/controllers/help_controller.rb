3fec45a70ff2e30edc6e0e8e3bffac1b91d797d9
PencilAmazing <16854231+PencilAmazing@users.noreply.github.com>
2026-03-02 23:32:47 -0500

    Make the share email checkbox do something

--- app/controllers/help_controller.rb
+++ app/controllers/help_controller.rb
@@ -22,7 +22,7 @@ class HelpController < SessionsController
         @help.email,
         @help.subject,
         @help.comments.to_plain_text,
-        params[:app_version]
+        params[:share_email]
       ).deliver_later
     else
       flash[:alert] = 'An error occurred while receiving your issue.'
@@ -34,46 +34,7 @@ class HelpController < SessionsController
     flash[:alert] = 'An internal error occurred while receiving your issue.'
     render :show, status: :unprocessable_entity
   end
-
-  def send_email
-    @help = Help.new(params[:help])
-
-    unless verify_turnstile
-      respond_to do |format|
-        format.html do
-          redirect_to help_path,
-                      alert:
-                        'Please make sure you fill out the captcha correctly.'
-        end
-        format.json do
-          render json: { status: 'error' }, status: :internal_server_error
-        end
-      end
-    end
-
-    unless @help.valid?
-      flash[:alert] = 'Please fill in all fields'
-      render 'main', status: :unprocessable_content
-    end
-
-    MsrMailer.issue_email(
-      params[:help][:name],
-      params[:help][:email],
-      params[:help][:subject],
-      params[:help][:comments],
-      params[:app_version]
-    ).deliver_later
-
-    respond_to do |format|
-      format.html do
-        redirect_to help_path,
-                    notice:
-                      'Email successfully sent. You will be contacted soon.'
-      end
-      format.json { render json: { status: 'success' } }
-    end
-  end
-
+  
   private
 
   def help_params
