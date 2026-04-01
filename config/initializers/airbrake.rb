e0cb85438e58d83e76d8a8a7e21a4ad0064b78a6
PencilAmazing <16854231+PencilAmazing@users.noreply.github.com>
2026-04-01 12:44:40 -0400

    Put up airbrake again
    Fill in the credentials, enable for staging

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -9,8 +9,10 @@
 #
 # Configuration details:
 # https://github.com/airbrake/airbrake-ruby#configuration
-if (project_id = ENV["AIRBRAKE_PROJECT_ID"]) &&
-     project_key = (ENV["AIRBRAKE_PROJECT_KEY"] || ENV["AIRBRAKE_API_KEY"])
+project_id = Rails.application.credentials[Rails.env.to_sym]&.airbrake&.project_id
+project_key = Rails.application.credentials[Rails.env.to_sym]&.airbrake&.project_key
+
+if project_id && project_key
   Airbrake.configure do |c|
     # You must set both project_id & project_key. To find your project_id and
     # project_key navigate to your project's General Settings and copy the
@@ -44,7 +46,7 @@ if (project_id = ENV["AIRBRAKE_PROJECT_ID"]) &&
     # unwanted environments such as :test.  NOTE: This option *does not* work if
     # you don't set the 'environment' option.
     # https://github.com/airbrake/airbrake-ruby#ignore_environments
-    c.ignore_environments = %w[test development staging]
+    c.ignore_environments = %w[test development]
 
     # A list of parameters that should be filtered out of what is sent to
     # Airbrake. By default, all "password" attributes will have their contents
